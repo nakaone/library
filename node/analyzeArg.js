420f196e6918d810a911803cf5e3020950f456cd
nakaone <57969878+nakaone@users.noreply.github.com>
2023-06-28 10:11:01 +0900

    MDを修正

---
+++ node/analyzeArg.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {object} analyzeArg - コマンドライン引数の分析結果
+ * @prop {Object.<string, number>} opt - スイッチを持つ引数について、スイッチ：値形式にしたオブジェクト
+ * @prop {string[]} val - スイッチを持たない引数の配列
+ */
+/** analyzeArg: コマンドライン引数を分析
+ * @param {void} - なし
+ * @returns {analyzeArg} 分析結果のオブジェクト
+ */
+function analyzeArg(){
+  console.log('===== analyzeArg start.');
+  const v = {rv:{}};
+  try {
+
+    for( v.i=2 ; v.i<process.argv.length ; v.i++ ){
+      // process.argv:コマンドライン引数の配列
+      v.m = process.argv[v.i].match(/^(\-*)([0-9a-zA-Z]+):*(.*)$/);
+      if( v.m && v.m[1].length > 0 ){
+        v.rv.opt[v.m[2]] = v.m[3];
+      } else {
+        v.rv.val.push(process.argv[v.i]);
+      }
+    }
+
+    console.log('v.rv='+v.rv);
+    console.log('===== analyzeArg end.');
+    return v.rv;
+  } catch(e){
+    // ブラウザで実行する場合はアラート表示
+    if( typeof window !== 'undefined' ) alert(e.stack); 
+    //throw e; //以降の処理を全て停止
+    v.rv.stack = e.stack; return v.rv; // 処理継続
+  }
+}
--- node/executable.js
+++ node/executable.js
@@ -50,40 +50,6 @@ function executable(content){
     v.rv.stack = e.stack; return v.rv; // 処理継続
   }
 }
-
-/**
- * @typedef {object} analyzeArg - コマンドライン引数の分析結果
- * @prop {Object.<string, number>} opt - スイッチを持つ引数について、スイッチ：値形式にしたオブジェクト
- * @prop {string[]} val - スイッチを持たない引数の配列
- */
-/** analyzeArg: コマンドライン引数を分析
- * @param {void} - なし
- * @returns {analyzeArg} 分析結果のオブジェクト
- */
-function analyzeArg(){
-  const v = {rv:{opt:{},val:[]}};
-  console.log('===== analyzeArg start.');
-  try {
-
-    for( v.i=2 ; v.i<process.argv.length ; v.i++ ){
-      // process.argv:コマンドライン引数の配列
-      v.m = process.argv[v.i].match(/^(\-*)([0-9a-zA-Z]+):*(.*)$/);
-      if( v.m && v.m[1].length > 0 ){
-        v.rv.opt[v.m[2]] = v.m[3];
-      } else {
-        v.rv.val.push(process.argv[v.i]);
-      }
-    }
-
-    //console.log('rv='+JSON.stringify(v.rv));
-    console.log('===== analyzeArg end.');
-    return v.rv;
-  } catch(e){
-    if( typeof window !== 'undefined' ) alert(e.stack); // ブラウザで実行する場合はアラート表示
-    //throw e; //以降の処理を全て停止
-    v.rv.stack = e.stack; return v.rv; // 処理継続
-  }
-}
 /* Node.js上で単体実行する場合の処理 */
 function onNode(){
   console.log('===== onNode start.');
@@ -111,14 +77,16 @@ if( typeof window === 'undefined' ){
 } else {
   // ブラウザ上で実行する場合の処理
   window.addEventListener('DOMContentLoaded',() => {
-    const v = {mode:{num:1,value:'doc'}};
+    const v = {mode:{num:1,value:'doc'},text:''};
 
     // MarkDownで記述された部分をHTML化して表示
     v.doc = document.createElement('div');
     v.doc.classList.add('sz');
-    v.text = document.querySelector('style.jsdoc').textContent
-      + document.querySelector('style.doc').textContent;
-    //v.text = executable(v.text);
+    ['style.jsdoc','style.doc'].forEach(selector => {
+      v.inner = document.querySelector(selector).textContent.trim();
+      v.m = v.inner.match(/^\/\*([\s\S]+)\*\//);
+      v.text += v.m ? v.m[1] : v.inner;
+    });
     v.md = marked(v.text);
     v.doc.innerHTML = v.md;
     document.querySelector('body div.doc').prepend(v.doc);
