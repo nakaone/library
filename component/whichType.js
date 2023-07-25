f33d494c699dae3d7ba74d1168a303685a40c6ac
nakaone <57969878+nakaone@users.noreply.github.com>
2023-07-25 12:35:18 +0900

    ローディング画面を追加

---
+++ component/whichType.js
@@ -0,0 +1,13 @@
+
+function whichType(arg,is){
+  let rv = String(Object.prototype.toString.call(arg).slice(8,-1));
+  switch(rv){
+    case 'Number': if(Number.isNaN(arg)) rv = 'NaN'; break;
+    case 'Function': if(!('prototype' in arg)) rv = 'Arrow'; break;
+  }
+  if( typeof is === 'string' ){
+    return rv.toLowerCase() === is.toLowerCase();
+  } else {
+    return rv;
+  }
+}
