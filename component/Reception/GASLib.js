662018fa624fcbecf65d2d9ddc7cfa28e6f1b3b2
nakaone <57969878+nakaone@users.noreply.github.com>
2023-08-14 18:08:41 +0900

    recept2A/2Bドラフト作成

--- component/Reception/GASLib.js
+++ component/Reception/GASLib.js
@@ -190,8 +190,41 @@ function recept2A(arg){
   try {
     console.log(v.whois+' start.',arg.dt);
 
+    v.step = '1.1'; // recept2B問合せ用にデータを暗号化
+    v.dt = JSON.stringify({
+      fc : 'recept2B',
+      arg: {  // recept2B()に渡す引数
+        entryNo  : arg.fm,    // クライアントの受付番号
+        publicKey: arg.publicKey,  // 同公開鍵
+        data     : arg.dt,    // 変更された参加者情報
+      }
+    });
+    console.log('v.dt='+v.dt);
+    
+    v.step = '1.2'; // 暗号化
+    v.dt = encodeURI(v.dt); // カナ漢字が入るのでencode
+    v.dt = cryptico.encrypt(v.dt,arg.master,arg.RSAkey);
+    if( v.dt.status !== 'success' )
+      throw new Error("recept2A encrypt failed.\n"+JSON.stringify(v.dt));
 
-    v.step = '';
+    v.step = '2.1'; // recept1Bに問い合わせ
+    v.res = UrlFetchApp.fetch(arg.masterAPI,{
+      'method': 'post',
+      'contentType': 'application/json',
+      'muteHttpExceptions': true, // https://teratail.com/questions/64619
+      'payload' : JSON.stringify({
+        fm: 'gateway',
+        to: 'master',
+        md: 3,  // 署名付き暗号化
+        ts: Date.now(),
+        dt: v.dt.cipher,
+      }),  
+    }).getContentText();
+    console.log('res='+v.res);
+    v.step = '2.2';
+    v.rv = JSON.parse(v.res);
+
+    v.step = '3'; // 成功/不成功に関わらずrecept1Bの戻り値を返信
     console.log(v.whois+' normal end.\n'+JSON.stringify(v.rv));
     return v.rv;
 
@@ -209,7 +242,6 @@ function recept2A(arg){
 /** 変更要求の正当性を確認、問題なければ変更結果を返す
  * 
  * @param {Object} arg 
- * @param {string} arg.keyword - 検索キー文字列(受付番号または氏名)
  * @param {string} arg.entryNo - 問合せ元の受付番号
  * @param {string} arg.publicKey - 問合せ元の公開鍵
  * @param {Object} arg.data - 変更された参加者情報(recept2Aの引数)
@@ -226,8 +258,29 @@ function recept2B(arg){
   try {
     console.log(v.whois+' start.',arg.dt);
 
-    
-    v.step = '';
+    v.step = '1'; // クライアント情報を抽出
+    v.master = szSheet('master');
+    v.client = v.master.lookup(arg.dt.entryNo,'entryNo');
+
+    // 正当性チェック
+    v.step = '2.1'; // 問合せ元のentryNoはauthrityを持つ
+    if( (v.client.authority & 2) === 0 ){
+      v.rv.isErr = true;
+      v.rv.message = '検索権限がありません';
+    }
+    v.step = '2.2'; // 問合せ元の公開鍵が登録されているpublicKeyと一致
+    if( v.client.publicKey !== arg.dt.publicKey ){
+      v.rv.isErr = true;
+      v.rv.message = '問合せ元が不適切です';
+    }
+    v.step = '2.3';
+    if( v.rv.isErr ) return v.rv;
+
+    v.step = '3'; // 検索キーに合致する参加者情報を抽出
+    v.rv.result = v.master.update(arg.dt,
+      {key:'entryNo',value:arg.data.entryNo});
+
+    v.step = '4';
     console.log(v.whois+' normal end.\n'+JSON.stringify(v.rv));
     return v.rv;
 
