5f391f569055aa5d713eb2b54d485c33ddf79442
nakaone <57969878+nakaone@users.noreply.github.com>
2023-08-29 15:49:32 +0900

    setupList

--- component/drawPassport/core.js
+++ component/drawPassport/core.js
@@ -35,7 +35,7 @@ class drawPassport {
       this.#setupSummary();
 
       v.step = 3; // 参加者一覧
-      //this.#setupList();
+      this.#setupList();
 
       v.step = 4; // 詳細情報
       //this.#setupDetail();
@@ -78,11 +78,25 @@ class drawPassport {
       document.head.appendChild(this.style);
       this.style.innerText = `
       .drawPassport {
+        margin: 1rem;
         width: calc(100% - 2rem);
         display: grid;
+        row-gap: 1rem;
         grid-template-columns: 1fr;
-        padding: 1rem;
-        font-size: 1rem;
+        /*
+        width: 100%;
+        margin: 1rem;
+        width: calc(100% - 2rem);
+        */
+      }
+      .drawPassport > div {
+        width: 100%;
+        display: grid;
+        gap: 1rem;
+        /*
+        width: calc(100% - 6rem);
+        margin: 1rem 0px;
+        */
       }
       .drawPassport rt {
         font-size: 50%;
@@ -128,24 +142,20 @@ class drawPassport {
     try {
 
       v.step = 1; // CSS定義
-      this.style.innerText += `
+      this.style.innerText = (this.style.innerText + `
       .drawPassport .summary {
-        width: 100%;
-        margin: 1rem 0px;
-        display: grid;
         grid-template-columns: repeat(12, 1fr);
-        gap: 1rem;
       }
       .drawPassport .summary [name="qrcode"]{
         padding: 0rem;
         grid-row: 1 / 3;
         grid-column: 1 / 5;
       }
-      .drawPassport .summary [name="entryNo"]{
+      .drawPassport .summary [name="entryStr"]{
         grid-row: 1 / 2;
         grid-column: 5 / 13;
       }
-      .drawPassport .summary [name="entryNo"] span {
+      .drawPassport .summary [name="entryStr"] span {
         font-size: 2rem;
       }
       .drawPassport .summary [name="申込者氏名"]{
@@ -158,19 +168,26 @@ class drawPassport {
       .drawPassport .summary rt span {
         font-size: 1rem;
       }
-      `;
+      `).replaceAll(/\n/g,'').replaceAll(/\s+/g,' ');
 
       v.step = 2; // HTML定義
-      this.parent.innerHTML += `
-      <div class="summary">
-        <div name="qrcode"></div>
-        <div name="entryNo">No.<span class="v"></span></div>
-        <div name="申込者氏名"><ruby><span class="v"></span>
-          <rt name="申込者カナ"><span class="v"></span></rt>
-        </ruby></div>
-      </div>
-      `;
-      this.summary = this.parent.querySelector(':scope > .summary');
+      this.summary = createElement(
+        {attr:{class:'summary'},children:[
+          {attr:{name:'qrcode'}},
+          {attr:{name:'entryStr'},text:'No.',children:[
+            {tag:'span',attr:{class:'v'}},
+          ]},
+          {attr:{name:'申込者氏名'},children:[
+            {tag:'ruby',children:[
+              {tag:'span',attr:{class:'v'}},
+              {tag:'rt',attr:{name:'申込者カナ'},children:[
+                {tag:'span',attr:{class:'v'}}
+              ]},
+            ]}
+          ]},
+        ]},
+      );
+      this.parent.appendChild(this.summary);
 
       v.step = 3; // QRコード表示
       this.data.entryStr = String('0000'+this.data.entryNo).slice(-4);
@@ -186,7 +203,7 @@ class drawPassport {
       });
   
       v.step = 4; // その他文字情報表示
-      ['entryNo','申込者氏名','申込者カナ'].forEach(x => {
+      ['entryStr','申込者氏名','申込者カナ'].forEach(x => {
         v.x = x;
         v.element = this.summary.querySelector('[name="'+x+'"] .v');
         v.element.innerText = this.data[v.x];
@@ -211,73 +228,128 @@ class drawPassport {
     try {
 
       v.step = 1; // CSS定義
-      this.style.innerText += `
+      this.style.innerText = (this.style.innerText + `
       .drawPassport .list .content {
         width: 100%;
         margin: 1rem 0px;
         display: grid;
-        grid-template-columns: repeat(12, 1fr);
+        grid-template-columns: repeat(10, 1fr);
         gap: 0.2rem;
       }
       .drawPassport .list .content.hide {
         display: none;
       }
       .drawPassport .list .content div:nth-child(4n+1) {
-        grid-column: 1 / 3;
+        grid-column: 1 / 2;
       }
       .drawPassport .list .content div:nth-child(4n+2) {
-        grid-column: 3 / 7;
+        grid-column: 2 / 7;
       }
       .drawPassport .list .content div:nth-child(4n+3) {
-        grid-column: 7 / 10;
+        grid-column: 7 / 9;
       }
       .drawPassport .list .content div:nth-child(4n+4) {
-        grid-column: 10 / 13;
+        grid-column: 9 / 11;
       }
-      `;
+      `).replaceAll(/\n/g,'').replaceAll(/\s+/g,' ');
 
       v.step = 2; // HTML定義
-      this.parent.innerHTML += `
-      <div class="list">
-        <div class="label">
-          <p>参加者一覧</p>
-          <button>表示</button>
-        </div>
-        <div class="content">
-          <div class="th">No</div>
-          <div class="th">氏名</div>
-          <div class="th">所属</div>
-          <div class="th">参加費</div>
-        </div>
-      </div>
-      `;
-      this.list = this.parent.querySelector(':scope > .list');
+      this.list = createElement(
+        {attr:{class:'list'},children:[
+          {attr:{class:'label'},children:[
+            {tag:'p',text:'参加者一覧'},
+            {tag:'button',text:'表示'},
+          ]},
+          {attr:{class:'content'},children:[
+            {attr:{class:'th'},text:'No'},
+            {attr:{class:'th'},text:'氏名'},
+            {attr:{class:'th'},text:'所属'},
+            {attr:{class:'th'},text:'参加費'},
+          ]},
+        ]}
+      );
+      this.parent.appendChild(this.list);
 
-      v.step = 3; // 参加者情報をセット
+      v.step = 3; // 参加者一覧開閉ボタン
+      // 初期状態設定
+      this.toggle('.list',this.showList);
+      // イベントリスナ設定
+      this.list.querySelector('.label button')
+      .addEventListener('click',this.toggle);
+
+      v.step = 4; // 参加者情報をセット
+      v.content = this.list.querySelector('.content');
       for( v.i=1 ; v.i<6 ; v.i++ ){
         v.prefix = '参加者0' + v.i;
+        // 氏名が未登録の場合はスキップ
         if( this.data[v.prefix+'氏名'].length === 0 )
           continue;
-        this.list.appendChild(createElement({
+        v.content.appendChild(createElement({
           attr: {class:'td'},
           style: {textAlign:'right'},
           text: v.i,
         }));
-        this.list.appendChild(createElement(
+        v.content.appendChild(createElement(
           {attr: {class:'td'},children:[
             {tag:'ruby',text:this.data[v.prefix+'氏名'],children:[
               {tag:'rt',text:this.data[v.prefix+'カナ']}
         ]}]}));
-        this.list.appendChild(createElement({
+        v.content.appendChild(createElement({
           attr: {class:'td'},
           text: this.data[v.prefix+'所属'],
         }));
-        this.list.appendChild(createElement({
+        v.content.appendChild(createElement({
           attr: {class:'td'},
           text: this.data['fee0'+v.i],
         }));
       }
     
+      console.log(v.whois+' normal end.',v.rv);
+      return v.rv;
+
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 表示/非表示ボタンクリック時の処理を定義
+   * @param {PointerEvent|string} event - クリック時のイベントまたはボタンのCSSセレクタ
+   * @param {boolean} show - trueなら開く
+   * @returns {void}
+   */
+  toggle = (event,show) => {
+    const v = {whois:'drawPassport.toggle',step:1,rv:null};
+    console.log(v.whois+' start.');
+    try {
+
+      let content;  // 表示/非表示を行う対象となる要素
+      let button;   // クリックされたボタンの要素
+      if( typeof event === 'string' ){
+        v.step = 1.1; // 初期設定時(引数がPointerEventではなくstring)
+        content = this.parent.querySelector(event+' .content');
+        button = this.parent.querySelector(event+' button');
+      } else {
+        v.step = 1.2; // ボタンクリック時
+        content = event.target.parentElement.parentElement
+        .querySelector('.content');
+        button = event.target;
+      }
+      console.log(content,button);
+
+      v.step = 2; // 表示->非表示 or 非表示->表示 を判断
+      let toOpen = show ? show : (button.innerText === '表示');
+      if( toOpen ){
+        v.step = 2.1; // 表示に変更する場合
+        button.innerText = '非表示';
+        content.classList.remove('hide');
+      } else {
+        v.step = 2.2; // 非表示に変更する場合
+        button.innerText = '表示';
+        content.classList.add('hide');
+      }
+
+      v.step = 3;
       console.log(v.whois+' normal end.',v.rv);
       return v.rv;
 
