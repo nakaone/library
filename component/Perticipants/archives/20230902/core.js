074ec95ed5311fe5f922ce2ba72904dc3b5f687b
nakaone <57969878+nakaone@users.noreply.github.com>
2023-09-05 13:26:37 +0900

    setupInstance: backup

---
+++ component/Perticipants/archives/20230902/core.js
@@ -0,0 +1,562 @@
+/**
+ * @classdesc 参加者情報の表示・編集
+ * 
+ * - [JavaScriptでの rem ⇔ px に変換するテクニック＆コード例](https://pisuke-code.com/javascript-convert-rem-to-px/)
+ */
+class Perticipants {
+  /**
+   * @constructor
+   * @param {HTMLElement|string} parent - 親要素またはそのCSSセレクタ
+   * @param {Object} [data=null] - Authから返された参加者情報(Auth.info)
+   * @param {Object} [opt={}] - オプション
+   * @returns {true|Error}
+   */
+  constructor(parent,data=null,opt={}){
+    const v = {whois:'Perticipants.constructor',rv:true,step:0,
+      default:{ // メンバ一覧、各種オプションの既定値、CSS/HTML定義
+        data: data, // {Object} 参加者情報
+        // メンバとして持つHTMLElementの定義
+        parent: parent, // {HTMLElement} 親要素(ラッパー)
+        parentSelector: null, // {string} 親要素(ラッパー)のCSSセレクタ
+        style: null,  // {HTMLStyleElement} CSS定義
+        summary: null, // {HTMLElement} 概要領域のDIV要素
+        list: null, // {HTMLElement} 参加者一覧領域のDIV要素
+        detail: null, // {HTMLElement} 詳細領域のDIV要素
+        buttons: null, // {HTMLElement} ボタン領域のDIV要素
+        // CSS/HTML定義
+        css:[
+          /* Perticipants共通部分 */ `
+          .Perticipants {
+            margin: 1rem;
+            width: calc(100% - 2rem);
+            display: grid;
+            row-gap: 1rem;
+            grid-template-columns: 1fr;
+          }
+          .Perticipants.hide {
+            display: none;
+          }
+          .Perticipants > div {
+            width: 100%;
+            display: grid;
+            gap: 1rem;
+          }
+          .Perticipants rt {
+            font-size: 50%;
+          }
+          .Perticipants .label {
+            margin-top: 1rem;
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 2rem;
+          }
+          .Perticipants .label p {
+            grid-column: 1 / 3;
+            font-size: 1.4rem;
+          }
+          .Perticipants .label button {
+            grid-column: 3 / 4;
+          }`,
+          /* 概要欄(QRコード、受付番号、申込者名) */`
+          .Perticipants .summary {
+            grid-template-columns: repeat(12, 1fr);
+          }
+          .Perticipants .summary [name="qrcode"]{
+            padding: 0rem;
+            grid-row: 1 / 3;
+            grid-column: 1 / 5;
+          }
+          .Perticipants .summary [name="entryStr"]{
+            grid-row: 1 / 2;
+            grid-column: 5 / 13;
+          }
+          .Perticipants .summary [name="entryStr"] span {
+            font-size: 2rem;
+          }
+          .Perticipants .summary [name="申込者氏名"]{
+            grid-row: 2 / 3;
+            grid-column: 5 / 13;
+          }
+          .Perticipants .summary ruby span {
+            font-size: 2rem;
+          }
+          .Perticipants .summary rt span {
+            font-size: 1rem;
+          }`,
+          /* 参加者一覧 */`
+          .Perticipants .list .label button.hide {
+            display: none;
+          }
+          .Perticipants .list .content {
+            width: 100%;
+            margin: 1rem 0px;
+            display: grid;
+            grid-template-columns: repeat(10, 1fr);
+            gap: 0.2rem;
+          }
+          .Perticipants .list .content.hide {
+            display: none;
+          }
+          .Perticipants .list .content div:nth-child(4n+1) {
+            grid-column: 1 / 2;
+          }
+          .Perticipants .list .content div:nth-child(4n+2) {
+            grid-column: 2 / 7;
+          }
+          .Perticipants .list .content div:nth-child(4n+3) {
+            grid-column: 7 / 9;
+          }
+          .Perticipants .list .content div:nth-child(4n+4) {
+            grid-column: 9 / 11;
+          }
+          .Perticipants .list .content .td[name="fee"] > .hide {
+            display: none;
+          }`,
+          /* 詳細情報 */`
+          .Perticipants .detail .content {
+            display: block;
+          }
+          .Perticipants .detail .content.hide {
+            display: none;
+          }
+          .Perticipants .detail .content .table {
+            width: 100%;
+            margin: 1rem 0px;
+            display: grid;
+            grid-template-columns: 2fr 3fr;
+            gap: 0.2rem;
+          }
+          .Perticipants .message {
+            display: block;
+          }
+          .Perticipants .message button {
+            margin-top: 1rem;
+            padding: 0.5rem 2rem;
+          }
+          .Perticipants .message.hide {
+            display: none;
+          }`,
+          /* 取消・決定・全員受領ボタン */`
+          .Perticipants .buttons {
+            width: 100%;
+            margin: 1rem 0px;
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 2rem;
+          }
+          .Perticipants .buttons.hide {
+            display: none;
+          }
+          .Perticipants .buttons button {
+            display: block;
+            width: 100%;
+            font-size: 2rem;
+          }
+          .Perticipants .buttons [name="取消"].hide {
+            display: none;
+          }
+          .Perticipants .buttons [name="決定"].hide {
+            display: none;
+          }
+          .Perticipants .buttons [name="全員"].hide {
+            display: none;
+          }`,
+        ],
+        html:[  // イベント定義を複数回行わないようにするため、eventで定義
+          {attr:{class:'Perticipants'},children:[
+            // 概要欄(QRコード、受付番号、申込者名)
+            {attr:{class:'summary'},children:[
+              {attr:{name:'qrcode'}},
+              {attr:{name:'entryStr'},text:'No.',children:[
+                {tag:'span',attr:{class:'v'}},
+              ]},
+              {attr:{name:'申込者氏名'},children:[
+                {tag:'ruby',children:[
+                  {tag:'span',attr:{class:'v'}},
+                  {tag:'rt',attr:{name:'申込者カナ'},children:[
+                    {tag:'span',attr:{class:'v'}}
+                  ]},
+                ]}
+              ]},
+            ]},
+            // 参加者一覧
+            {attr:{class:'list'},children:[
+              {attr:{class:'label'},children:[
+                {tag:'p',text:'参加者一覧'},
+                {tag:'button',text:'非表示',event:{click:this.toggle}},
+              ]},
+              {attr:{class:'content'},children:[
+                {attr:{class:'th'},text:'No'},
+                {attr:{class:'th'},text:'氏名'},
+                {attr:{class:'th'},text:'所属'},
+                {attr:{class:'th'},text:'参加費'},
+              ]},
+            ]},
+            // 詳細情報
+            {attr:{class:'detail'},children:[
+              {attr:{class:'label'},children:[
+                {tag:'p',text:'詳細情報'},
+                {tag:'button',text:'非表示',event:{click:this.toggle}},
+              ]},
+              {attr:{class:'content'},children:[
+                {attr:{class:'table'}},
+                {attr:{class:'message'},children:[
+                  {attr:{name:'editURL'},children:[
+                    {tag:'p',text:'参加者一覧・詳細情報に誤謬・変更がある場合、以下のボタンをクリックして申込フォームを開いて修正してください。'},
+                    {
+                      tag:'button',
+                      text:'申込フォームを開く',
+                      event:{click:()=>window.open(this.data.editURL,'_blank')},
+                    },
+                  ]}
+                ]},
+              ]},
+            ]},
+            // 取消・決定・全員受領ボタン
+            {attr:{class:'buttons'},children:[
+              {tag:'button',attr:{name:'取消'},text:'取消'},
+              {tag:'button',attr:{name:'決定'},text:'決定'},
+              {tag:'button',attr:{name:'全員'},html:'全員<br>受領'},
+            ]},
+          ]},
+        ],
+      },
+    };
+    console.log(v.whois+' start.',opt);
+    try {
+
+      v.step = 1; // メンバの値セット、HTML/CSSの生成
+      v.rv = setupInstance(this,opt,v.default);
+      if( v.rv instanceof Error ) throw v.rv;
+
+      v.step = 2; // メンバとして持つHTMLElementを設定
+      ['summary','list','detail','buttons'].forEach(x => {
+        this[x] = this.parent.querySelector('.'+x);
+      });
+
+      v.step = 3; // 参加者情報があればセット
+      if( this.data !== null ){
+        v.r = this.#setData();
+        if( v.r instanceof Error ) throw v.r;
+      }
+
+      v.step = 4; // 画面を非表示に
+      this.close();
+
+      v.step = 5; // 終了処理
+      console.log(v.whois+' normal end.',v.rv);
+      return v.rv;
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 参加者情報(this.data)を画面にセット
+   * @param {void}
+   * @returns {void}
+   */
+  #setData = () => {
+    const v = {whois:'Perticipants.#setData',rv:true,step:0};
+    console.log(v.whois+' start.');
+    try {
+
+      // ---------------------------------------------
+      // 1. QRコード、受付番号、申込者名
+      // ---------------------------------------------
+      v.step = 1.1; // QRコード表示
+      this.data.entryStr = String('0000'+this.data.entryNo).slice(-4);
+      v.qrcode = this.summary.querySelector('[name="qrcode"]');
+      v.qrSize = v.qrcode.clientWidth;
+      v.qrcode.innerHTML = '';  // 一度クリア
+      new QRCode(v.qrcode,{
+        text: this.data.entryStr,
+        width: v.qrSize,
+        height: v.qrSize,
+        colorDark: "#000",
+        colorLight: "#fff",
+        correctLevel : QRCode.CorrectLevel.H,
+      });
+  
+      v.step = 1.2; // その他文字情報表示
+      ['entryStr','申込者氏名','申込者カナ'].forEach(x => {
+        v.x = x;
+        v.element = this.summary.querySelector('[name="'+x+'"] .v');
+        v.element.innerText = this.data[v.x];
+      });
+
+      // ---------------------------------------------
+      // 2. 参加者一覧
+      // ---------------------------------------------
+      v.step = 2.1; // 参加者情報をセット
+      v.content = this.list.querySelector('.content');
+      // 過去の参加者情報をクリア
+      v.content.querySelectorAll('.td').forEach(x => x.remove());
+      for( v.i=1 ; v.i<6 ; v.i++ ){
+        v.prefix = '参加者0' + v.i;
+        // 氏名が未登録の場合はスキップ
+        if( this.data[v.prefix+'氏名'].length === 0 )
+          continue;
+        v.step = 2.2; // No
+        v.content.appendChild(createElement({
+          attr: {class:'td'},
+          style: {textAlign:'right'},
+          text: v.i,
+        }));
+        v.step = 2.3; // 氏名
+        v.content.appendChild(createElement(
+          {attr: {class:'td'},children:[
+            {tag:'ruby',text:this.data[v.prefix+'氏名'],children:[
+              {tag:'rt',text:this.data[v.prefix+'カナ']}
+        ]}]}));
+        v.step = 2.4; // 所属
+        v.content.appendChild(createElement({
+          attr: {class:'td'},
+          text: this.data[v.prefix+'所属'],
+        }));
+        v.step = 2.5; // 参加費
+        // 参加費欄はプルダウンとテキストと両方作成
+        v.options = [];
+        this.data['fee0'+v.i] = this.data['fee0'+v.i] || '未入場';
+        ['未入場','無料','未収','既収','退場済'].forEach(x => {
+          v.options.push({
+            tag:'option',
+            value:x,
+            text:x,
+            logical:{selected:(this.data['fee0'+v.i] === x)},
+          });
+        });
+        v.content.appendChild(createElement(
+          {attr:{class:'td',name:'fee'},children:[
+            {text: this.data['fee0'+v.i]},
+            {
+              tag     : 'select',
+              attr    : {class:'fee',name:'fee0'+v.i},
+              children: v.options
+            },
+          ]},
+        ));
+      }
+
+      // ---------------------------------------------
+      // 3. 詳細情報
+      // ---------------------------------------------
+      v.step = 3.1; // 詳細情報をセット
+      v.detail = this.detail.querySelector('.content .table');
+      v.detail.innerHTML = '';  // 過去の詳細情報をクリア
+      ["メールアドレス","申込者の参加","宿泊、テント","引取者氏名","緊急連絡先",
+      "ボランティア募集","キャンセル","備考"].forEach(x => {
+        v.detail.appendChild(createElement({
+          attr: {class:'th'},
+          text: x,
+        }));
+        v.detail.appendChild(createElement({
+          attr: {class:'td'},
+          text: this.data[x],
+        }));
+      });
+
+      // ---------------------------------------------
+      // 4. 取消・決定・全員受領ボタン
+      // ---------------------------------------------
+      v.step = 4.1; // ボタンを正方形に
+      this.buttons.querySelectorAll('button').forEach(x => {
+        x.style.height = x.clientWidth+'px';
+      });
+
+      console.log(v.whois+' normal end.');
+      return v.rv;
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 参加費の編集を行い、編集結果を返す
+   * @param {Object} [data=null] - 編集対象となる参加者情報 
+   * @returns {Object} {entryNo:{string},fee0n:'未入場/無料/未収/既収/退場済'}
+   */
+  edit = async (data=null) => {
+    const v = {whois:'Perticipants.edit',rv:true,step:0};
+    console.log(v.whois+' start.');
+    try {
+      // 親要素を表示
+      // ※display:noneのままだと内部要素のサイズが全て0に
+      this.open();
+
+      // 参加者情報を渡された場合、セット
+      if( data !== null ) this.data = data;
+
+      // 編集対象となる参加者情報を表示
+      v.rv = this.#setData();
+      if( v.rv instanceof Error ) throw v.rv;
+
+      // 参加者情報をボタンを削除(非表示)
+      this.list.querySelector('.label button').classList.add('hide');
+
+      // 参加者一覧・参加費欄でプルダウン以外は非表示
+      this.list.querySelectorAll('.content .td[name="fee"] div').forEach(x => {
+        x.classList.add('hide');
+      });
+
+      // 詳細情報を非表示状態に変更
+      v.rv = this.toggle('.detail',false);
+      if( v.rv instanceof Error ) throw v.rv;
+
+      // 取消・決定・全員収納ボタンを表示
+      return new Promise(resolve => {
+        // 取消 -> 戻り値はnull
+        this.buttons.querySelector('[name="取消"]')
+        .addEventListener('click',() => {
+          // 戻り値の作成
+          const rv = null;
+          // 終了処理
+          console.log('取消 -> '+JSON.stringify(rv)
+          + '\nPerticipants.edit normal end.\n');
+          this.close();
+          resolve({entryNo:this.data.entryNo,result:rv});
+        });
+
+        // 決定 -> {Object.<string, string>}、キャンセルなら{}
+        this.buttons.querySelector('[name="決定"]')
+        .addEventListener('click',() => {
+          // 戻り値の作成
+          let rv = {entryNo:this.data.entryNo};
+          this.list.querySelectorAll('.fee').forEach(x => {
+            rv[x.getAttribute('name')] = x.value;
+          });
+          /* 入力内容の確認
+          if( !window.confirm(
+            '参加費を画面のように登録します。\nよろしいですか？'
+          ) ) rv = {};*/
+          // 終了処理
+          console.log('決定 -> '+JSON.stringify(rv)
+          + '\nPerticipants.edit normal end.\n');
+          this.close();
+          resolve(rv);
+        });
+
+        // 全員受領 -> {Object.<string, string>}、キャンセルなら{}
+        this.buttons.querySelector('[name="全員"]')
+        .addEventListener('click',() => {
+          // 戻り値の作成
+          let rv = {entryNo:this.data.entryNo};
+          this.list.querySelectorAll('.fee').forEach(x => {
+            rv[x.getAttribute('name')] = '既収';
+          });
+          /* 入力内容の確認
+          if( !window.confirm(
+            '全参加者の参加費を「受領」にします。\nよろしいですか？'
+          ) ) rv = {};*/
+          // 終了処理
+          console.log('全員受領 -> '+JSON.stringify(rv)
+          + '\nPerticipants.edit normal end.\n');
+          this.close();
+          resolve(rv);
+        });
+      });
+
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 参加者情報を表示する
+   * @param {Object} [data=null] - 表示対象となる参加者情報 
+   * @returns {void}
+   */
+  view = (data=null) => {
+    const v = {whois:'Perticipants.view',rv:true,step:0};
+    console.log(v.whois+' start.');
+    try {
+      // 親要素を表示
+      // ※display:noneのままだと内部要素のサイズが全て0に
+      this.open();
+
+      // 参加者情報を渡された場合、セット
+      if( data !== null ) this.data = data;
+
+      // 編集対象となる参加者情報を表示
+      v.rv = this.#setData();
+      if( v.rv instanceof Error ) throw v.rv;
+
+      // 参加者一覧・参加費欄でプルダウンは非表示
+      this.list.querySelectorAll('.content .td[name="fee"] select').forEach(x => {
+        x.classList.add('hide');        
+      });
+
+      // 参加者一覧・詳細情報を非表示状態に変更
+      v.rv = this.toggle('.list',false);
+      v.rv = this.toggle('.detail',false);
+
+      // 取消・決定・全員収納ボタンは非表示
+      this.buttons.classList.add('hide');
+
+      console.log(v.whois+' normal end.',v.rv);
+      return v.rv;
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 表示/非表示ボタンクリック時の処理を定義
+   * @param {PointerEvent|string} event - クリック時のイベントまたはボタンのCSSセレクタ
+   * @param {boolean} show - trueなら開く
+   * @returns {void}
+   */
+  toggle = (event,show) => {
+    const v = {whois:'Perticipants.toggle',step:1,rv:null};
+    console.log(v.whois+' start.');
+    try {
+
+      let content;  // 表示/非表示を行う対象となる要素
+      let button;   // クリックされたボタンの要素
+      if( typeof event === 'string' ){
+        v.step = 1.1; // 初期設定時(引数がPointerEventではなくstring)
+        content = this.parent.querySelector(event+' .content');
+        button = this.parent.querySelector(event+' button');
+      } else {
+        v.step = 1.2; // ボタンクリック時
+        content = event.target.parentElement.parentElement
+        .querySelector('.content');
+        button = event.target;
+      }
+      console.log(content,button);
+
+      v.step = 2; // 表示->非表示 or 非表示->表示 を判断
+      let toOpen = show ? show : (button.innerText === '表示');
+      if( toOpen ){
+        v.step = 2.1; // 表示に変更する場合
+        button.innerText = '非表示';
+        content.classList.remove('hide');
+      } else {
+        v.step = 2.2; // 非表示に変更する場合
+        button.innerText = '表示';
+        content.classList.add('hide');
+      }
+
+      v.step = 3;
+      console.log(v.whois+' normal end.',v.rv);
+      return v.rv;
+
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
+
+  /** 親要素(parent)内を表示 */
+  open = () => {
+    //this.parent.classList.remove('hide');
+    this.parent.style.display = '';
+  }
+
+  /** 親要素(parent)内を隠蔽 */
+  close = () => {
+    //this.parent.classList.add('hide');
+    this.parent.style.display = 'none';
+  }
+}
---
+++ component/setupInstance/archives/20230902/core.js
@@ -0,0 +1,213 @@
+/** クラス(constructor)共通の初期処理を行う。
+ * 1. オプション・既定値をメンバに設定
+ * 1. オプション(Object)の第一階層にメンバ"css"が存在すると、新規styleを生成
+ * 1. 第一階層にメンバ"html"が存在すると、親要素内に指定のHTML要素を生成
+ * 1. 第一階層にメンバ"parent"が存在する場合、
+ *    - HTMLElement型ならそのまま`parent:(HTMLElement)}`として設定
+ *    - 文字列型ならCSSセレクタと解して`parent:{selector:(string),element:(HTMLElement)}`を生成
+ * 
+ * @param {Object} dest - 設定先のオブジェクト。初回呼出時はthis
+ * @param {Object} opt - 起動時にオプションとして渡されたオブジェクト
+ * @param {Object} def - 既定値のオブジェクト。初回呼出時はnull(内部定義を使用)
+ * @returns {void}
+ * 
+ * ## 使用方法
+ * 
+ * 1. 主にclass内constructorで冒頭に使用することを想定。
+ * 
+ * ### 入力例
+ * 
+ * ```
+ * const menu = new BurgerMenu({parent:'body',auth:confObj,fuga:{a:10}})
+ * 
+ * class BurgerMenu {
+ *   constructor(opt){
+ *     setupInstance(
+ *       this,  // 第一引数はthis(固定)
+ *       opt,   // 第二引数はconstructorに渡されたオプション
+ *       {      // 第三引数は既定値のオブジェクト
+ *         css: [  // このクラスに適用するCSS定義の配列
+ *           {
+ *             sel:'.date', // CSSセレクタ
+ *             prop:{       // 設定する値。ラベル・値ともクォーテーションで囲む
+ *               'margin-top':'1rem',
+ *               'padding-left': '1rem',
+ *             }
+ *           },
+ *         ],
+ *         parent: '',  // 親要素のCSSセレクタ or HTMLElementの既定値
+ *         auth: null,
+ *         hoge: 'Hello World',
+ *         fuga: {
+ *           a: null,
+ *           b: 20,
+ *         },
+ *       });
+ *     // 以降、constructorの処理
+ * ```
+ * 
+ * ### 出力例
+ * 
+ * ```
+ * this = {
+ *   parent: {
+ *     selector: 'body',  // optより導出
+ *     element : (body element),  // optより導出
+ *   },
+ *   auth: confObj, // optの値を設定
+ *   hoge: 'Hello World',  // 既定値を設定
+ *   fuga: {
+ *     a:10,  // optの値を設定
+ *     b:20,  // 既定値を設定
+ *   },
+ * }
+ * ```
+ * 
+ * ```
+ * <style type="text/css">
+ * .date {
+ *   margin-top: 1rem;
+ *   padding-left: 1rem;
+ * }
+ * </style>
+ * ```
+ * 
+ * ### デシジョンテーブル
+ * 
+ * | 優先(a) | 劣後(b) | 結果 | 備考 |
+ * | :--: | :--: | :--: | :-- |
+ * |  A  |  -  |  A  | 優先(A)のみ存在するメンバはそのまま |
+ * |  A  |  B  |  A  | |
+ * |  A  | [B] |  A  | |
+ * |  A  | {B} |  A  | |
+ * | [A] |  -  | [A] | |
+ * | [A] |  B  | [A] | |
+ * | [A] | [B] | [A+B] | 配列は置換ではなく結合。但し重複不可 |
+ * | [A] | {B} | [A] | |
+ * | {A} |  -  | {A} | |
+ * | {A} |  B  | {A} | |
+ * | {A} | [B] | {A} | |
+ * | {A} | {B} | {A+B} | オブジェクトも置換ではなく結合する |
+ * |  -  |  -  |  -  | |
+ * |  -  |  B  |  B  | |
+ * |  -  | [B] | [B] | |
+ * |  -  | {B} | {B} | |
+ * 
+ */
+const setupInstance = (dest,opt,def) => {
+  const v = {whois:'setupInstance',rv:true,step:0,
+    // 配列・オブジェクトの判定式
+    isObj: obj => obj && String(Object.prototype.toString.call(obj).slice(8,-1)) === 'Object',
+    isArr: obj => obj && String(Object.prototype.toString.call(obj).slice(8,-1)) === 'Array',
+    // ディープコピー。値の設定ロジックは上記デシジョンテーブル参照
+    deepcopy: (dest,opt) => {
+      Object.keys(opt).forEach(x => {
+        if( !dest.hasOwnProperty(x) ){
+          // コピー先に存在しなければ追加
+          dest[x] = opt[x];
+        } else {
+          if( v.isObj(dest[x]) && v.isObj(opt[x]) ){
+            // 両方オブジェクト -> メンバをマージ
+            v.deepcopy(dest[x],opt[x]);
+          } else if( v.isArr(dest[x]) && v.isArr(opt[x]) ){
+            // 両方配列 -> 配列をマージ
+            // Setで配列要素の重複を排除しているが、
+            // 配列要素が配列型・オブジェクト型の場合は重複する(中身もマージされない)
+            dest[x] = [...new Set([...dest[x],...opt[x]])];
+          } else {
+            // optの値でdestの値を置換
+            dest[x] = opt[x];
+          }
+        }
+      });
+    },
+    cssDefs: '',  // CSS定義文字列による指定の場合、その結合した文字列
+  };
+
+  console.log(v.whois+' start.',dest,opt,def);
+  try {
+
+    v.step = 1; // ディープコピー
+    dest = Object.assign(dest,def); // 既定値をセット
+    v.deepcopy(dest,opt);
+
+    v.step = 2; // parentの処理
+    if( dest.hasOwnProperty('parent') ){
+      if( typeof dest.parent === 'string' ){
+        // CSSセレクタだった場合
+        dest.parentSelector = dest.parent;
+        v.parent = dest.parent = document.querySelector(dest.parent);
+      } else if( dest.parent instanceof HTMLElement ){
+        // HTML要素だった場合
+        dest.parentSelector = null;
+        v.parent = dest.parent;
+      } else {
+        throw new Error(v.whois+': parent is not string or HTMLElement.');
+      }
+    }
+
+    v.step = 3; // CSS定義に基づき新たなstyleを生成
+    // style.nameに設定するため、呼出元のクラス名を取得
+    v.destName = dest.constructor.name || 'undef';
+    if( dest.hasOwnProperty('css') && // dest.cssがあり、未定義なら追加
+    document.head.querySelector('style[name="'+v.destName+'"]') === null ){
+      dest.style = createElement({
+        tag:'style',
+        attr:{type:'text/css',name:v.destName}
+      });
+      document.head.appendChild(dest.style);
+      for( v.i=0 ; v.i<dest.css.length ; v.i++ ){
+        v.x = dest.css[v.i];
+        if( v.isObj(v.x) ){
+          // {sel,prop}による指定の場合(将来的に廃止予定)
+          for( v.y in v.x.prop ){
+            v.prop = dest.parent.selector + ' ' + v.x.sel
+              + ' { ' + v.y + ' : ' + v.x.prop[v.y] + '; }\n';
+            dest.style.sheet.insertRule(v.prop,
+              dest.style.sheet.cssRules.length,
+            );
+          }
+        } else {
+          // CSS定義文字列による指定の場合
+          v.cssDefs = v.cssDefs + v.x;
+        }
+      }
+      if( v.cssDefs.length > 0 ){
+        dest.style.innerText = v.cssDefs.replaceAll(/\n/g,'').replaceAll(/\s+/g,' ');
+      }
+    }
+
+    v.step = 4; // HTML定義に基づき親要素内のHTML要素を生成
+    if( dest.hasOwnProperty('html') ){
+      if( v.isObj(dest.html) ){
+        v.step = 4.1; // オブジェクトならcreateElementの引数
+        v.html = createElement(dest.html);
+        if( v.html instanceof Error ) throw v.html;
+        v.parent.appendChild(v.html);
+      } else {
+        v.step = 4.2;
+        if( v.isArr(dest.html) ){
+          v.step = 4.21;
+          if( typeof dest.html[0] === 'string' ){
+            v.step = 4.211;
+            // 配列の最初の要素が文字列なら結合してinnerHTML
+            v.parent.innerHTML = dest.html.join('');
+          } else {
+            v.step = 4.212;
+            // オブジェクトならcreateElementして親要素に追加
+            dest.html.forEach(x => v.parent.appendChild(createElement(x)));
+          }
+        } else {
+          v.step = 4.22;  // 文字列ならinnerHTMLそのもの
+          v.parent.innerHTML = dest.html;
+        }
+      }
+    }
+
+    console.log(v.whois+' normal end.\n',dest);
+    return v.rv;
+  } catch(e){
+    console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+    return e;
+  }
+}
