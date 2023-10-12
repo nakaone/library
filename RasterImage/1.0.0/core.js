22514a9fa8bbd6236bd7e0b1299e28629c794782
nakaone <57969878+nakaone@users.noreply.github.com>
2023-10-12 18:35:42 +0900

    BasePageにLoadingIconのページを追加

--- RasterImage/1.0.0/core.js
+++ RasterImage/1.0.0/core.js
@@ -9,10 +9,11 @@ class BasePage {
    *  parentがHTMLElementだった場合は空文字列('')が設定される。
    * @prop {HTMLElement} wrapper - ページ表示領域
    * @prop {string} wrapperSelector - ページ表示領域に対するCSSセレクタ
-   * @prop {string|string[]} css - CSS定義。配列は途中にコメントを入れたい場合に使用
+   * @prop {string[]} css - CSS定義
    * @prop {string|CEDefObj|CEDefObj[]} html - HTML定義。
    *  文字列ならinnerHTMLそのもの、オブジェクトならcreateElementの引数と看做す
    * @prop {Object.<string, HTMLElement>} screenList - 複数画面を切り替える場合の画面名と要素
+   * @prop {string} home - ホーム画面名
    */
   /**
    * @constructor
@@ -39,27 +40,179 @@ class BasePage {
    *    this.wrapper内に指定のHTML要素を生成
    * 
    * ## 注意事項
+   * 
    * 1. 複数画面使用時、各画面はwrapper直下で定義し、メンバとして名前をつける<br>
    *    これにより画面切り替え(changeScreen)を行う
    * 
+   * ## 参考
+   * 
+   * - [ローディングアイコン集](https://projects.lukehaas.me/css-loaders/)
+   * - [CSSで全画面オーバーレイを実装する方法＆コード例](https://pisuke-code.com/css-fullscreen-overlay/)
    */
   constructor(def={},opt={}){
     const v = {whois:'BasePage.constructor',rv:null,step:0,
-      // 基底クラス(本クラス)の既定値
-      def:{parent:'body',wrapper:null,css:[],html:null,screenList:{}}
+      // def:基底クラス(本クラス)の既定値
+      def:{parent:'body',wrapper:null,screenList:{},
+        css: [
+          /* LoadingIcon共通部分 */ `
+          .LoadingIcon {
+            position: absolute;
+            left: 0; top: 0;
+            width: 100%; height:100%;
+            background: #fff;
+            z-index: 2147483647;
+            display: grid;
+            place-items: center;
+          }`,
+          /* pattern.5 */`
+          .loading5 {
+            --dot-size: 4rem;
+            --R: 0;
+            --G: 0;
+            --B: 0;
+            --back: rgba(var(--R),var(--G),var(--B),1);
+            --pale: rgba(var(--R),var(--G),var(--B),0.2);
+            --middle: rgba(var(--R),var(--G),var(--B),0.5);
+            --dark: rgba(var(--R),var(--G),var(--B),0.7);
+            --m0: calc(var(--dot-size) * 0.8); /* 軌道の大きさ */
+            --m1: calc(var(--m0) * -2.6);
+            --m2: calc(var(--m0) * -1.8);
+            --m3: calc(var(--m0) * 1.75);
+            --m4: calc(var(--m0) * 1.8);
+            --m5: calc(var(--m0) * 2.5);
+
+            margin: 100px auto;
+            font-size: 25px;
+            width: var(--dot-size);
+            height: var(--dot-size);
+            border-radius: 50%;
+            position: relative;
+            text-indent: -9999em;
+            -webkit-animation: load5 1.1s infinite ease;
+            animation: load5 1.1s infinite ease;
+            -webkit-transform: translateZ(0);
+            -ms-transform: translateZ(0);
+            transform: translateZ(0);
+          }
+          @-webkit-keyframes load5 {
+            0%,
+            100% {box-shadow:
+              0em var(--m1) 0em 0em var(--back),
+              var(--m4) var(--m2) 0 0em var(--pale),
+              var(--m5) 0em 0 0em var(--pale),
+              var(--m3) var(--m3) 0 0em var(--pale),
+              0em var(--m5) 0 0em var(--pale),
+              var(--m2) var(--m4) 0 0em var(--pale),
+              var(--m1) 0em 0 0em var(--middle),
+              var(--m2) var(--m2) 0 0em var(--dark);
+            }
+            12.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--dark), var(--m4) var(--m2) 0 0em var(--back), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--middle);
+            }
+            25% {
+              box-shadow: 0em var(--m1) 0em 0em var(--middle), var(--m4) var(--m2) 0 0em var(--dark), var(--m5) 0em 0 0em var(--back), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            37.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--middle), var(--m5) 0em 0 0em var(--dark), var(--m3) var(--m3) 0 0em var(--back), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            50% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--middle), var(--m3) var(--m3) 0 0em var(--dark), 0em var(--m5) 0 0em var(--back), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            62.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--middle), 0em var(--m5) 0 0em var(--dark), var(--m2) var(--m4) 0 0em var(--back), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            75% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--middle), var(--m2) var(--m4) 0 0em var(--dark), var(--m1) 0em 0 0em var(--back), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            87.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--middle), var(--m1) 0em 0 0em var(--dark), var(--m2) var(--m2) 0 0em var(--back);
+            }
+          }
+          @keyframes load5 {
+            0%,
+            100% {
+              box-shadow: 0em var(--m1) 0em 0em var(--back), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--middle), var(--m2) var(--m2) 0 0em var(--dark);
+            }
+            12.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--dark), var(--m4) var(--m2) 0 0em var(--back), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--middle);
+            }
+            25% {
+              box-shadow: 0em var(--m1) 0em 0em var(--middle), var(--m4) var(--m2) 0 0em var(--dark), var(--m5) 0em 0 0em var(--back), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            37.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--middle), var(--m5) 0em 0 0em var(--dark), var(--m3) var(--m3) 0 0em var(--back), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            50% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--middle), var(--m3) var(--m3) 0 0em var(--dark), 0em var(--m5) 0 0em var(--back), var(--m2) var(--m4) 0 0em var(--pale), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            62.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--middle), 0em var(--m5) 0 0em var(--dark), var(--m2) var(--m4) 0 0em var(--back), var(--m1) 0em 0 0em var(--pale), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            75% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--middle), var(--m2) var(--m4) 0 0em var(--dark), var(--m1) 0em 0 0em var(--back), var(--m2) var(--m2) 0 0em var(--pale);
+            }
+            87.5% {
+              box-shadow: 0em var(--m1) 0em 0em var(--pale), var(--m4) var(--m2) 0 0em var(--pale), var(--m5) 0em 0 0em var(--pale), var(--m3) var(--m3) 0 0em var(--pale), 0em var(--m5) 0 0em var(--pale), var(--m2) var(--m4) 0 0em var(--middle), var(--m1) 0em 0 0em var(--dark), var(--m2) var(--m2) 0 0em var(--back);
+            }
+            
+          }`,
+        ],
+        html: [
+          {attr:{class:'BasePage'},children:[
+            {attr:{class:'LoadingIcon'},name:'loading',children:[
+              {attr:{class:'loading5'},text:'loading...'}
+            ]},
+          ]},
+        ],
+      },
     };
     console.log(v.whois+' start.',def,opt);
     try {
-      v.step = 1; // オプション・既定値をメンバに設定
-      v.step = 1.1; // (継承先)クラス名をthis.classNameに保存
-      this.className = this.constructor.name;
-      v.step = 1.2; // 与えられたオプションをメンバに保存
-      this.deepcopy(this,v.def); // 基底クラス(本クラス)の既定値を設定
-      this.deepcopy(this,def); // 継承先クラスの既定値を設定
-      this.deepcopy(this,opt); // オプションで上書き
-      console.log("%s step.%s\n",v.whois,v.step,this);
 
-      v.step = 2; // parentの処理
+      // BasePageの既定値をメンバに保存し、特有のCSSを定義
+      v.step = 1.1; // 既定値をメンバに保存
+      this.deepcopy(this,v.def);
+      console.log("%s step.%s\n",v.whois,v.step,this.css);
+
+      v.step = 1.2; // BasePage用(=継承先クラス共通)のCSSを定義
+      // BasePageで作成するHTML,CSSは継承先クラス共通なので、
+      // HTMLはBODY直下に`<div name="BasePage">として作成、
+      // CSSはHEAD内部に`<style name="BasePage">`として作成し、
+      // 継承先インスタンス生成の都度存否確認し、存在しなければ生成する。
+      this.className = 'BasePage';
+      v.rv = this.setStyleSheet();
+      if( v.rv instanceof Error ) throw v.rv;
+      this.css = []; // CSSのBasePage特有部分は定義済なのでクリア
+
+      v.step = 1.3; // BasePage用(=継承先クラス共通)のHTMLを生成
+      if( document.querySelector('body > div.BasePage') === null ){
+        this.createElement(this.html,document.querySelector('body'));
+      }
+      this.html = []; // HTMLのBasePage特有部分は作成済なのでクリア
+
+
+      // 継承先クラスの既定値・オプションをメンバに保存
+      v.step = 2.1; // (継承先)クラス名をthis.classNameに保存
+      this.className = this.constructor.name;
+      v.step = 2.2; // def.css,htmlが配列でなかった場合は配列化
+      ['css','html'].forEach(x => {
+        if( def.hasOwnProperty(x) ){
+          // 存在するが配列でないならば配列化
+          if( !this.isArr(def[x]) ){
+            def[x] = [def[x]];
+          }
+        } else {  // 未定義なら空配列をセット
+          def[x] = [];
+        }
+      });
+      v.step = 2.3; // 継承先クラスの既定値を設定
+      this.deepcopy(this,def); 
+      console.log("%s step.%s\n",v.whois,v.step,this.css);
+      v.step = 2.4; // 継承先クラスのconstructorに渡されたオプションで上書き
+      this.deepcopy(this,opt);
+      console.log("%s step.%s\n",v.whois,v.step,this.screenList);
+
+      v.step = 3; // parentの処理
       if( typeof this.parent === 'string' ){
         // 文字列(CSSセレクタ)として渡された場合
         this.parentSelector = this.parent;
@@ -69,7 +222,7 @@ class BasePage {
         this.parentSelector = '';
       }
 
-      v.step = 3; // wrapperの処理
+      v.step = 4; // wrapperの処理
       if( this.wrapper === null ){
         this.wrapper = this.createElement(
           {attr:{name:'wrapper',class:this.className}},
@@ -79,31 +232,11 @@ class BasePage {
       = (this.parentSelector === null ? '' : this.parentSelector + ' > ')
       + 'div.' + this.className + '[name="wrapper"]';
 
-      v.step = 4; // CSS定義に基づき新たなstyleを生成
-      v.cssDefs = '';
-      // 既に他インスタンスで作成済みならスキップ
-      if( document.head.querySelector('style[name="'+this.className+'"]') === null ){
-        (this.isArr(this.css) ? this.css.join() : this.css)
-        .match(/[^\{\}]+?\{.+?\}/g).forEach(l => {
-          v.m = l.match(/\s*([^\{\}]+?)\s*\{(.+?)\}/);
-          v.selector = v.m[1].trim();
-          // CSSセレクタに継承先クラス名がない場合は追加
-          if( v.selector.indexOf(this.className) < 0 ){
-            v.selector = ' .' + this.className + ' ' + v.selector;
-          }
-          v.cssDefs += v.selector + ' {' + v.m[2] + '} ';
-        });
-        if( v.cssDefs.length > 0 ){
-          v.cssDefs = v.cssDefs.replaceAll(/\n/g,' ').replaceAll(/\s+/g,' ');
-          this.createElement({
-            tag:'style',
-            attr:{type:'text/css',name:this.className},
-            text:v.cssDefs
-          },document.head);
-        }
-      }
-  
-      v.step = 5; // HTML定義に基づき親要素内のHTML要素を生成
+      v.step = 5; // CSS定義に基づき新たなstyleを生成
+      v.rv = this.setStyleSheet();
+      if( v.rv instanceof Error ) throw v.rv;
+
+      v.step = 6; // HTML定義に基づき親要素内のHTML要素を生成
       if( this.html !== null ){
         if( typeof this.html === 'string' ){
           this.wrapper.innerHTML = this.html;
@@ -112,7 +245,11 @@ class BasePage {
         }
       }
   
-      v.step = 6; // 終了処理
+      v.step = 7; // ホーム画面に遷移
+      v.rv = this.changeScreen();
+      if( v.rv instanceof Error ) throw v.rv;
+
+      v.step = 8; // 終了処理
       console.log(v.whois+' normal end.\n',v.rv);
   
     } catch(e){
@@ -169,6 +306,8 @@ class BasePage {
             // Setで配列要素の重複を排除しているが、
             // 配列要素が配列型・オブジェクト型の場合は重複する(中身もマージされない)
             dest[x] = [...new Set([...dest[x],...opt[x]])];
+            //dest[x] = dest[x].concat(opt[x]);
+            //console.log(dest[x],opt[x]);
           } else {
             v.step = 5; // 両方オブジェクトでも両方配列でもない場合、optの値でdestの値を置換
             dest[x] = opt[x];
@@ -186,7 +325,7 @@ class BasePage {
     }
   }
 
-  /**
+  /** 
    * @typedef CEDefObj - createElementに渡すオブジェクト形式
    * @prop {string} [tag='div'] - タグ
    * @prop {Object.<string, string>} [attr={}] - タグの属性(ex.src, class)
@@ -197,6 +336,30 @@ class BasePage {
    * @prop {CEDefObj[]} [children=[]] - 子要素
    * @prop {string} [name=''] - クラスメンバにする場合、メンバ名となる文字列
    */
+
+  /** 表示する画面の切替
+   * @param {string} screenName - 表示する画面のcreateElementで指定したnameの値
+   * @returns {null|Error}
+   */
+  changeScreen = (screenName=this.home) => {
+    const v = {whois:'BasePage.changeScreen',rv:null,step:0};
+    console.log(v.whois+' start.',screenName);
+    try {
+
+      v.step = 1;
+      for( v.x in this.screenList ){
+        this[v.x].style.display = v.x === screenName ? '' : 'none';
+      }
+  
+      v.step = 2; // 終了処理
+      console.log(v.whois+' normal end.\n',v.rv);
+      return v.rv;
+  
+    } catch(e){
+      console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+      return e;
+    }
+  }
 
   /** HTMLElementを生成する
    * @param {CEDefObj|CEDefObj[]} arg - 生成するHTMLElementの定義
@@ -259,7 +422,7 @@ class BasePage {
 
         v.step = 9; // 子要素の追加(parentは指定しない)
         for( v.j=0 ; v.j<v.def.children.length ; v.j++ ){
-          v.obj.appendChild(createElement(v.def.children[v.j]));
+          v.obj.appendChild(this.createElement(v.def.children[v.j]));
         }
 
         v.step = 10; // 戻り値への登録
@@ -288,34 +451,77 @@ class BasePage {
     }
   }
 
-  /** 引数がオブジェクトか判定
-   * @param {any} obj - 判定対象
-   * @returns {boolean}
-   */
-  isObj = obj => obj && String(Object.prototype.toString.call(obj).slice(8,-1)) === 'Object';
-
   /** 引数が配列か判定
    * @param {any} obj - 判定対象
    * @returns {boolean}
    */
   isArr = obj => obj && String(Object.prototype.toString.call(obj).slice(8,-1)) === 'Array';
 
-  /** 表示する画面の切替
-   * @param {string} screenName - 表示する画面のcreateElementで指定したnameの値
+  /** 引数がオブジェクトか判定
+   * @param {any} obj - 判定対象
+   * @returns {boolean}
+   */
+  isObj = obj => obj && String(Object.prototype.toString.call(obj).slice(8,-1)) === 'Object';
+
+  /** 指定時間待機
+   * @param {number} sec - 待機時間(ミリ秒)
+   * @returns {void}
+   */
+  sleep = (sec) =>
+    {return new Promise(resolve => setTimeout(resolve,sec))};
+
+  /** this.cssからスタイルシートを追加作成
+   * @param {void}
    * @returns {null|Error}
    */
-  changeScreen = screenName => {
-    const v = {whois:'BasePage.changeScreen',rv:null,step:0};
-    console.log(v.whois+' start.',screenName);
+  setStyleSheet = () => {
+    const v = {whois:this.className+'.setStyleSheet',rv:null,step:0,cssDefs:''};
+    console.log(v.whois+' start.',this.css);
     try {
-
-      v.step = 1;
-      for( v.x in this.screenList ){
-        this[v.x].style.display = v.x === screenName ? '' : 'none';
-      }
   
-      v.step = 2; // 終了処理
-      console.log(v.whois+' normal end.\\n',v.rv);
+      v.step = 1; // CSS定義が空ならスキップ
+      if( this.css.length === 0 ){
+        console.log(v.whois+' normal end.(empty CSS)\n');
+        return v.rv;        
+      }
+
+      v.step = 2; // 既に他インスタンスで作成済みならスキップ
+      if( document.head.querySelector('style[name="'+this.className+'"]') !== null ){
+        console.log(v.whois+' normal end.(already exist)\n');
+        return v.rv;
+      }
+
+      v.step = 3; // CSS全文を一つのテキストにした上で`〜 {〜}`の部分を全て抽出
+      v.css = this.css.join('').replaceAll(/\n/g,' ').replaceAll(/\s+/g,' ');
+      console.log(this.css.join('').match(/[^\{\}]+?\{.+?\}/g));
+      v.css.match(/[^\{\}]+?\{.+?\}/g).forEach(l => {
+        v.step = 4; // `〜 {〜}`の部分からselectorと括弧内を抽出
+        v.m = l.match(/\s*([^\{\}]+?)\s*\{(.+?)\}/);
+        v.selector = v.m[1].trim();
+        if( v.selector.indexOf(this.className) < 0  // CSSセレクタに継承先クラス名がない
+         && v.selector.slice(0,1) !== '@' // CSSのアットルールに該当しない
+         && v.selector.match(/[0-9\.%]+/) === null ){  // animationの定義(比率)に該当しない
+          v.step = 5; // 先頭にクラス名をクラスとして追加(style scopedもどき)
+          v.selector = ' .' + this.className + ' ' + v.selector;
+        }
+        v.step = 6; // 作成するCSS本文(cssDefs)に追加
+        v.cssDefs += v.selector + ' {' + v.m[2] + '} ';
+      });
+
+      v.step = 7; // cssDefsの中身があれば定義
+      if( v.cssDefs.length > 0 ){
+        v.step = 8; // 余計な空白を削除
+        v.cssDefs = v.cssDefs.replaceAll(/\n/g,' ').replaceAll(/\s+/g,' ');
+        v.step = 9; // スタイルタグを作成して追加
+        this.createElement({
+          tag:'style',
+          attr:{type:'text/css',name:this.className},
+          text:v.cssDefs
+        },document.head);
+      }
+
+      v.step = 10; // 終了処理
+      console.log(v.whois+' normal end.\n',v.rv);
       return v.rv;
   
     } catch(e){
@@ -329,9 +535,9 @@ class RasterImage extends BasePage {
   constructor(opt){
     const v = {whois:'RasterImage.constructor',rv:null,step:0,def:{
       parent: 'body',
-      css: "#target {border: solid 5px #ccc; padding:2em; text-align:center;}",
+      css: ["#target {border: solid 5px #ccc; padding:2em; text-align:center;}"],
       html: [
-        {attr:{id:'target'},text:'ここに画像ファイルをドロップします。'},
+        {attr:{id:'target'},name:'dropImage',text:'ここに画像ファイルをドロップします。'},
         {tag:'img',attr:{id:'preview'}},
       ],
     }};
--- RasterImage/1.0.0/core.md
+++ RasterImage/1.0.0/core.md
@@ -9,7 +9,7 @@
 ## Typedefs
 
 <dl>
-<dt><a href="#BasePageOption">BasePageOption</a></dt>
+<dt><a href="#BasePageMembers">BasePageMembers</a></dt>
 <dd></dd>
 <dt><a href="#CEDefObj">CEDefObj</a></dt>
 <dd></dd>
@@ -28,6 +28,8 @@
     * [.createElement](#BasePage+createElement) ⇒ <code>HTMLElement</code> \| <code>Error</code>
     * [.isObj](#BasePage+isObj) ⇒ <code>boolean</code>
     * [.isArr](#BasePage+isArr) ⇒ <code>boolean</code>
+    * [.changeScreen](#BasePage+changeScreen) ⇒ <code>null</code> \| <code>Error</code>
+    * [.sleep](#BasePage+sleep) ⇒ <code>void</code>
 
 <a name="new_BasePage_new"></a>
 
@@ -48,7 +50,17 @@
 1. オプション(Object)の第一階層にメンバ"css"が存在する場合、
    呼出元クラスで作成されたスタイルシートが存在しないなら新規作成する
 1. オプション(Object)の第一階層にメンバ"html"が存在する場合、
-   this.wrapper内に指定のHTML要素を生成  
+   this.wrapper内に指定のHTML要素を生成
+
+## 注意事項
+
+1. 複数画面使用時、各画面はwrapper直下で定義し、メンバとして名前をつける<br>
+   これにより画面切り替え(changeScreen)を行う
+
+## 参考
+
+- [ローディングアイコン集](https://projects.lukehaas.me/css-loaders/)
+- [CSSで全画面オーバーレイを実装する方法＆コード例](https://pisuke-code.com/css-fullscreen-overlay/)  
 
 | Param | Type | Default | Description |
 | --- | --- | --- | --- |
@@ -121,16 +133,45 @@ HTMLElementを生成する
 | --- | --- | --- |
 | obj | <code>any</code> | 判定対象 |
 
-<a name="BasePageOption"></a>
+<a name="BasePage+changeScreen"></a>
 
-## BasePageOption
+### basePage.changeScreen ⇒ <code>null</code> \| <code>Error</code>
+表示する画面の切替
+
+**Kind**: instance property of [<code>BasePage</code>](#BasePage)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| screenName | <code>string</code> | 表示する画面のcreateElementで指定したnameの値 |
+
+<a name="BasePage+sleep"></a>
+
+### basePage.sleep ⇒ <code>void</code>
+指定時間待機
+
+**Kind**: instance property of [<code>BasePage</code>](#BasePage)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| sec | <code>number</code> | 待機時間(ミリ秒) |
+
+<a name="BasePageMembers"></a>
+
+## BasePageMembers
 **Kind**: global typedef  
 **Properties**
 
-| Name | Type | Description |
-| --- | --- | --- |
-| className | <code>string</code> | 継承先クラス名。自動設定 |
-| parent | <code>string</code> \| <code>HTMLElement</code> | ページ表示領域の親要素またはCSSセレクタ。  constructorでHTMLElementに変換される。 |
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+| className | <code>string</code> |  | 継承先クラス名。自動設定 |
+| parent | <code>string</code> \| <code>HTMLElement</code> | <code>&quot;&#x27;body&#x27;&quot;</code> | ページ表示領域の親要素またはCSSセレクタ。  constructorでHTMLElementに自動的に変換される。 |
+| parentSelector | <code>string</code> |  | ページ表示領域の親要素に対するCSSセレクタ。  parentがHTMLElementだった場合は空文字列('')が設定される。 |
+| wrapper | <code>HTMLElement</code> |  | ページ表示領域 |
+| wrapperSelector | <code>string</code> |  | ページ表示領域に対するCSSセレクタ |
+| css | <code>string</code> \| <code>Array.&lt;string&gt;</code> |  | CSS定義。配列は途中にコメントを入れたい場合に使用 |
+| html | <code>string</code> \| [<code>CEDefObj</code>](#CEDefObj) \| [<code>Array.&lt;CEDefObj&gt;</code>](#CEDefObj) |  | HTML定義。  文字列ならinnerHTMLそのもの、オブジェクトならcreateElementの引数と看做す |
+| screenList | <code>Object.&lt;string, HTMLElement&gt;</code> |  | 複数画面を切り替える場合の画面名と要素 |
+| home | <code>string</code> |  | ホーム画面名 |
 
 <a name="CEDefObj"></a>
 
@@ -147,4 +188,5 @@ HTMLElementを生成する
 | [text] | <code>string</code> | <code>&quot;&#x27;&#x27;&quot;</code> | innerTextにセットする文字列 |
 | [html] | <code>string</code> | <code>&quot;&#x27;&#x27;&quot;</code> | innerHTMLにセットする文字列 |
 | [children] | [<code>Array.&lt;CEDefObj&gt;</code>](#CEDefObj) | <code>[]</code> | 子要素 |
+| [name] | <code>string</code> | <code>&quot;&#x27;&#x27;&quot;</code> | クラスメンバにする場合、メンバ名となる文字列 |
 
