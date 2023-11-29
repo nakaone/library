33b5f2a6536cbb657b27f22514984d05c7cb3dd3
nakaone <57969878+nakaone@users.noreply.github.com>
2023-11-29 15:19:14 +0900

    create toLocale/1.0.0/core.js

---
+++ toLocale/1.0.0/core.js
@@ -0,0 +1,59 @@
+/** 日時を指定形式の文字列にして返す
+ * @param {Date} dObj - 日付型オブジェクト
+ * @param {string} [format='yyyy/MM/dd'] - 日時を指定する文字列。年:y,月:M,日:d,時:h,分:m,秒:s,ミリ秒:n
+ * @returns {string} 指定形式に変換された文字列。無効な日付なら長さ0の文字列
+ *
+ * @example
+ * ```
+ * "1965/9/5"[yy/MM/dd hh:mm:ss.nnn] ⇒ "65/09/05 00:00:00.000"
+ * "1965/9/5"[yyyy-MM-dd] ⇒ "1965-09-05"
+ * "1965/9/5"[hh:mm] ⇒ "00:00"
+ * "1977-03-04"[yy/MM/dd hh:mm:ss.nnn] ⇒ "77/03/04 09:00:00.000"
+ * "1977-03-04"[yyyy-MM-dd] ⇒ "1977-03-04"
+ * "1977-03-04"[hh:mm] ⇒ "09:00"
+ * 1688189258262[yy/MM/dd hh:mm:ss.nnn] ⇒ "23/07/01 14:27:38.262"
+ * 1688189258262[yyyy-MM-dd] ⇒ "2023-07-01"
+ * 1688189258262[hh:mm] ⇒ "14:27"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[yy/MM/dd hh:mm:ss.nnn] ⇒ "23/07/01 14:16:30.000"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[yyyy-MM-dd] ⇒ "2023-07-01"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[hh:mm] ⇒ "14:16"
+ * "12:34"[yy/MM/dd hh:mm:ss.nnn] ⇒ ""
+ * "12:34"[yyyy-MM-dd] ⇒ ""
+ * "12:34"[hh:mm] ⇒ ""
+ * ```
+ */
+
+function toLocale(dObj,format='yyyy/MM/dd'){
+  const v = {rv:format,l:{ // 地方時ベース
+    y: dObj.getFullYear(),
+    M: dObj.getMonth()+1,
+    d: dObj.getDate(),
+    h: dObj.getHours(),
+    m: dObj.getMinutes(),
+    s: dObj.getSeconds(),
+    n: dObj.getMilliseconds()
+  }};
+  try {
+
+    v.step = 1; // 無効な日付なら空文字列を返して終了
+    if( isNaN(dObj.getTime()) ) return '';
+
+    v.step = 2; // 日付文字列作成
+    for( v.x in v.l ){
+      v.m = v.rv.match(new RegExp(v.x+'+'));
+      if( v.m ){
+        v.str = v.m[0].length > 1
+          ? ('000'+v.l[v.x]).slice(-v.m[0].length)
+          : String(v.l[v.x]);
+        v.rv = v.rv.replace(v.m[0],v.str);
+      }
+    }
+
+    v.step = 3; // 終了処理
+    return v.rv;
+
+  } catch(e){
+    console.error(e,v);
+    return e;
+  }
+}
