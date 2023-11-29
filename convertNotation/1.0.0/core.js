645c381e08aebafe40f2d5e29a7a72f384c1674a
nakaone <57969878+nakaone@users.noreply.github.com>
2023-11-29 15:12:44 +0900

    create convertNotation/1.0.0/core.js

---
+++ convertNotation/1.0.0/core.js
@@ -0,0 +1,51 @@
+/** 列記号<->列番号(数値)の相互変換
+ * @param {string|number} arg - 列記号または列番号(自然数)
+ */
+function convertNotation(arg){
+  const v = {rv:null,map: new Map([
+    // 26進数 -> 列記号
+    ['0','A'],['1','B'],['2','C'],['3','D'],['4','E'],
+    ['5','F'],['6','G'],['7','H'],['8','I'],['9','J'],
+    ['a','K'],['b','L'],['c','M'],['d','N'],['e','O'],
+    ['f','P'],['g','Q'],['h','R'],['i','S'],['j','T'],
+    ['k','U'],['l','V'],['m','W'],['n','X'],['o','Y'],
+    ['p','Z'],
+    // 列記号 -> 26進数
+    ['A',1],['B',2],['C',3],['D',4],['E',5],
+    ['F',6],['G',7],['H',8],['I',9],['J',10],
+    ['K',11],['L',12],['M',13],['N',14],['O',15],
+    ['P',16],['Q',17],['R',18],['S',19],['T',20],
+    ['U',21],['V',22],['W',23],['X',24],['Y',25],
+    ['Z',26],
+  ])};
+  try {
+
+    if( typeof arg === 'number' ){
+      v.step = 1; // 数値の場合
+      // 1未満はエラー
+      if( arg < 1 ) throw new Error('"'+arg+'" is lower than 1.');
+      v.rv = '';
+      v.str = (arg-1).toString(26); // 26進数に変換
+      // 26進数 -> 列記号に変換
+      for( v.i=0 ; v.i<v.str.length ; v.i++ ){
+        v.rv += v.map.get(v.str.slice(v.i,v.i+1));
+      }
+    } else if( typeof arg === 'string' ){
+      v.step = 2; // 文字列の場合
+      arg = arg.toUpperCase();
+      v.rv = 0;
+      for( v.i=0 ; v.i<arg.length ; v.i++ )
+        v.rv = v.rv * 26 + v.map.get(arg.slice(v.i,v.i+1));
+    } else {
+      v.step = 3; // 数値でも文字列でもなければエラー
+      throw new Error('"'+JSON.stringify(arg)+'" is invalid argument.');
+    }
+
+    v.step = 4; // 終了処理
+    return v.rv;
+
+  } catch(e) {
+    e.message = 'convertNotation: ' + e.message;
+    return e;
+  }
+}
