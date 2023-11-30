2efe556dcc3ac5202e93c787d79a84da25ac54c4
nakaone <57969878+nakaone@users.noreply.github.com>
2023-11-30 10:07:26 +0900

    range2Tableのバグ修正

--- toolbox/2.1.0/toolbox.js
+++ toolbox/2.1.0/toolbox.js
@@ -1,19 +1,13 @@
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   var menu = ui.createMenu('道具箱')
-  .addItem("選択範囲をHTML化",menu01);
+  .addItem("選択範囲をHTML化","range2Table");
   // サブメニュー使用時はaddSubMenuを使用
   menu.addToUi();
 }
 
-/** メニュー選択時に実行する関数群 */
-function menu01(){
-  showSidebar('選択範囲をHTML化','range2Table');
-}
-
-/** サイドバー(ダイアログ)を開く */
-function showSidebar(dialogTitle,dialogName) {
-  const htmlOutput = HtmlService.createTemplateFromFile(dialogName).evaluate();
-  htmlOutput.setTitle(dialogTitle);
+function range2Table(){
+  const htmlOutput = HtmlService.createTemplateFromFile('range2Table').evaluate();
+  htmlOutput.setTitle('選択範囲をHTML化');
   SpreadsheetApp.getUi().showSidebar(htmlOutput);
 }
