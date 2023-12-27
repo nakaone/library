4d79b59fde3b3f540919945d014eb836a384733c
nakaone <57969878+nakaone@users.noreply.github.com>
2023-12-27 15:42:01 +0900

    crossTab: 初版作成

--- crossTab/1.0.0/core.js
+++ crossTab/1.0.0/core.js
@@ -1,25 +1,56 @@
-/** 累計を行う
+/** 単純テーブルをクロス集計に変換する
+ * @param {Object.<string, string>[]} data - 集計の基となるデータ
  * @param {string[]} yCols - 縦軸となる項目名の配列
- * @param {Object} xCols
- * @param {string} xCols.label - 横軸となる項目名
- * @param {string[]|number[]} xCols.list - 横軸となる項目の値の配列
- * @param {Function} func - 一つ前の値に対して行う関数
- * @returns 
+ * @param {string} xCol - 横軸となる項目名
+ * @param {string|string[]} val - 値としてほしい項目名の配列。
+ * @returns
  */
-function accumulate(yCols,xCol,func){
-  const v = {whois:'accumulate',rv:null,step:0};
+function crossTab(data,yCols,xCol,val){
+  const v = {whois:'crossTab',rv:[],step:0};
   //console.log(`${v.whois} start.`);
   try {
 
-    v.step = 9; // 終了処理
+    for( v.i=0 ; v.i<data.length ; v.i++ ){
+      v.step = 1.1; // 縦軸となる項目名を持つ行が登録済か検索
+      v.obj = v.rv.find(x => {
+        let rv = true;
+        yCols.forEach(y => {if(x[y] !== data[v.i][y]) rv=false});
+        return rv;
+      });
+
+      if( !v.obj ){
+        v.step = 1.2; // 未登録の場合は新規登録
+        v.obj = {};
+        // 縦軸となる項目名
+        yCols.forEach(y => v.obj[y] = data[v.i][y]);
+        // 横軸となる項目名をオブジェクトとして登録
+        v.obj[xCol] = {};
+        v.rv.push(v.obj);
+      }
+
+      v.step = 2; // 登録する値を作成
+      if( typeof val === 'string' ){
+        v.step = 2.1; // 項目が一つだけの場合
+        v.obj[xCol][data[v.i][xCol]] = data[v.i][val];
+      } else {
+        v.step = 2.2; // 複数項目の場合
+        v.val = {};
+        val.forEach(x => v.val[x] = data[v.i][x] || 0)
+        v.obj[xCol][data[v.i][xCol]] = v.val;
+      }
+    }
+
+    v.step = 3; // 終了処理
     //console.log(`${v.whois} normal end.`);
     return v.rv;
 
   } catch(e) {
     e.message = `\n${v.whois} abnormal end at step.${v.step}`
     + `\n${e.message}`
-    + `\njournalsRange=${journalsRange}`  // 引数
-    + `, accountsRange=${accountsRange}`;
+    + `\ndata=${JSON.stringify(data.slice(0,5))}`
+    + `\nyCols=${JSON.stringify(yCols)}`
+    + `\nxCol=${xCol}`
+    + `\nval=${JSON.stringify(val)}`;
     console.error(`${e.message}\nv=${JSON.stringify(v)}`);
     return e;
   }
