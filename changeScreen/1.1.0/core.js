f25318e58d69ec92ab27ea57eff3f8325b35df1e
nakaone <ena.kaon@gmail.com>
2024-03-12 08:45:47 +0900

    changeScreen: 1.1.0を作成

--- changeScreen/1.1.0/core.js
+++ changeScreen/1.1.0/core.js
@@ -10,17 +10,33 @@ function changeScreen(screenName){
   const v = {whois:'changeScreen',rv:null,step:0};
   try {
 
-    v.step = 1;
-    document.querySelectorAll('.screen').forEach(x => {
-      x.style.display = x.getAttribute('name') === screenName ? '' : 'none';
-    });
-  
-    v.step = 2; // 終了処理
+    v.step = 1; // 全スクリーンを閉じる
+    document.querySelectorAll('.screen').forEach(x => x.style.display = 'none');
+
+    v.step = 2; // 対象スクリーンを取得
+    v.screen = document.querySelector(`.screen[name="${screenName}"]`);
+
+    v.step = 3; // 祖先に遡ってスクリーンを表示
+    v.i = 100; // 永久ループ防止用
+    while( v.screen.tagName !== 'BODY' && v.i > 0){
+      if( v.screen.tagName === 'DIV' ){
+        v.screen.style.display = '';
+      }
+      v.screen = v.screen.parentNode;
+      v.i--;
+    }
+
+    v.step = 4; // 見つからなければエラー
+    if( v.i <= 0 ) throw new Error(`${screenName}が見つかりません`)
+
+    v.step = 5; // 終了処理
     return v.rv;
 
-  } catch(e){
-    console.error(v.whois+' abnormal end(step.'+v.step+').\n',e,v);
+  } catch(e) {
+    e.message = `${v.whois} abnormal end at step.${v.step}`
+    + `\n${e.message}`
+    + `\nscreenName=${stringify(screenName)}`;  // 引数
+    console.error(`${e.message}\nv=${stringify(v)}`);
     return e;
   }
-
 }
