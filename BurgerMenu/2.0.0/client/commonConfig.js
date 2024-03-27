aeb22ebb593c8517583066bc0e6adf2c3598e7f2
nakaone <ena.kaon@gmail.com>
2024-03-27 11:52:08 +0900

    Auth別途作成に方針変更、現状をコミット

--- BurgerMenu/2.0.0/client/commonConfig.js
+++ BurgerMenu/2.0.0/client/commonConfig.js
@@ -2,6 +2,106 @@ const config = (()=>{
   const common = {};  // client/server共通定義
 
   common.listSheetName = 'list';  // {string} 名簿シートのシート名
+  common.passcodeLength = 6; // {number} パスコードの長さ
+
+  // ------------------------------------------
+  // listシートの項目定義
+  // ------------------------------------------
+  common.cols = {
+    definition: {
+      id:{ // レコードのプライマリキー
+        label:'受付番号',  // {string} シート上の項目名。無指定ならメンバ名を流用
+        isValid: // {Arrow|Function} 正常性判定。引数は当該レコード(Object)、戻り値は真偽値
+          row=>{return (typeof row.id === 'number') && (isFinite(row.id))},
+        default: // {Arrow|Function} 新規レコード追加時の既定値。引数はシートデータ(Object[])
+          data=>{return Math.max(...data.map(x=>x.id))+1},
+      },
+      isTest:{  // デバッグ用のテストレコード識別用。テスト用ならtrue
+        isValid: row=> typeof row.isTest === 'boolean',
+        default: ()=>false,
+      },
+      created:{ // レコード生成日時
+        isValid: row=>{
+          let value=new Date(row.created).getTime();
+          return ((typeof value === 'number') && (isFinite(value)));
+        },
+        default:()=>Date.now(),
+      },
+      name:{  // 申込者名
+        label: '氏名',
+        isValid: row=>row.name.match(/\S+?[\s　]+.+/),
+        default: ()=>'',
+        form: {
+          desc: '姓と名の間にはスペースを入れてください',
+          required: true,
+          type: 'text',
+          width: '15rem',  
+        },
+      },
+      reading:{ // 申込者名読み
+        isValid: row=>row.reading.match(/[ァ-ヾ]+　+[ァ-ヾ　]+/) ? true : false,
+        default: ()=>'',
+        form: {
+          desc: '全角カナで、姓と名の間にはスペースを入れてください',
+          required: true,
+          type: 'text',
+          width: '15rem',
+        },
+      },
+      email:{ // 申込者のe-mail
+        isValid: row=>row.email.match(/[\w\-._]+@[\w\-._]+\.[A-Za-z]+/),
+        default: ()=>'',
+        form: {
+          required: true,
+        },
+      },
+      tel:{
+        isValid: row=>row.tel.match(/^\d{2,4}-?\d{2,4}-?\d{4}$/),
+        message: '区切り無し、またはハイフン区切りで入力してください',  // isValidでエラーになった場合のメッセージ
+        default: ()=>'',
+        form: {
+          required: false,
+          desc: '区切り無し、またはハイフン区切りで入力してください',
+          msg: 'ハイフンで区切って入力してください',
+        }
+      }
+    }
+  };
+  common.table = {}; // id,label両方から参照可能にしたObj
+  for( let x in common.cols.definition ){
+    let y = common.cols.definition[x];
+    if( !y.hasOwnProperty('label') ) y.label = x;
+    y.id = x;
+    common.table[x] = common.table[y.label] = y;
+  }
+/*
+id
+isTest : テスト用ならtrue
+timestamp : 生成日時
+name
+reading
+email
+tel
+authority : adminからシート上で付与された権限
+note : フォームから入力された備考
+status : {number} -1:無効(キャンセル済)、1:有効 
+trial : {
+  passcode : パスコードとして設定した6桁の数字
+  created : パスコード生成日時(UNIX時刻)
+  log :[ 直近のパスコード検証履歴
+    timestamp : 入力日時(UNIX時刻)
+    value : 入力されたパスコード
+    msg : エラーメッセージ(不成功となった理由)。nullなら成功
+  ]
+}
+memo : シートで入力した内部用備考
+
+
+publicKey
+keyCreated
+certificate : 判定日時
+*/
+
 
   // ------------------------------------------
   // アクセス権限
--- BurgerMenu/2.0.0/client/setProperties.js
+++ BurgerMenu/2.0.0/client/setProperties.js
@@ -3,5 +3,29 @@
  * @returns {null|Error}
  */
 setProperties(arg){
-  console.log(arg);
+  const v = {whois:this.className+'.setProperties',rv:null,step:0};
+  console.log(`${v.whois} start.`);
+  try {
+
+    v.step = 1; // commonConfigとclientConfigをマージ、既定値作成
+    //::clientConfig.js::
+    //::commonConfig.js::
+
+    // 既定値とconstructorの引数をマージ
+
+    // commonConfigでの導出項目の計算
+    // clientConfigでの導出項目の計算
+    // メンバとして登録
+
+    v.step = 9; // 終了処理
+    console.log(`${v.whois} normal end.`);
+    return v.rv;
+
+  } catch(e) {
+    e.message = `${v.whois} abnormal end at step.${v.step}`
+    + `\n${e.message}`
+    + `\narg=${stringify(arg)}`;  // 引数
+    console.error(`${e.message}\nv=${stringify(v)}`);
+    return e;
+  }
 }
