fc91d8c7c655068009013b004b6d9cc28b08af29
nakaone <ena.kaon@gmail.com>
2024-03-29 16:31:33 +0900

    embedRecursively: 初版作成

---
+++ embedRecursively/1.0.0/build.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# -x  つけるとverbose
+# created with reference to embedRecursively/2.0.0/build.sh, BurgerMenu/2.0.0/build.sh, camp2024/client/build.sh
+set -e # エラー時点で停止
+
+# ----------------------------------------------
+# 1.事前準備
+# ----------------------------------------------
+hr="\n=======================================\n"
+echo "\n$hr[embedRecursively] build start$hr"
+
+# 1.1 変数・ツールの定義
+echo "step.1.1 start."
+GitHub="/Users/ena.kaon/Desktop/GitHub"
+lib="$GitHub/library"
+mod="$lib/embedRecursively/1.0.0"
+esed="node $lib/esed/1.0.0/core.js"
+querySelector="node $lib/querySelector/2.0.0/core.js"
+tmp="$mod/tmp"; rm -rf $tmp/*
+#w01="$tmp/work01";w02="$tmp/work02";w03="$tmp/work03"
+
+# 1.2 .DS_storeの全削除
+echo "step.1.2 start."
+cd $mod
+find . -name '.DS_Store' -type f -ls -delete
+
+
+# ----------------------------------------------
+# 2. pipe.jsの生成
+# ----------------------------------------------
+# 2.1 pipe処理部分の記述
+cat << EOS > $mod/pipe.js
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lines = []; ; //標準入力から受け取ったデータを格納する配列
+var reader = require('readline').createInterface({　//readlineという機能を用いて標準入力からデータを受け取る
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', line => lines.push(line));
+reader.on('close', () => {
+  console.log(embedRecursively(lines.join('\n'),analyzeArg().opt));
+});
+
+const log = (arg) => console.log(arg); // テスト用
+EOS
+# 2.2 embedRecursively(core.js)
+cat $mod/core.js | awk 1 \
+| $esed -x:" *console.log.+\n" -s:"" >> $mod/pipe.js
+# 2.3 その他ライブラリ
+cat $lib/analyzeArg/1.1.0/core.js | awk 1 \
+| $esed -x:" *console.log.+\n" -s:"" >> $mod/pipe.js
+cat $lib/stringify/1.1.1/core.js | awk 1 >> $mod/pipe.js
+cat $lib/whichType/1.0.1/core.js | awk 1 \
+| $esed -x:" *console.log.+\n" -s:"" >> $mod/pipe.js
+
+
+# ----------------------------------------------
+# ----------------------------------------------
+jsdoc2md $mod/core.js > $tmp/jsdoc.md
+cat << EOS > $tmp/source.md
+\`\`\`
+`cat $mod/core.js | awk 1`
+\`\`\`
+EOS
+cat $mod/proto.md | awk 1 \
+| $esed -x:"\/\*::CSS/1.3.0/core.css::\*\/" -f:$lib/CSS/1.3.0/core.css \
+| $esed -x:"<!--::JSDoc.md::-->" -f:$tmp/jsdoc.md \
+| $esed -x:"<!--::source.md::-->" -f:$tmp/source.md \
+> $mod/readme.md
+#| node $mod/pipe.js -library:"$lib" -tmp:"$tmp" > $mod/readme.md
+
+echo "\n$hr[embedRecursively] build end$hr"
---
+++ embedRecursively/1.0.0/core.js
@@ -0,0 +1,149 @@
+/** 文書内の挿入指示文字列を指示ファイルの内容で置換
+ * @param {string} arg - 処理対象テキスト
+ * @param {Object.<string:string>} opt - 「::〜::」で指定されるパス名内の変数'$xxx'を置換
+ * @param {number} [opt.maxDepth=10] - 最深階層(最大ループ回数)
+ * @param {string} [opt.encoding='utf-8'] - 入力ファイルのエンコード
+ * @returns {string}
+ * 
+ * @example
+ * 
+ * - 入力内容内の挿入指示文字列
+ *   - 「::(パス)::」 ⇒ 該当部分をパスで指定されたファイルの内容で置換
+ *   - 「::(タイトル)::(パス)::」 ⇒ 同上。タイトルはメモとして無視される
+ *   - 「::(>タイトル)::(パス)::」 ⇒ '>'を除いたタイトルをh1として追加
+ * - 読み込まれた文書は一つレベルが下がる(# -> ##)
+ * 
+ * #### 入力(proto.md)
+ * 
+ * ```
+ * # 開発用メモ
+ * 
+ * <!--::フォルダ構成::$test/folder.md::-->
+ * 
+ * <!--::>プログラムソース::$test/source.txt::-->
+ * ```
+ * 
+ * #### 被参照ファイル①：./test/folder.md
+ * 
+ * ```md
+ * # フォルダ構成
+ * - client/ : client(index.html)関係のソース
+ *   - commonConfig.js : client/server共通config
+ * ```
+ * 
+ * #### 被参照ファイル②：./test/source.js
+ * 
+ * ```javascript
+ * function embedRecursively(arg,opt={}){
+ *   const v = {whois:'embedRecursively',rv:arg,step:0,level:1,fs:require('fs')};
+ * (後略)
+ * ```
+ * 
+ * #### 実行するコマンド
+ * 
+ * ```bash
+ * cat << EOS > ./test/source.txt
+ * \`\`\`
+ * `cat ./test/source.js`
+ * \`\`\`
+ * EOS
+ * cat proto.md | awk 1 | node pipe.js -test:"./test"
+ * ```
+ * 
+ * #### 結果
+ * 
+ * ```
+ * # 開発用メモ
+ * 
+ * ## フォルダ構成
+ * - client/ : client(index.html)関係のソース
+ *   - commonConfig.js : client/server共通config
+ * 
+ * ## プログラムソース
+ * 
+ * \`\`\`
+ * function embedRecursively(arg,opt={}){
+ *   const v = {whois:'embedRecursively',rv:arg,step:0,level:1,fs:require('fs')};
+ * (後略)
+ * \`\`\`
+ * ```
+ */
+function embedRecursively(arg,opt={}){
+  const v = {whois:'embedRecursively',rv:arg,step:0,level:1,fs:require('fs')};
+  console.log(`${v.whois} start.\nopt=${stringify(opt)}`);
+  try {
+
+    v.step = 1.1; // 置換対象文字列の正規表現定義
+    v.rex = /(<!--|\/\/|\/\*)::(.+)::.*/;
+    v.step = 1.2; // 既定値の設定
+    if( !opt.hasOwnProperty('maxDepth') ) opt.maxDepth = 10;
+    if( !opt.hasOwnProperty('encoding') ) opt.encoding = 'utf-8';
+
+    //log(`v.rex=${v.rex}\nmatch=${stringify(v.rv.match(v.rex))}\nv.rv=${v.rv}`)
+    while( v.rv.match(v.rex) && v.level < opt.maxDepth ){
+      v.step = 2; // 行単位に分割
+      v.arr = v.rv.split('\n');
+      for( v.i=0 ; v.i<v.arr.length ; v.i++ ){
+
+        v.step = 3; // 「::〜::」が存在するか確認、無ければスキップ
+        v.m0 = v.arr[v.i].match(v.rex);
+        if( v.m0 === null ) continue;
+
+        v.msg = `v.arr[${v.i}]=${v.arr[v.i]}\nv.m0=${stringify(v.m0)}\n`;
+
+        v.step = 4; // 「::パス名::」か「::タイトル::パス名::」か判断
+        v.m1 = v.m0[2].match(/^(.+?)::(.+)$/);
+        v.msg += `v.m1=${stringify(v.m1)}\n`;
+        v.title = v.m1 ? v.m1[1] : '';
+        v.path = v.m1 ? v.m1[2] : v.m0[2];
+        v.msg += `v.title=${v.title}\nv.path=${v.path}\n`;
+
+        v.step = 5; // パスの中の変数を実値に置換
+        v.m2 = v.path.match(/\$(.+?)\//g);
+        v.msg += `v.m2=${stringify(v.m2)}\n`;
+        if( v.m2 ){
+          v.m2.forEach(x => {
+            let y = x.replaceAll(/\$/g,'').replaceAll(/\//g,'');
+            v.path = v.path.replace(x,opt[y]+'/');
+            v.msg += `x=${x}, y=${y}, path=${v.path}\n`;
+          });
+        }
+
+        v.step = 6; // 置換結果となる文字列をファイルから読み込み
+        v.after = v.fs.readFileSync(v.path,opt.encoding);
+
+        v.step = 7; // タイトルの文頭に'>'が有れば、置換後文字列にh1として追加
+        v.m3 = v.title.match(/^>(.+)$/)
+        if( v.m3 ){
+          v.after = `# ${v.m3[1]}\n\n${v.after}`;
+        }
+
+        v.step = 8; // 再帰呼出の場合、その分タイトルのレベルを下げる
+        v.lines = v.after.split('\n');
+        for( v.j=0 ; v.j<v.lines.length ; v.j++ ){
+          v.lines[v.j] = v.lines[v.j].replaceAll(/^#/g,'#'.repeat(v.level));
+        }
+        v.after = v.lines.join('\n');
+
+        v.step = 9; // 「::〜::」部分を置換後文字列に置換
+        v.arr[v.i] = v.arr[v.i].replace(v.m0[0],v.after);
+        v.msg += `v.arr[${v.i}]=${v.arr[v.i]}`;
+
+        //log(`${v.msg}\n`);
+      }
+      v.rv = v.arr.join('\n');
+      v.level++;
+    }
+
+    v.step = 9; // 終了処理
+    console.log(`${v.whois} normal end.`);
+    return v.rv;
+
+  } catch(e) {
+    e.message = `${v.whois} abnormal end at step.${v.step}`
+    + `\n${e.message}`
+    + `\narg=${stringify(arg)}\nopt=${stringify(opt)}`;
+    console.error(`${e.message}\nv=${stringify(v)}`);
+    return e;
+  }
+}
