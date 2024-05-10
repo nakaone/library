894ca50333c8afbfd1b23b50c01c69cfc5dbd8a0
nakaone <ena.kaon@gmail.com>
2024-05-10 11:47:36 +0900

    SingleTable : build時のパス指定、メッセージ表示を修正

--- SingleTable/1.2.0/build.sh
+++ SingleTable/1.2.0/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # -x  つけるとverbose
 
-echo "\n\n\n\n\nbuild.sh start"
+echo "\n\n\n\n\nSingleTable build.sh start"
 
 # .DS_storeの全削除
 find .. -name '.DS_Store' -type f -ls -delete
 
+echo "`date +"%T"` - SingleTable: step.1 start."
 lib="/Users/ena.kaon/Desktop/GitHub/library"
 nised="$lib/nised/1.0.0/pipe.cjs"
 mod="$lib/SingleTable/1.2.0"
@@ -16,6 +17,7 @@ mkdir $tmp
 # ----------------------------------------------
 # core.jsの作成
 # ----------------------------------------------
+echo "`date +"%T"` - SingleTable: step.2.1 start."
 methods=( # type=gsは複数なのでループ処理
   constructor
   prepSheet
@@ -28,9 +30,10 @@ methods=( # type=gsは複数なのでループ処理
 )
 cp $mod/proto/core.js $tmp/before.js
 for method in ${methods[@]}; do
+  echo "`date +"%T"` - SingleTable: step.2.2 [$method] start."
   # 各メソッドのソースをインデント
   #   ※VSCodeで編集するため、メソッドも行頭から記述しているため
-  sed 's/^/  /g' $method/core.js > $tmp/$method.js
+  sed 's/^/  /g' $mod/$method/core.js > $tmp/$method.js
   # プロトタイプ(proto/core.js)にメソッドを追加
   cat $tmp/before.js \
   | node $nised -f:"  // ::$method::" -r:$tmp/$method.js \
@@ -44,8 +47,11 @@ cp $tmp/after.js $mod/core.js
 # -------------------------------------------
 # readme.mdの作成
 # -------------------------------------------
+echo "`date +"%T"` - SingleTable: step.3 start."
 jsdoc2md $mod/core.js > $tmp/core.md
 cat $mod/proto/readme.md \
 | node $nised -f:"__JSDoc" -r:$tmp/core.md \
 | node $nised -f:"__source" -r:$mod/core.js \
 > $mod/../readme.md
+
+echo "`date +"%T"` - SingleTable: end."
