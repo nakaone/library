d24f982169bcae8f1d25da221d8a039365c0ae3d
nakaone <ena.kaon@gmail.com>
2024-07-09 10:25:58 +0900

    toLocale 1.1.0を作成

---
+++ toLocale/1.1.0/core.js
@@ -0,0 +1,70 @@
+/** 日時を指定形式の文字列にして返す
+ * @param {string|Date} arg - 変換元の日時
+ * @param {string} [format='yyyy-MM-ddThh:mm:ss.nnnZ'] - 日時を指定する文字列。年:y,月:M,日:d,時:h,分:m,秒:s,ミリ秒:n,タイムゾーン:Z
+ * @returns {string} 指定形式に変換された文字列。無効な日付なら長さ0の文字列
+ *
+ * @example
+ * ```
+ * "1965/9/5"[yy/MM/dd hh:mm:ss.nnn] ⇒ "65/09/05 00:00:00.000"
+ * "1965/9/5"[yyyy-MM-dd] ⇒ "1965-09-05"
+ * "1965/9/5"[hh:mm] ⇒ "00:00"
+ * "1977-03-04"[yy/MM/dd hh:mm:ss.nnn] ⇒ "77/03/04 09:00:00.000"
+ * "1977-03-04"[yyyy-MM-dd] ⇒ "1977-03-04"
+ * "1977-03-04"[hh:mm] ⇒ "09:00"
+ * 1688189258262[yy/MM/dd hh:mm:ss.nnn] ⇒ "23/07/01 14:27:38.262"
+ * 1688189258262[yyyy-MM-dd] ⇒ "2023-07-01"
+ * 1688189258262[hh:mm] ⇒ "14:27"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[yy/MM/dd hh:mm:ss.nnn] ⇒ "23/07/01 14:16:30.000"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[yyyy-MM-dd] ⇒ "2023-07-01"
+ * "Sat Jul 01 2023 14:16:30 GMT+0900"[hh:mm] ⇒ "14:16"
+ * "12:34"[yy/MM/dd hh:mm:ss.nnn] ⇒ ""
+ * "12:34"[yyyy-MM-dd] ⇒ ""
+ * "12:34"[hh:mm] ⇒ ""
+ * ```
+ */
+
+function toLocale(arg,format='yyyy-MM-ddThh:mm:ss.nnnZ'){
+  const v = {rv:format};
+  try {
+
+    dObj = String(Object.prototype.toString.call(arg).slice(8,-1)) !== 'Date'
+    ? arg : new Date(arg);
+
+    v.step = 1; // 無効な日付なら空文字列を返して終了
+    if( isNaN(dObj.getTime()) ) return '';
+
+    v.local = { // 地方時ベース
+      y: dObj.getFullYear(),
+      M: dObj.getMonth()+1,
+      d: dObj.getDate(),
+      h: dObj.getHours(),
+      m: dObj.getMinutes(),
+      s: dObj.getSeconds(),
+      n: dObj.getMilliseconds(),
+      Z: Math.abs(dObj.getTimezoneOffset())
+    }
+    // タイムゾーン文字列の作成
+    v.local.Z = v.local.Z === 0 ? 'Z'
+    : ((dObj.getTimezoneOffset() < 0 ? '+' : '-')
+    + ('0' + Math.floor(v.local.Z / 60)).slice(-2)
+    + ':' + ('0' + (v.local.Z % 60)).slice(-2));
+
+    v.step = 2; // 日付文字列作成
+    for( v.x in v.local ){
+      v.m = v.rv.match(new RegExp(v.x+'+'));
+      if( v.m ){
+        v.str = v.m[0].length > 1
+          ? ('000'+v.local[v.x]).slice(-v.m[0].length)
+          : String(v.local[v.x]);
+        v.rv = v.rv.replace(v.m[0],v.str);
+      }
+    }
+
+    v.step = 3; // 終了処理
+    return v.rv;
+
+  } catch(e){
+    console.error(e,v);
+    return e;
+  }
+}
