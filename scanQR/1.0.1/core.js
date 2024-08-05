f583fa4d6b7e063b292750f7727c153407a4b01b
nakaone <ena.kaon@gmail.com>
2024-08-05 12:27:01 +0900

    Update core.js

--- scanQR/1.0.1/core.js
+++ scanQR/1.0.1/core.js
@@ -1,4 +1,5 @@
 /** QRコードをスキャン
+ * なお縦横いずれでも汎用的に使えるよう、撮像領域(ファインダ)は正方形とする
  * @param {string} parent - 親要素のCSSセレクタ
  * @param {Object} [opt={}] - オプション
  * @param {number} opt.size - ファインダ領域のサイズ
@@ -9,20 +10,9 @@
  *
  * - Qiita [html＋javascriptだけで実装したシンプルなQRコードリーダー](https://qiita.com/murasuke/items/c16e4f15ac4436ed2744)
  * - [Promiseでsleep機能を作る](https://www.sejuku.net/blog/24629#index_id5)
- *
- * @typedef scanQRopt
- * @prop {number} [size=500] - ファインダのサイズ
  */
 async function scanQR(parent,opt={}){
   const v = {whois:'scanQR',rv:null,
-    constraints:{
-      audio: false,
-      video: {
-        facingMode: 'environment',
-        //width: opt.size || 500,
-        //height: opt.size || 500,
-      },
-    },
     max: opt.max || 90000, // 最大待機時間。単位：ミリ秒
     interval: opt.interval || 300, // 撮像間隔。ミリ秒
     minSize : opt.minSize || 640, // 最小撮像サイズ。px
@@ -32,39 +22,35 @@ async function scanQR(parent,opt={}){
   console.log(`${v.whois} start.\nparent=${parent}\nopt=${stringify(opt)}`);
   try {
 
-    // ファインダ領域の作成
-    v.step = '1.1'; // 親要素の高さを最大に
+    v.step = 1; // 親要素のサイズ設定、取得
+    // 親要素の幅を最大に、高さを幅に合わせる
     v.parent = document.querySelector(parent);
-    v.parent.style.width = v.parent.style.height = '100%';
-    v.step = '1.2'; // 撮像領域を親要素の幅・高さ、いずれか小さい方の正方形に設定
-    if( opt.hasOwnProperty('size') ){
-      v.ps = opt.size;
-    } else {
-      v.pw = v.parent.clientWidth;
-      v.ph = v.parent.clientHeight;
-      if( v.pw < v.ph ){
-        v.ps = v.pw < v.minSize ? v.minSize : v.pw;
-        v.parent.style.height = v.ps + 'px';
-      } else {
-        v.ps = v.ph < v.minSize ? v.minSize : v.ph;
-        v.parent.style.width = v.ps + 'px';
-      }
-    }
-    v.constraints.video.width = v.constraints.video.height = v.ps;
-    v.step = '1.3'; // video要素の生成
-    v.video = createElement({tag:'video',style:{margin:'auto'}},v.video);
+    v.parent.style.width = '100%';
+    opt.size = opt.size || v.parent.clientWidth;
+    v.szNum = opt.size < v.parent.clientWidth // 数値単位の撮像領域幅。ex.980
+      ? opt.size : v.parent.clientWidth;
+    v.szStr = v.parent.style.height = v.szNum + 'px'; // 文字列型の撮像領域幅。ex.980px
 
-    v.step = '2';
+    v.step = 2; // video要素の生成
+    v.video = createElement({tag:'video',style:{width:v.szStr,height:v.szStr}},v.parent);
+    v.constraints = {
+      audio: false, // 音声は使用しない
+      video: {
+        //facingMode: 'environment',
+        width: v.szNum,
+        height: v.szNum,
+      },
+    };
     const stream = await navigator.mediaDevices.getUserMedia(v.constraints);
     v.video.srcObject = stream;
     v.video.play();
 
-    v.step = '3';
+    v.step = 3; // canvas要素の生成
     const { width, height } = v.constraints.video;
     v.canvas = new OffscreenCanvas(width, height);
     const context = v.canvas.getContext('2d');
 
-    v.step = '4'; // 定期的にスキャン実行
+    v.step = 4; // 定期的なスキャン実行
     v.cnt = 0;
     do {
       context.drawImage(v.video, 0, 0, width, height);
