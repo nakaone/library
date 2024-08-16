b0929197c4ac3d133c1afd817e0bfd9da3b67bd6
nakaone <ena.kaon@gmail.com>
2024-08-16 15:43:48 +0900

    assignVariables: 1.1.0実装

--- assignVariables/1.1.0/core.js
+++ assignVariables/1.1.0/core.js
@@ -3,15 +3,17 @@
  * @example
  * 
  * ```
- * <a data-assing="href:refURL,innerText:title"></a>
- * <img data-assing="src:refQR" />
+ * <a data-assing="href:public.refURL,innerText:title"></a>
+ * <img data-assing="src:public.refQR" />
  * 
  * let r = assignVariables({
  *   attribute: 'data-assign',
  *   data: {
- *     refURL : 'https://〜',
  *     title  : '参考資料',
- *     refQR  : 'https://api.qrserver.com/〜?data=' + 'https://〜',
+ *     public: {
+ *       refURL : 'https://〜',
+ *       refQR  : 'https://api.qrserver.com/〜?data=' + 'https://〜',
+ *     }
  *   },
  * });
  * 
@@ -23,27 +25,41 @@
  * @param {string} [arg.parent='body'] - 対象範囲となる包摂要素のCSSセレクタ
  * @param {string} [arg.attribute="data-assign"] - 位置指定属性の名称
  * @param {Object.<string, string>} arg.data - {属性名：値}となるオブジェクト
+ * 
+ * - 1.1.0 2024/08/16 : 階層化オブジェクトに対応
  */
 function assignVariables(arg){
   const v = {whois:'assignVariables',step:0,rv:null};
   console.log(`${v.whois} start.\narg=${stringify(arg)}`);
   try {
 
-    v.step = 1;
+    v.step = 1; // 事前準備
     v.arg = Object.assign({parent:'body',attribute:'data-assign'},arg);
+    v.arg.data = JSON.stringify(arg.data);  // 壊れないよう文字列化
     v.parent = document.querySelector(v.arg.parent);
     v.elements = v.parent.querySelectorAll('['+v.arg.attribute+']');
 
+    v.step = 2; // 対象要素毎に属性置換
     v.elements.forEach(element => {
+
+      v.step = 2.1; // data-assign文字列をカンマで切り分け
       v.attributes = element.getAttribute(v.arg.attribute).split(',');
       v.attributes.forEach(attribute => {
-        v.m = attribute.match(/(\w+)\s*:\s*(\w+)/);
+
+        v.step = 2.2; // m[1]=属性名、m[2]=arg.dataのメンバ名に切り分け
+        v.m = attribute.match(/(\w+)\s*:\s*([\w\.]+)/);
+
+        v.step = 2.3; // '.'で階層毎に切り分け、arg.dataの該当メンバの値をv.dataにセット
+        v.data = JSON.parse(v.arg.data);
+        v.m[2].split('.').forEach(x => v.data = v.data[x]);
+
+        v.step = 2.4; // HTML要素に適用
         if( v.m[1].match(/inner/) ){
           // innerText, innerHTML指定の場合
-          element[v.m[1]] = v.arg.data[v.m[2]];
+          element[v.m[1]] = v.data;
         } else {
           // innerText, innerHTML以外の、属性指定の場合
-          element.setAttribute(v.m[1],v.arg.data[v.m[2]]);
+          element.setAttribute(v.m[1],v.data);
         }
       });
     });
