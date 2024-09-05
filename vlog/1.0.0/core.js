b6edf4ec3199c20366bedad38769224fb3eb32d7
nakaone <ena.kaon@gmail.com>
2024-09-05 11:30:06 +0900

    vlog: 時刻表示を追加

--- vlog/1.0.0/core.js
+++ vlog/1.0.0/core.js
@@ -6,10 +6,12 @@
  */
 const vlog = (v,m,l=null) => {
   // return; // デバッグ用。本番時はコメントを外す
-  let h = l !== null ? `l.${l} ` : '';
+
+  let h = new Date().toLocaleTimeString() + ' ';
+  h += l !== null ? `l.${l} ` : '';
   h += v.whois ? v.whois + ' ' : '';
-  h += v.step ? `step ${v.step}` : '';
-  h += h.length > 0 ? ' : ' : '';
+  h += v.step ? `step ${v.step} ` : '';
+  h += h.length > 0 ? ': ' : '';
   if( whichType(m,'String') ) m = [m];
   const msg = [];
   m.forEach(x => msg.push(`${x}(${whichType(v[x])})=${stringify(v[x])}`));
