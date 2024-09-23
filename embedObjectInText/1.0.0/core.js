ffd468ada9351bcb9e5f4bbcf33a3b948acfad49
nakaone <ena.kaon@gmail.com>
2024-09-23 18:11:32 +0900

    embedObjectInText: 配列に対応

--- embedObjectInText/1.0.0/core.js
+++ embedObjectInText/1.0.0/core.js
@@ -7,27 +7,40 @@
  * 
  * - オブジェクトメンバに存在しない位置指定文字列は変換しない
  * - 埋め込むオブジェクトが階層化している場合、ピリオドで区切る
- * - オブジェクトメンバは、配列には非対応
+ * - dataは配列不可だが、メンバは配列可
  */
 function embedObjectInText(source,rex,data){
   const v = {whois:'embedObjectInText',step:0,rv:source};
   console.log(`${v.whois} start.`);
   try {
 
-    // テキストから位置指定文字列を抽出
+    v.step = 1; // テキストから位置指定文字列を抽出
     v.map = Array.from(new Set(source.match(new RegExp(rex,'g'))));
     vlog(v,'map');
 
     for( v.i=0 ; v.i<v.map.length ; v.i++ ){
-      v.m = v.map[v.i].match(new RegExp(rex))[1].split('.');
-      vlog(v,'m')
+      v.step = 2; // オブジェクトのメンバ名を抽出、階層別に分離
+      v.m1 = v.map[v.i].match(new RegExp(rex))[1].split('.');
+      vlog(v,'m1')
 
+      v.step = 3; // 代入する値を特定
       v.target = data;
-      for( v.d=0 ; v.d<v.m.length ; v.d++ ){
-        v.target = v.target[v.m[v.d]];
+      for( v.d=0 ; v.d<v.m1.length ; v.d++ ){
+        // 配列か判断
+        v.m2 = v.m1[v.d].match(/^(.+?)\[(\d+)\]$/);
+        if( v.m2 ){
+          // 配列の場合、メンバ名と添字で値を特定
+          v.target = v.target[v.m2[1]][Number(v.m2[2])];
+        } else {
+          // 配列では無い場合、メンバ名で値を特定
+          v.target = v.target[v.m1[v.d]];
+        }
       }
-      vlog(v,'target');
-      v.rv = v.rv.replaceAll(new RegExp(v.map[v.i],'g'),v.target);
+
+      v.step = 4; // 全置換を実行
+      // 配列添字の鉤括弧は文字列として扱うよう置換
+      v.l = v.map[v.i].replaceAll(/\[/g,'\\[').replaceAll(/\]/g,'\\]');
+      v.rv = v.rv.replaceAll(new RegExp(v.l,'g'),v.target);
     }
 
     v.step = 9; // 終了処理
