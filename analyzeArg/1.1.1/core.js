3b6d518aaeda6701e175ce60612dc25b5a7bb3f4
nakaone <ena.kaon@gmail.com>
2024-10-25 16:26:30 +0900

    analyzeArg ログ出力を削除

---
+++ analyzeArg/1.1.1/core.js
@@ -0,0 +1,40 @@
+/**
+ * @typedef {object} AnalyzeArg - コマンドライン引数の分析結果
+ * @prop {Object.<string, number>} opt - スイッチを持つ引数について、スイッチ：値形式にしたオブジェクト
+ * @prop {string[]} val - スイッチを持たない引数の配列
+ */
+/**
+ * @desc コマンドラインから`node xxx.js aa bb`を実行した場合の引数(`aa`,`bb`)を取得し、オブジェクトにして返す。<br>
+ *
+ * @example
+ *
+ * ```
+ * node xxx.js -i:aaa.html bbb -o:ccc.json ddd eee
+ * ⇒ {opt:{i:"aaa.html",o:"ccc.json"},val:["bbb","ddd","eee"]}
+ * ```
+ *
+ * <caution>注意</caution>
+ *
+ * - スイッチは`(\-*)([0-9a-zA-Z]+):*(.*)$`形式であること
+ * - スイッチに該当しないものは配列`val`にそのまま格納される
+ *
+ * @param {void} - なし
+ * @returns {AnalyzeArg} 分析結果のオブジェクト
+ */
+function analyzeArg(){
+  const v = {whois:'analyzeArg',rv:{opt:{},val:[]}};
+  try {
+    for( v.i=2 ; v.i<process.argv.length ; v.i++ ){
+      v.m = process.argv[v.i].match(/^(\-*)([0-9a-zA-Z]+):*(.*)$/);
+      if( v.m && v.m[1].length > 0 ){
+        v.rv.opt[v.m[2]] = v.m[3];
+      } else {
+        v.rv.val.push(process.argv[v.i]);
+      }
+    }
+    return v.rv;
+  } catch(e){
+    console.error(v.whois+' abnormal end(step.'+v.step+').',e,v);
+    return e;
+  }
+}
