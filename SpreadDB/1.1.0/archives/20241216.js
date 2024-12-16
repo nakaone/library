422fb5ea02beab302e6adba680b89690c4461eba
nakaone <ena.kaon@gmail.com>
2024-12-16 20:09:09 +0900

    Update 20241216.js

--- SpreadDB/1.1.0/archives/20241216.js
+++ SpreadDB/1.1.0/archives/20241216.js
@@ -858,9 +858,57 @@ function SpreadDb(query=[],opt={}){
       return e;
     }
   }
+  /** functionalize: where句のオブジェクト・文字列を関数化
+   * - update/delete他、引数でwhereを渡されるメソッドで使用
+   */
+  function functionalize(arg){
+    const v = {whois:'sdbTable.functionalize',step:0,rv:null};
+    console.log(`${v.whois} start.\narg(${whichType(arg)})=${stringify(arg)}`);
+    try {
+
+      switch( typeof arg ){
+        case 'function': v.step = 2.1;  // 関数指定ならそのまま利用
+          v.rv = arg;
+          break;
+        case 'object':
+          v.step = 2.2;
+          v.keys = Object.keys(arg);
+          if( v.keys.length === 2 && v.keys.includes('key') && v.keys.includes('value') ){
+            v.step = 2.3; // {key:〜,value:〜}形式での指定の場合
+            v.rv = new Function('o',`return isEqual(o['${arg.key}'],'${arg.value}')`);
+          } else {
+            v.step = 2.4; // {キー項目名:値}形式での指定の場合
+            v.c = [];
+            for( v.j=0 ; v.j<v.keys.length ; v.j++ ){
+              v.c.push(`isEqual(o['${v.keys[v.j]}'],'${arg[v.keys[v.j]]}')`);
+            }
+            v.rv = new Function('o',`return (${v.c.join(' && ')})`);
+          }
+          break;
+        default: v.step = 2.5; // primaryKeyの値指定
+          v.rv = new Function('o',`return isEqual(o['${this.schema.primaryKey}'],${arg})`);
+      }
+
+      v.step = 9; // 終了処理
+      console.log(`${v.whois} normal end.\nv.rv(${whichType(v.rv)})=${stringify(v.rv)}`);
+      return v.rv;
+
+    } catch(e) {
+      e.message = `${v.whois} abnormal end at step.${v.step}\n${e.message}`;
+      console.error(`${e.message}\nv=${stringify(v)}`);
+      return e;
+    }
+  }
   /** selectRow: テーブルから該当行を抽出
-   * @param {Object|Object[]} arg - 追加するオブジェクトの配列
-   * @returns {sdbLog[]}
+   * @param {Object|Object[]} arg
+   * @param {sdbTable} arg.table - 操作対象のテーブル管理情報
+   * @param {Object|function} arg.where - 対象レコード判定条件
+   * @returns {Object[]} 該当行オブジェクト
+   * 
+   * - where句の指定方法
+   *   - Object ⇒ {キー項目名:キー項目の値}形式で、key:valueに該当するレコードを更新
+   *   - Function ⇒ 行オブジェクトを引数に対象ならtrueを返す関数で、trueが返されたレコードを更新
+   *   - その他 ⇒ 項目定義で"primaryKey"指定された項目の値で、primaryKey項目が指定値なら更新
    */
   function selectRow(arg){
     const v = {whois:`${pv.whois}.selectRow`,step:0,rv:[]};
