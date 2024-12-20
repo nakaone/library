c23011fd917f5ace0cd272ef8e68201230e5559b
nakaone <ena.kaon@gmail.com>
2024-12-20 12:51:02 +0900

    Update 20241220.js
    [bug] 複数テーブル追加時「既に存在しています」

--- SpreadDB/1.1.0/archives/20241220.js
+++ SpreadDB/1.1.0/archives/20241220.js
@@ -617,7 +617,7 @@ function SpreadDb(query=[],opt={}){
     console.log(`${v.whois} start. arg.name=${arg.name}`);
     try {
   
-      v.step = 1.1; // 一件分のログオブジェクトを作成
+      v.step = 1; // 一件分のログオブジェクトを作成
       v.log = genLog({
         table: arg.name,
         command: 'create',
@@ -628,87 +628,89 @@ function SpreadDb(query=[],opt={}){
       });
       if( v.log instanceof Error ) throw v.log;
   
-      v.step = 1.2; // sdbTableのプロトタイプ作成
-      v.table = {
-        name: arg.name, // {string} テーブル名(範囲名)
-        account: pv.opt.user ? pv.opt.user.id : null, // {string} 更新者のアカウント
-        sheet: pv.spread.getSheetByName(arg.name), // {Sheet} スプレッドシート内の操作対象シート(ex."master"シート)
-        schema: null, // {sdbSchema} シートの項目定義
-        values: [], // {Object[]} 行オブジェクトの配列。{項目名:値,..} 形式
-        header: [], // {string[]} 項目名一覧(ヘッダ行)
-        notes: [], // {string[]} ヘッダ行のメモ
-        colnum: 0, // {number} データ領域の列数
-        rownum: 0, // {number} データ領域の行数
-      };
-  
       // ----------------------------------------------
-      // テーブル管理情報の作成
+      // テーブル管理情報が未作成の場合、作成
       // ----------------------------------------------
-  
-      if( arg.cols ){ // 項目定義情報が存在する場合
-  
-        v.step = 2; // 項目定義が存在する場合
-        v.table.header = arg.cols.map(x => x.name);
-        v.table.colnum = v.table.header.length;
-  
-        if( arg.values ){
-  
-          v.step = 3; // 項目定義と初期データの両方存在 ⇒ 項目の並びを指定してconvertRow
-          v.convertRow = convertRow(arg.values,v.table.header);
-          if( v.convertRow instanceof Error ) throw v.convertRow;
-          v.table.values = v.convertRow.obj;
-          v.table.rownum = v.convertRow.raw.length;
-  
-        } else {
-  
-          v.step = 4; // 項目定義のみ存在 ⇒ values, rownumは取得不能なので既定値のまま
-          v.convertRow = null;
-  
-        }
-  
-      } else { // 項目定義情報が存在しない場合
-  
-        if( arg.values ){
-  
-          v.step = 5; // 項目定義不在で初期データのみ存在の場合
-          v.convertRow = convertRow(arg.values);
-          if( v.convertRow instanceof Error ) throw v.convertRow;
-          v.table.values = v.convertRow.obj;
-          v.table.header = v.convertRow.header;
+      if( Object.hasOwn(pv.table,arg.name) ) v.table = pv.table[arg.name]; else {
+
+        v.step = 2; // sdbTableのプロトタイプ作成
+        v.table = {
+          name: arg.name, // {string} テーブル名(範囲名)
+          account: pv.opt.user ? pv.opt.user.id : null, // {string} 更新者のアカウント
+          sheet: pv.spread.getSheetByName(arg.name), // {Sheet} スプレッドシート内の操作対象シート(ex."master"シート)
+          schema: null, // {sdbSchema} シートの項目定義
+          values: [], // {Object[]} 行オブジェクトの配列。{項目名:値,..} 形式
+          header: [], // {string[]} 項目名一覧(ヘッダ行)
+          notes: [], // {string[]} ヘッダ行のメモ
+          colnum: 0, // {number} データ領域の列数
+          rownum: 0, // {number} データ領域の行数
+        };
+    
+        if( arg.cols ){ v.step = 3; // 項目定義情報が存在する場合
+    
+          v.table.header = arg.cols.map(x => x.name);
           v.table.colnum = v.table.header.length;
-          v.table.rownum = v.convertRow.raw.length;
-  
-        } else {
-          v.step = 6; // シートも項目定義も初期データも無いならエラー
-          throw new Error(`シートも項目定義も初期データも存在しません`);
+    
+          if( arg.values ){ v.step = 3.1; // 項目定義と初期データの両方存在
+    
+            // 項目の並びを指定してconvertRow
+            v.convertRow = convertRow(arg.values,v.table.header);
+            if( v.convertRow instanceof Error ) throw v.convertRow;
+            v.table.values = v.convertRow.obj;
+            v.table.rownum = v.convertRow.raw.length;
+    
+          } else {  v.step = 3.2; // 項目定義のみ存在
+    
+            // values, rownumは取得不能なので既定値のまま
+            v.convertRow = null;
+    
+          }
+    
+        } else { v.step = 4; // 項目定義情報が存在しない場合
+    
+          if( arg.values ){ v.step = 4.1; // 項目定義不在で初期データのみ存在
+    
+            v.convertRow = convertRow(arg.values);
+            if( v.convertRow instanceof Error ) throw v.convertRow;
+            v.table.values = v.convertRow.obj;
+            v.table.header = v.convertRow.header;
+            v.table.colnum = v.table.header.length;
+            v.table.rownum = v.convertRow.raw.length;
+    
+          } else {  v.step = 4.2; // シートも項目定義も初期データも無い
+            throw new Error(`シートも項目定義も初期データも存在しません`);
+          }
         }
+    
+        v.step = 5; // スキーマをインスタンス化
+        v.r = genSchema({
+          cols: arg.cols || null,
+          header: v.table.header,
+          notes: v.table.notes,
+          values: v.table.values,
+        });
+        if( v.r instanceof Error ) throw v.r;
+        v.table.schema = v.r.schema;
+        v.table.notes = v.r.notes;
       }
-  
-      v.step = 7; // スキーマをインスタンス化
-      v.r = genSchema({
-        cols: arg.cols || null,
-        header: v.table.header,
-        notes: v.table.notes,
-        values: v.table.values,
-      });
-      if( v.r instanceof Error ) throw v.r;
-      v.table.schema = v.r.schema;
-      v.table.notes = v.r.notes;
-  
+
       // ----------------------------------------------
-      v.step = 8; // シートが存在しない場合、新規追加
+      v.step = 6; // シートが存在しない場合、新規追加
       // ----------------------------------------------
-      v.step = 8.1; // シートの追加
-      v.table.sheet = pv.spread.insertSheet();
-      v.table.sheet.setName(arg.name);
-  
-      v.step = 8.2; // シートイメージのセット
-      v.data = [v.table.header, ...(v.convertRow === null ? [] : v.convertRow.raw)];
-      v.table.sheet.getRange(1,1,v.data.length,v.table.colnum).setValues(v.data);
-      v.table.sheet.autoResizeColumns(1,v.table.colnum);  // 各列の幅を項目名の幅に調整
-  
-      v.step = 8.3; // 項目定義メモの追加
-      v.table.sheet.getRange(1,1,1,v.table.colnum).setNotes([v.table.notes]);
+      v.table.sheet = pv.spread.getSheetByName(v.table.name);
+      if( v.table.sheet === null ){
+        v.step = 6.1; // シートの追加
+        v.table.sheet = pv.spread.insertSheet();
+        v.table.sheet.setName(arg.name);
+    
+        v.step = 6.2; // シートイメージのセット
+        v.data = [v.table.header, ...(v.convertRow === null ? [] : v.convertRow.raw)];
+        v.table.sheet.getRange(1,1,v.data.length,v.table.colnum).setValues(v.data);
+        v.table.sheet.autoResizeColumns(1,v.table.colnum);  // 各列の幅を項目名の幅に調整
+    
+        v.step = 6.3; // 項目定義メモの追加
+        v.table.sheet.getRange(1,1,1,v.table.colnum).setNotes([v.table.notes]);
+      }
   
       v.step = 9; // 終了処理
       pv.table[v.table.name] = v.table;
