8dfb956a468ee85cb182e072b6b7808d78d3f723
nakaone <ena.kaon@gmail.com>
2024-12-23 10:20:29 +0900

    Update 20241223.js
    [bug] createで欄の並びがcols定義と違う

--- SpreadDB/1.1.0/archives/20241223.js
+++ SpreadDB/1.1.0/archives/20241223.js
@@ -748,14 +748,13 @@ function SpreadDb(query=[],opt={}){
    *   - header {string} ヘッダ行
    */
   function convertRow(data,header=[]){
-    const v = {whois:pv.whois+'.convertRow',step:0,rv:{raw:[],obj:[],header:[]}};
+    const v = {whois:pv.whois+'.convertRow',step:0,rv:{raw:[],obj:[],header:header}};
     console.log(`${v.whois} start.`);
     try {
 
-      if( Array.isArray(data[0]) ){
-        v.step = 1; // シートイメージ -> 行オブジェクト
-        v.rv.raw = data;
-        v.rv.header = data[0];
+      if( Array.isArray(data[0]) ){ v.step = 1; // シートイメージ -> 行オブジェクト
+
+        v.step = 1.1; // シートイメージを一度行オブジェクトに変換(∵列の並びをheader指定に合わせる)
         for( v.i=1 ; v.i<data.length ; v.i++ ){
           v.o = {};
           for( v.j=0 ; v.j<data[v.i].length ; v.j++ ){
@@ -765,30 +764,28 @@ function SpreadDb(query=[],opt={}){
           }
           v.rv.obj.push(v.o);
         }
-      } else {
-        v.step = 2; // 行オブジェクト -> シートイメージ
-        v.rv.obj = data;
-        // いまここ
-        // 引数headerが指定されていなかった場合、ヘッダ項目を洗い出し
-        // ヘッダ項目の並びで二次元配列を作成
-        v.rv.header = header.length > 0 ? header : Object.keys(data[0]);
-        for( v.map={},v.i=0 ; v.i<v.rv.header.length ; v.i++ ){
-          v.map[v.rv.header[v.i]] = v.i;
+
+        v.step = 1.2; // 引数headerが無ければrv.headerはシートイメージ先頭行とする
+        if( header.length === 0 ){
+          v.rv.header = data[0];
         }
-        for( v.i=0 ; v.i<data.length ; v.i++ ){
-          v.arr = [];
-          for( v.j in data[v.i] ){
-            if( v.map[v.j] === undefined && header.length === 0 ){
-              // ヘッダ行指定無しで未登録の項目があれば追加
-              v.map[v.j] = v.rv.header.length;
-              v.rv.header.push(v.j);
-            }
-            console.log(`l.783 v.arr[${v.map[v.j]}] = data[${v.i}][${v.j}] = ${data[v.i][v.j]}`)
-            v.arr[v.map[v.j]] = data[v.i][v.j];
-          }
-          console.log(`l.786 v.arr=${JSON.stringify(v.arr)}`);  // いまここ：v.arrが空配列
-          v.rv.raw.push(v.arr);
+
+      } else { v.step = 2; // 行オブジェクト -> シートイメージ
+
+        v.rv.obj = data;        
+        if( header.length === 0 ){ // 引数headerが無ければメンバ名からrv.headerを生成
+          v.rv.header = [...new Set(data.flatMap(d => Object.keys(d)))];
         }
+
+      }
+
+      v.step = 3; // ヘッダの項目名の並びに基づき、行オブジェクトからシートイメージを生成
+      for( v.i=0 ; v.i<v.rv.obj.length ; v.i++ ){
+        v.arr = [];
+        for( v.j=0 ; v.j<v.rv.header.length ; v.j++ ){
+          v.arr.push(v.rv.obj[v.i][v.rv.header[v.j]] || '');
+        }
+        v.rv.raw.push(v.arr);
       }
 
       v.step = 9; // 終了処理
