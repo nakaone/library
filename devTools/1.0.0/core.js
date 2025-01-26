cb83f5cfe75cd4721535f0ff9612b326ab83d5f9
nakaone <ena.kaon@gmail.com>
2025-01-26 15:38:10 +0900

    devTools.1.0.0作成

---
+++ devTools/1.0.0/core.js
@@ -0,0 +1,228 @@
+/** devTools: 開発支援関係メソッド集
+ * @param {Object} option
+ * @param {boolean} option.start=true - 開始・終了メッセージの表示
+ * @param {boolean} option.arg=true - 開始時に引数を表示
+ * @param {boolean} option.step=false - step毎の進捗ログの出力
+ */
+function devTools(option) {
+  let opt = Object.assign({ start: true, arg: true, step: false }, option);
+  let seq = 0;  // 関数の呼出順
+  let stack = []; // 呼出元関数情報のスタック
+  return { changeOption: changeOption, check: check, dump: dump, end: end, error: error, start: start, step: step};
+
+  /** オプションの変更 */
+  function changeOption(option) {
+    opt = Object.assign(opt, option);
+    console.log(`devTools.changeOption result: ${JSON.stringify(opt)}`);
+  }
+  /** 実行結果の確認
+   * - JSON文字列の場合、オブジェクト化した上でオブジェクトとして比較する
+   * @param {Object} arg
+   * @param {any} arg.asis - 実行結果
+   * @param {any} arg.tobe - 確認すべきポイント(Check Point)。エラーの場合、エラーオブジェクトを渡す
+   * @param {string} arg.title='' - テストのタイトル(ex. SpreadDbTest.delete.4)
+   * @param {Object} [arg.opt] - isEqualに渡すオプション
+   * @returns {boolean} - チェック結果OK:true, NG:false
+   */
+  function check(arg = {}) {
+    /** recursive: 変数の内容を再帰的にチェック
+     * @param {any} asis - 結果の値
+     * @param {any} tobe - 有るべき値
+     * @param {Object} opt - isEqualに渡すオプション
+     * @param {number} depth=0 - 階層の深さ
+     * @param {string} label - メンバ名または添字
+     */
+    const recursive = (asis, tobe, opt, depth = 0, label = '') => {
+      let rv;
+      // JSON文字列はオブジェクト化
+      asis = (arg => { try { return JSON.parse(arg) } catch { return arg } })(asis);
+      // データ型の判定
+      let type = String(Object.prototype.toString.call(tobe).slice(8, -1));
+      switch (type) {
+        case 'Number': if (Number.isNaN(tobe)) type = 'NaN'; break;
+        case 'Function': if (!('prototype' in tobe)) type = 'Arrow'; break;
+      }
+      let indent = '  '.repeat(depth);
+      switch (type) {
+        case 'Object':
+          msg.push(`${indent}${label.length > 0 ? label + ': ' : ''}{`);
+          for (let mn in tobe) {
+            rv = !Object.hasOwn(asis, mn) ? false // 該当要素が不在
+              : recursive(asis[mn], tobe[mn], opt, depth + 1, mn);
+          }
+          msg.push(`${indent}}`);
+          break;
+        case 'Array':
+          msg.push(`${indent}${label.length > 0 ? label + ': ' : ''}[`);
+          for (let i = 0; i < tobe.length; i++) {
+            rv = (asis[i] === undefined && tobe[i] !== undefined) ? false // 該当要素が不在
+              : recursive(asis[i], tobe[i], opt, depth + 1, String(i));
+          }
+          msg.push(`${indent}]`);
+          break;
+        case 'Function': case 'Arrow':
+          rv = tobe(asis);  // 合格ならtrue, 不合格ならfalseを返す関数を定義
+          msg.push(
+            indent + (label.length > 0 ? (label + ': ') : '')
+            + (rv ? asis : `[NG] (${tobe.toString()})(${asis}) -> false`)
+          );
+          break;
+        default:
+          if (tobe === undefined) {
+            rv = true;
+          } else {
+            rv = isEqual(asis, tobe, opt);
+            msg.push(
+              indent + (label.length > 0 ? (label + ': ') : '')
+              + (rv ? asis : `[NG] ToBe=${tobe}, AsIs=${asis}`)
+            );
+          }
+      }
+      return rv;
+    }
+
+    // 主処理
+    let msg = [];
+    let isOK = true;  // チェックOKならtrue
+
+    arg = Object.assign({ msg: '', opt: {} }, arg);
+    if (arg.tobe === undefined) {
+      // check未指定の場合、チェック省略、結果表示のみ
+      msg.push(`===== ${arg.title} Check Result : Not checked`);
+    } else {
+      // arg.asisとarg.tobeのデータ型が異なる場合、またはrecursiveで不一致が有った場合はエラーと判断
+      if (String(Object.prototype.toString.call(arg.asis).slice(8, -1))
+        !== String(Object.prototype.toString.call(arg.tobe).slice(8, -1))
+        || recursive(arg.asis, arg.tobe, arg.opt) === false
+      ) {
+        isOK = false;
+        msg.unshift(`===== ${arg.title} Check Result : Error`);
+      } else {
+        msg.unshift(`===== ${arg.title} Check Result : OK`);
+      }
+    }
+
+    // 引数として渡されたmsgおよび結果(JSON)を先頭に追加後、コンソールに表示
+    msg = `::::: Verified by devTools.check\n`
+      + `===== ${arg.title} Returned Value\n`
+      + JSON.stringify(arg.asis, (k, v) => typeof v === 'function' ? v.toString() : v, 2)
+      + `\n\n\n${msg.join('\n')}`;
+    if (isOK) console.log(msg); else console.error(msg);
+    return isOK;
+  }
+  /** dump: 渡された変数の内容をコンソールに表示
+   * - 引数には対象変数を列記。最後の引数が数値だった場合、行番号と看做す
+   * @param {any|any[]} arg - 表示する変数および行番号
+   * @returns {void}
+   */
+  function dump() {
+    let arg = [...arguments];
+    let line = typeof arg[arg.length - 1] === 'number' ? arg.pop() : null;
+    const o = stack[stack.length - 1];
+    let msg = (line === null ? '' : `l.${line} `) + `${o.label} step.${o.step}`;
+    for (let i = 0; i < arg.length; i++) {
+      // 対象変数が複数有る場合、Noを追記
+      msg += '\n' + (arg.length > 0 ? `${i}: ` : '') + stringify(arg[i]);
+    }
+    console.log(msg);
+  }
+  /** end: 正常終了時の呼出元関数情報の抹消＋終了メッセージの表示 */
+  function end() {
+    const o = stack.pop();
+    if (opt.start) console.log(`${o.label} normal end.`);
+  }
+  /** error: 異常終了時の呼出元関数情報の抹消＋終了メッセージの表示 */
+  function error(e) {
+    const o = stack.pop();
+    // 参考 : e.lineNumber, e.columnNumber, e.causeを試したが、いずれもundefined
+    e.message = `${o.label} abnormal end at step.${o.step}\n${e.message}`;
+    console.error(e.message
+      + `\n-- footprint\n${o.footprint}`
+      + `\n-- arguments\n${o.arg}`
+    );
+  }
+  /** start: 呼出元関数情報の登録＋開始メッセージの表示
+   * @param {string} name - 関数名
+   * @param {any[]} arg - start呼出元関数に渡された引数([...arguments]固定)
+   */
+  function start(name, arg = []) {
+    const o = {
+      class: '',  // nameがクラス名.メソッド名だった場合のクラス名
+      name: name,
+      seq: seq++,
+      step: 0,
+      footprint: [],
+      arg: [],
+    };
+    o.sSeq = ('000' + o.seq).slice(-4);
+    const caller = stack.length === 0 ? null : stack[stack.length - 1]; // 呼出元
+    // nameがクラス名.メソッド名だった場合、クラス名をセット
+    if (name.includes('.')) [o.class, o.name] = name.split('.');
+    // ラベル作成。呼出元と同じクラスならクラス名は省略
+    o.label = `[${o.sSeq}]` + ((o.class === '' || caller.class === o.class) ? '' : `${o.class}.`) + o.name;
+    // footprintの作成
+    stack.forEach(x => o.footprint.push(`${x.label}.${x.step}`));
+    o.footprint = o.footprint.length === 0 ? '(root)' : o.footprint.join(' > ');
+    // 引数情報の作成
+    if (arg.length === 0) {
+      o.arg = '(void)';
+    } else {
+      for (let i = 0; i < arg.length; i++) o.arg[i] = stringify(arg[i]);
+      o.arg = o.arg.join('\n');
+    }
+    // 作成した呼出元関数情報を保存
+    stack.push(o);
+
+    if (opt.start) {  // 開始メッセージの表示指定が有った場合
+      console.log(`${o.label} start.\n-- footprint\n${o.footprint}` + (opt.arg ? `\n-- arguments\n${o.arg}` : ''));
+    }
+  }
+  /** step: 呼出元関数の進捗状況の登録＋メッセージの表示 */
+  function step(step, msg = '') {
+    const o = stack[stack.length - 1];
+    o.step = step;
+    if (opt.step) console.log(`${o.label} step.${o.step} ${msg}`);
+  }
+  /** stringify: 変数の内容をラベル＋データ型＋値の文字列として出力
+   * @param {any} arg - 文字列化する変数
+   * @returns {string}
+   */
+  function stringify(arg) {
+    /** recursive: 変数の内容を再帰的にメッセージ化
+     * @param {any} arg - 内容を表示する変数
+     * @param {number} depth=0 - 階層の深さ
+     * @param {string} label - メンバ名または添字
+     */
+    const recursive = (arg, depth = 0, label = '') => {
+      // データ型の判定
+      let type = String(Object.prototype.toString.call(arg).slice(8, -1));
+      switch (type) {
+        case 'Number': if (Number.isNaN(arg)) type = 'NaN'; break;
+        case 'Function': if (!('prototype' in arg)) type = 'Arrow'; break;
+      }
+      // ラベル＋データ型＋値の出力
+      let indent = '  '.repeat(depth);
+      switch (type) {
+        case 'Object':
+          msg.push(`${indent}${label.length > 0 ? label + ': ' : ''}{`);
+          for (let mn in arg) recursive(arg[mn], depth + 1, mn);
+          msg.push(`${indent}}`);
+          break;
+        case 'Array':
+          msg.push(`${indent}${label.length > 0 ? label + ': ' : ''}[`);
+          for (let i = 0; i < arg.length; i++) recursive(arg[i], depth + 1, String(i));
+          msg.push(`${indent}]`);
+          break;
+        default:
+          let val = typeof arg === 'function' ? `"${arg.toString()}"` : (typeof arg === 'string' ? `"${arg}"` : arg);
+          // Class Sheetのメソッドのように、toStringが効かないnative codeは出力しない
+          if (typeof val !== 'string' || val.indexOf('[native code]') < 0) {
+            msg.push(`${indent}${label.length > 0 ? label + ': ' : ''}${val}(${type})`);
+          }
+      }
+    }
+    const msg = [];
+    recursive(arg);
+    return msg.join('\n');
+  }
+}
