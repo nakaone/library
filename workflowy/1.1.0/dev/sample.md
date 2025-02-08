6db83cc9a1b98bcc4b39a6872490c955c9c3c8fe
nakaone <ena.kaon@gmail.com>
2025-02-08 16:58:10 +0900

    余計なコメントを削除

--- workflowy/1.1.0/dev/sample.md
+++ workflowy/1.1.0/dev/sample.md
@@ -1,11 +1,11 @@
-{"id":"X000000","children":["X000001","X000003","X000005","X000009"],"depth":1,"text":"テスト用サンプル","link":[],"note":"","href":[],"parent":null}
-{"id":"X000001","children":["X000002","a0376dbc8b20"],"depth":2,"text":"No.1","link":[],"note":"<p>No.1のノート<br>- 項目1<br>- 項目2</p>\n","href":[],"parent":"X000000"}
-{"id":"X000002","children":[],"depth":3,"text":"No.1.1","link":[],"note":"<p>ノート内で<a href=\"#a0376dbc8b20\">リンク</a>を張ってみた</p>\n","href":["a0376dbc8b20"],"parent":"X000001"}
-{"id":"a0376dbc8b20","children":[[]],"depth":3,"text":"<a name=\"a0376dbc8b20\">No.1.2</a>","link":[],"note":"","href":[],"parent":"X000001"}
-{"id":"X000003","children":["X000004"],"depth":2,"text":"No.2。<b>太文字</b>と<span class=\"colored c-red\">赤文字</span>を使用","link":[],"note":"","href":[],"parent":"X000000"}
-{"id":"X000004","children":[],"depth":3,"text":"<a href=\"#a0376dbc8b20\">No.1.2</a>へのリンク","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">No.1.2</a>","a0376dbc8b20","No.1.2"]],"note":"","href":[["a0376dbc8b20"]],"parent":"X000003"}
-{"id":"X000005","children":["X000006","X000007","X000008"],"depth":2,"text":"No.3 : 置換関係","link":[],"note":"","href":[],"parent":"X000000"}
-{"id":"X000006","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[■]リンク先</a>でリンク元を置換(リンク先子要素も追加)","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[■]リンク先</a>","a0376dbc8b20","[■]リンク先"]],"note":"","href":[],"parent":"X000005"}
-{"id":"X000007","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▼]リンク先</a>子要素でリンク元を置換","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▼]リンク先</a>","a0376dbc8b20","[▼]リンク先"]],"note":"","href":[],"parent":"X000005"}
-{"id":"X000008","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▽]リンク先</a>子要素をリンク元の弟要素として追加","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▽]リンク先</a>","a0376dbc8b20","[▽]リンク先"]],"note":"","href":[],"parent":"X000005"}
-{"id":"X000009","children":[],"depth":2,"text":"No.4 h1指定","link":[],"note":"","href":[],"parent":"X000000"}
+{"id":"X000001","children":["X000002","X000004","X000006","X000010"],"depth":1,"text":"テスト用サンプル","link":[],"note":"","href":[],"parent":null}
+{"id":"X000002","children":["X000003","a0376dbc8b20"],"depth":2,"text":"No.1","link":[],"note":"No.1のノート\n- 項目1\n- 項目2","href":[],"parent":"X000001"}
+{"id":"X000003","children":[],"depth":3,"text":"No.1.1","link":[],"note":"ノート内で[リンク](#a0376dbc8b20)を張ってみた","href":["a0376dbc8b20"],"parent":"X000002"}
+{"id":"a0376dbc8b20","children":[[]],"depth":3,"text":"<a name=\"a0376dbc8b20\">No.1.2</a>","link":[],"note":"","href":[],"parent":"X000002"}
+{"id":"X000004","children":["X000005"],"depth":2,"text":"No.2。<b>太文字</b>と<span class=\"colored c-red\">赤文字</span>を使用","link":[],"note":"","href":[],"parent":"X000001"}
+{"id":"X000005","children":[],"depth":3,"text":"<a href=\"#a0376dbc8b20\">No.1.2</a>へのリンク","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">No.1.2</a>","a0376dbc8b20","No.1.2"]],"note":"","href":[["a0376dbc8b20"]],"parent":"X000004"}
+{"id":"X000006","children":["X000007","X000008","X000009"],"depth":2,"text":"No.3 : 置換関係","link":[],"note":"","href":[],"parent":"X000001"}
+{"id":"X000007","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[■]リンク先</a>でリンク元を置換(リンク先子要素も追加)","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[■]リンク先</a>","a0376dbc8b20","[■]リンク先"]],"note":"","href":[],"parent":"X000006"}
+{"id":"X000008","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▼]リンク先</a>子要素でリンク元を置換","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▼]リンク先</a>","a0376dbc8b20","[▼]リンク先"]],"note":"","href":[],"parent":"X000006"}
+{"id":"X000009","children":[],"depth":3,"text":"<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▽]リンク先</a>子要素をリンク元の弟要素として追加","link":[["<a href=\"https://workflowy.com/#/a0376dbc8b20\">[▽]リンク先</a>","a0376dbc8b20","[▽]リンク先"]],"note":"","href":[],"parent":"X000006"}
+{"id":"X000010","children":[],"depth":2,"text":"No.4 h1指定","link":[],"note":"","href":[],"parent":"X000001"}
--- workflowy/1.1.0/pipe.js
+++ workflowy/1.1.0/pipe.js
@@ -26,7 +26,6 @@ reader.on('close', () => {
 /** workflowy: Markdown形式でエクスポートされた内容を修正 */
 function workflowy(option={}){
   const convert = require('xml-js');
-  const marked = require('marked');
   const pv = { whois: 'workflowy', id: 1 , map: new Map()};
   pv.opt = Object.assign({
     mdHeader: 3,  // body直下を第1レベルとし、MarkDown化の際どのレベルまでheader化するかの指定
@@ -34,11 +33,11 @@ function workflowy(option={}){
   return {markdown:markdown,sample:sample};
 
   /** markdown: outlineタグをpv.mapに格納
-   * 
-   * @param {*} obj 
-   * @param {*} parent 
-   * @param {*} depth 
-   * @returns 
+   *
+   * @param {*} obj
+   * @param {*} parent
+   * @param {*} depth
+   * @returns
    * ローカルリンクが有った場合、子要素または兄弟要素に追加
    * 無印 : hrefに置換。リンク先要素への置換・リンク先子要素の追加はしない
    * □ : リンク元要素の子要素としてリンク先子要素を追加(従来のリンク元子要素は残置)
@@ -58,11 +57,10 @@ function workflowy(option={}){
 
       // outlineタグを再帰的にオブジェクト化
       obj = JSON.parse(convert.xml2json(obj));
-      rv = markdown(obj);
+      markdown(obj);
 
       rv = [];
       pv.map.forEach(outline => {
-        
         // textにリンク設定が有った場合、ローカルリンクを展開
         if( outline.link.length > 0 ){
           outline.link.forEach(link => {
@@ -93,7 +91,6 @@ function workflowy(option={}){
         rv.push(JSON.stringify(outline)); // デバッグ用
       });
 
-      // noteはmarkedでhtml化してから追加？
       return rv.join('\n'); // 備考：使わないようなら、depthの再帰内での設定は削除
 
       /*
@@ -140,12 +137,10 @@ function workflowy(option={}){
           id: m ? m[2] : 'X'+('00000'+(pv.id++)).slice(-6),
           depth: depth,
           text: m ? `<a name="${m[2]}">${m[1]}</a>` : obj.attributes.text,
-          //text: m ? m[1] : obj.attributes.text,
           link: [...obj.attributes.text.matchAll(linkRex)],  // textに含まれたリンク
-          note: obj.attributes._note ? marked.parse(  // markdown->html化
+          note: obj.attributes._note ? (
             obj.attributes._note.replaceAll(linkRex,"[$2](#$1)")  // ローカルリンクをa.hrefに変換
-            .replaceAll(/&lt;/g,'<').replaceAll(/&gt;/g,'>') // html特殊文字は還元
-            .replaceAll(/\n/g,'<br>').trim()  // 改行はbrに変換
+            .replaceAll(/&lt;/g,'<').replaceAll(/&gt;/g,'>').trim() // html特殊文字は還元
           ) : '',
           href: // 参照しているidの配列。この段階ではnote内のリンクのみ(textでのリンクは展開方法決定時に追加)
             obj.attributes._note ? [...obj.attributes._note.matchAll(linkRex)].map(x => x[1]) : [],
@@ -159,92 +154,6 @@ function workflowy(option={}){
         obj.elements.forEach(o => outline.children.push(markdown(o,outline.id,depth+1)));
       }
       return outline.id;
-    }
-  }
-
-  function old_markdown(obj,depth=0){
-    let rv = []; let m;
-    const anchorRex = /^(.+)\s*\(([a-z0-9]{12})\)\s*$/;
-    const linkRex = /<a href="https:\/\/workflowy\.com\/#\/([a-z0-9]{12})">(.+)<\/a>/g;
-    if( typeof obj === 'string' ){
-      // 再帰呼出前、最初に呼ばれた時の処理
-
-      // hrefから呼び出されるリンク先リストを作成。リンク切れチェックの準備
-      let hrefs = new Set([...obj.matchAll(/workflowy\.com\/#\/([a-z0-9]{12})/g)].map(a => a[1]));
-
-      // 主処理
-      obj = JSON.parse(convert.xml2json(obj));
-      rv = markdown(obj);
-      rv = rv.join('\n').replaceAll(/\n\n+/g,'\n\n').trim(); // 連続する空白行は一つにまとめる
-
-      // リンク切れチェック
-      // a nameで定義されたアンカーのリストを作成
-      let names = new Set([...rv.matchAll(/<a name="([a-z0-9]{12})"/g)].map(a => a[1]))
-      let hrefsOnly = new Set([...hrefs].filter(e => (!names.has(e)))); // リンク有るのにアンカー無し
-      let namesOnly = new Set([...names].filter(e => (!hrefs.has(e)))); // アンカー有るのにリンク無し
-      let msg = [];
-      if( hrefsOnly.size > 0 ){
-        msg.push('===== [fatal] hrefs only: リンク有るのにアンカー未定義の項目');
-        [...hrefsOnly].forEach(x => {
-          m = rv.match(new RegExp(`\\[(.+)\\]\\(#${x}\\)`));
-          msg.push(`${x}: ${m?m[1]:''}`);
-        })
-      }
-      if( namesOnly.size > 0 ){
-        msg.push('===== [warning] names only: アンカー有るのにリンクは存在しない項目');
-        [...namesOnly].forEach(x => {
-          m = rv.match(new RegExp(`<a name="${x}">(.+)</a>`));
-          msg.push(`${x}: ${m?m[1]:''}`);
-        })
-
-      }
-
-      // リンク切れチェック結果を先頭に付けて処理結果を出力
-      return (msg.length > 0 ? msg.join('\n') + '\n\n' : '') + rv;
-
-    } else {
-      // opml > bodyタグ発見時、depthをリセット
-      if( obj.name === 'body' ) depth = 0;
-
-      // outlineタグの場合、markdown化
-      if( obj.name === 'outline' && obj.attributes ){
-
-        // Backlinksは処理対象外、子要素の再帰も無し。なお"Backlink"は1なら単数形、2以上は複数形
-        if( obj.attributes.text.match(/^\d+ Backlinks?$/) ) return [];
-
-        // ヘッダ部の作成
-        // アンカー文字列を削除してa.nameで囲む
-        m = obj.attributes.text.match(anchorRex);
-        if( m ) obj.attributes.text = `<a name="${m[2]}">${m[1]}</a>`;
-        // ローカルリンクをa.hrefに変換
-        obj.attributes.text = obj.attributes.text.replaceAll(linkRex,"[$2](#$1)");
-
-        // ヘッダに'#'または'-'を付加
-        obj.attributes.text = depth <= pv.opt.mdHeader
-        ? `\n${'#'.repeat(depth)} ${obj.attributes.text}\n` // 指定階層以内の場合は'#'
-        : `${'\t'.repeat(depth-pv.opt.mdHeader-1)}- ${obj.attributes.text}${obj.attributes._note?'<br>':''}`; // 指定階層超の場合は'-'
-        rv.push(obj.attributes.text);
-
-        // ボディ部の作成
-        if( Object.hasOwn(obj.attributes,'_note') ){
-          // ローカルリンクをa.hrefに変換
-          obj.attributes._note = (obj.attributes._note.replaceAll(linkRex,"[$2](#$1)")).trim();
-          // 文中サニタイズされたhtml特殊文字は還元
-          obj.attributes._note = obj.attributes._note.replaceAll(/&lt;/g,'<').replaceAll(/&gt;/g,'>');
-          obj.attributes._note.split('\n').forEach(l => {
-            l = (depth < pv.opt.mdHeader ? '' : '\t'.repeat(depth-pv.opt.mdHeader)) + l;
-            rv.push(l);
-          })
-        }
-      }
-
-      // elements(子要素)が有った場合、再帰呼出
-      if( Array.isArray(obj.elements) ){
-        obj.elements.forEach(o => {
-          rv = [...rv,...markdown(o,depth+1)];
-        });
-      }
-      return rv;
     }
   }
 
