bdf791192347711b1e90247f344ab1048eb3a9f5
nakaone <ena.kaon@gmail.com>
2025-08-22 16:21:38 +0900

    rev.1.1.0 前版を収集・コピー

---
+++ LocalDb/1.0.0/schemaDef.js
@@ -0,0 +1,25 @@
+/** SpreadDb/LocalDb/createTable共通
+ * @typedef {Object} schemaDef - DB構造定義オブジェクト
+ * @param {string} dbName - データベース名
+ * @param {tableDef[]} tables - DB内の個々のテーブルの定義
+ * @param {Object.<string,Function>} [custom] - AlaSQLのカスタム関数
+ * @param {string} created - 作成日時。export時に付記
+ * 
+ * @typedef {Object} tableDef - テーブル構造定義オブジェクト
+ * @param {string} name - テーブル名。シート名も一致させる
+ * @param {string|string[]} [primaryKey] - 主キーとなる項目名。複合キーの場合配列で指定
+ * @param {columnDef[]} cols - 項目定義
+ * @param {Function} [initial] - 初期設定用関数(テーブルに初期データ登録＋シート作成)
+ * @param {Object[]} data - テーブルの行オブジェクトの配列。import/export時のみ設定
+ * @param {Object} [exportDef={}] - export時の設定。exportDef=nullの場合、出力対象外とする
+ * @param {string[]} exportDef.select=[] - 出力項目を絞り込む場合の項目名リスト。空配列なら全項目出力
+ * @param {string} exportDef.where="" - 出力行を絞り込む場合の条件(SQLのwhere句)
+ * 
+ * @typedef {Object} columnDef - 項目定義オブジェクト
+ * @param {string} name - 項目名
+ * @param {string} [label] - テーブル・シート表示時の項目名。省略時はnameを流用
+ * @param {string} type - データ型。string/number/boolean
+ * @param {any} [default] - 既定値。関数の場合、引数は行オブジェクト
+ * @param {Function} [printf] - 表示時点で行う文字列の整形用関数。引数は行オブジェクト
+ * @param {string} [note] - 備考
+ */
---
+++ LocalDb/1.1.0/core.js
@@ -0,0 +1,141 @@
+/** localDB: IndexedDBを保存先とするブラウザ内RDB
+ * @param {Object} arg
+ * @param {schemaDef} arg.schema={} - DB構造定義オブジェクト
+ * @returns {Object} 使用可能なメソッドのオブジェクト
+ */
+function LocalDb(arg={}) {
+  const pv = { whois: 'LocalDb', rv: null,
+    schema: arg.schema || {},
+    idb: null,          // IndexedDB
+    dbName: 'LocalDb',  // IndexedDBの名称
+    storeName: 'RDB',   // IndexedDBのストア名
+    rdb: new alasql.Database(), // alasql
+  };
+
+  /*
+  async function constructor(arg) {
+    const v = { whois: `${pv.whois}.constructor`, rv: null};
+    dev.start(v.whois, [...arguments]);
+    return new Promise(async(resolve, reject) => {
+      dev.step(1);  // オプションに既定値設定
+      pv.opt = Object.assign(pv.opt,arg);
+      dev.dump(pv.opt);
+
+      dev.step(2);
+      pv.idb = await openIndexedDB();
+
+      for( v.tableName in cf.tableDef.mappingTable){
+        dev.step(3);  // rdbからテーブル名をキーとして検索
+        v.existingData = await getIndexedDB(v.tableName);
+        
+        if (v.existingData) {
+          dev.step(4.1);  // 存在すればrowsをJSON.parseしてpv.rdbに格納
+          pv.rdb.tables[v.tableName] = JSON.parse(v.existingData.rows);
+        } else {
+          dev.step(4.2);  // 存在しなければ新規登録
+          await setIndexedDB(v.tableName, '[]');
+          pv.rdb.exec(`create table \`${v.tableName}\``);
+        }
+      }
+
+      dev.end(); // 終了処理
+      return v.rv;
+
+    });
+  }
+
+  async function openIndexedDB() {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open(pv.opt.dbName, 1);
+      
+      request.onupgradeneeded = (event) => {
+        pv.idb = event.target.result;
+        pv.idb.createObjectStore(pv.opt.storeName, { keyPath: 'name' });
+      };
+
+      request.onsuccess = (event) => {
+        resolve(event.target.result);
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  }
+
+  async function getIndexedDB(key) {
+    return new Promise((resolve) => {
+      const transaction = pv.idb.transaction([pv.opt.storeName]);
+      const store = transaction.objectStore(pv.opt.storeName);
+      const request = store.get(key);
+
+      request.onsuccess = (event) => {
+        resolve(event.target.result);
+      };
+    });
+  }
+
+  async function setIndexedDB(name, rows) {
+    return new Promise((resolve) => {
+      const transaction = pv.idb.transaction(pv.opt.storeName, 'readwrite');
+      const store = transaction.objectStore(pv.opt.storeName);
+      const request = store.put({ name: name, rows: rows });
+
+      request.onsuccess = () => {
+        resolve();
+      };
+    });
+  }
+  */
+
+  /** execSQL: alasqlでSQLを実行
+   * @param {string} sql
+   * @param {Array[]} arg - alasqlの第二引数
+   * @returns {Object[]}
+   */
+  function execSQL(sql,arg=null) {
+    return arg === null ? pv.rdb.exec(sql) : pv.rdb.exec(sql,arg);
+  }
+
+  /** hasTable: RDB(alasql)内にテーブルを持っているか確認
+   * @param {string} tableName
+   * @returns {boolean}
+   */
+  function hasTable(tableName) {
+    return tableName in pv.rdb.tables;
+  }
+
+  dev.start(pv.whois, [...arguments]);
+  try {
+
+    dev.step(3);  // AlaSQLカスタム関数の用意
+    if( pv.schema.hasOwnProperty('custom') ){
+      Object.keys(pv.schema.custom).forEach(x => alasql.fn[x] = pv.schema.custom[x]);
+    }
+
+    /*
+    constructor(arg).then(r => {
+      dev.end(); // 終了処理
+      return {
+        //'import': importJSON,
+        //'export': exportJSON,
+        //'save': saveRDB,
+        //'load': loadRDB,
+        'do': execSQL,
+        //'hasTable': hasTable,
+      };
+    });
+    */
+
+    dev.end(); // 終了処理
+    return {
+      'exec': execSQL,
+      //'load': loadRDB,
+      //'save': saveRDB,
+      //'import': importJSON,
+      //'export': exportJSON,
+      'hasTable': hasTable,
+    };
+
+  } catch (e) { dev.error(e); return e; }
+}
