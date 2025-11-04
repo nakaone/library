8bccd9340b11296c8d4a98617a12afa7ee6247bf
nakaone <ena.kaon@gmail.com>
2025-11-04 11:37:27 +0900

    未完了JavaScriptのクリア

--- underDev/Auth/src/Member/proto.js
+++
@@ -1,7 +0,0 @@
-class Member {
-  //::$src/Member/constructor.js::
-  //::$src/Member/getAllMembers.js::
-  //::$src/Member/getMember.js::
-  //::$src/Member/judgeStatus.js::
-  //::$src/Member/setMember.js::
-}
