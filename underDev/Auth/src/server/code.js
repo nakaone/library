eef59595f39986376fa7d90a6b5510fa1cb57aaa
nakaone <ena.kaon@gmail.com>
2025-12-16 10:11:00 +0900

    Create code.js

---
+++ underDev/Auth/src/server/code.js
@@ -0,0 +1,36 @@
+// ライブラリ関数定義
+//::$lib/devTools/2.1.0/core.js::
+
+// authServer関係クラス定義
+//::$tmp/authServer.js::
+//::$tmp/cryptoServer.js::
+//::$tmp/Member.js::
+
+// グローバル変数定義
+const dev = devTools();
+const asv = authServer({
+  // プロジェクト毎の独自パラメータ
+});
+
+// Webアプリ定義
+function doPost(e) {
+  const rv = asv.exec(e.postData.contents); // 受け取った本文(文字列)
+  if( rv !== null ){ // fatal(無応答)の場合はnullを返す
+    return ContentService
+      .createTextOutput(rv);
+  }
+}
+
+// スプレッドシートメニュー定義
+const ui = SpreadsheetApp.getUi();
+ui.createMenu('追加したメニュー')
+  .addItem('加入認否入力', 'menu10')
+  .addSeparator()
+  .addSubMenu(ui.createMenu("システム関係")
+    .addItem('実行環境の初期化', 'menu21')
+    .addItem("【緊急】鍵ペアの更新", "menu22")
+  )
+  .addToUi();
+const menu10 = () => asv.listNotYetDecided();
+const menu21 = () => asv.setupEnvironment();
+const menu22 = () => asv.resetSPkey();
