39ce3e2744f0cc3f372c9042004a573f371f96a0
nakaone <ena.kaon@gmail.com>
2025-12-19 14:29:09 +0900

    2.1.1をコピー

---
+++ devTools/2.1.2/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# -x  つけるとverbose
+set -e # エラー時点で停止
+source ~/Desktop/GitHub/tools/common.sh
+
+# ----------------------------------------------
+# 0. 事前準備
+# ----------------------------------------------
+#prj="$lib/devTools/2.1.1"
+
+# ES Module対応版(core.mjs)から埋込用(core.js)を作成
+sed 's/^export //' core.mjs > core.js
