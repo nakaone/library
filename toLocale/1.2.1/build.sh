b6812f2e16dd21006133e6cf2156d20939534ba2
nakaone <ena.kaon@gmail.com>
2025-12-24 12:49:13 +0900

    toLocale: whichTypeへの依存を除去

--- toLocale/1.2.1/build.sh
+++ toLocale/1.2.1/build.sh
@@ -3,10 +3,5 @@
 set -e # エラー時点で停止
 source ~/Desktop/GitHub/tools/common.sh
 
-# ----------------------------------------------
-# 0. 事前準備
-# ----------------------------------------------
-#prj="$lib/devTools/3.0.0"
-
 # ES Module対応版(core.mjs)から埋込用(core.js)を作成
 sed '/^import /d; s/^export //' core.mjs > core.js
