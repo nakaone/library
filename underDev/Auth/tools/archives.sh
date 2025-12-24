770017c4d27dc0dabac8661713c839f29bb213e4
nakaone <ena.kaon@gmail.com>
2025-12-24 09:43:29 +0900

    Update archives.sh

--- underDev/Auth/tools/archives.sh
+++ underDev/Auth/tools/archives.sh
@@ -12,5 +12,5 @@ doc="$prj/doc"
 # バックアップファイルの作成
 # ----------------------------------------------
 cd $prj
-zip -r $arc/`date +"%Y%m%d"`.zip ./ -x "archives/*" "tmp/*"
+zip -r $arc/`date +"%Y%m%d"`.zip ./ -x "archives/*" "node_modules/*" "tmp/*"
 # 除外指定(-x)ではフォルダ名を直接指定のこと(変数だと除外されない)
