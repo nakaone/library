e6d6509b9dee387cb295fd673639014a2f232516
nakaone <ena.kaon@gmail.com>
2026-01-18 12:42:35 +0900

    common/config.json -> config.mjs

---
+++ underDev/Auth/src/common/config.mjs
@@ -0,0 +1,16 @@
+/** config: アプリケーションの設定情報
+ * - 複数のライブラリを使用可能にするため、第一レベルはアプリ名とする
+ * - 秘匿情報はここで設定せず、アプリ側で追加・修正する
+ */
+export const config = {
+  "auth":{
+    "common":{
+      "adminMail":"ena.kaon@gmail.com",
+      "adminName":"あどみ"
+    },
+    "client":{
+      "api":"AKfycbw-n_isgCLvAEntGEX5Lpn4AUNaqpp3r1W0nVIGXFjNlEdsRB_ue4b9NRmNH_Em5IxT"
+    },
+    "server":{}
+  }
+};
