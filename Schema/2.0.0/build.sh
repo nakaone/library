ce0b8b576db30aa58626930b21587cbcf17f2759
nakaone <ena.kaon@gmail.com>
2026-01-19 10:08:00 +0900

    [bug] Schema実装漏れ

---
+++ Schema/2.0.0/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# -x  つけるとverbose
+set -e # エラー時点で停止
+source ~/Desktop/GitHub/tools/common.sh
+
+# ----------------------------------------------
+# 0. 事前準備
+# ----------------------------------------------
+prj="$lib/Schema/2.0.0"
+
+# ES Module対応版(core.mjs)から埋込用(core.js)を作成
+sed '/^import /d; s/^export //' core.mjs > core.js
--- underDev/Auth/src/server/code.js
+++ underDev/Auth/src/server/code.js
@@ -3,6 +3,7 @@
 
 // ライブラリ関数定義
 //::$lib/devTools/3.0.0/core.js::
+//::$lib/Schema/2.0.0/core.js::
 //::$lib/toLocale/1.2.1/core.js::
 
 // auth関係クラス定義・動作設定
