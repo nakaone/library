ed14e1198382f92adffa06c0a2827d1697b40115
nakaone <ena.kaon@gmail.com>
2026-02-02 12:46:39 +0900

    ソースを整理

--- underDev/specify/createSpec.mjs
+++ underDev/specify/createSpec.mjs
@@ -134,6 +134,70 @@ async function createSpec() {
    */
   const doclet = [];
 
+  /** execJSDoc: 対象ファイルに順次jsdocを実行、結果をdocletに保存 */
+  async function execJsdoc(){
+    const v = {whois:`${pv.whois}.execJsdoc`, arg:{}, rv:null};
+    const dev = new devTools(v);
+    try {
+
+      dev.step(1);  // ファイル単位の処理：jsdocを実行、結果をdocletに格納
+      for( v.i=0 ; v.i<sourceFile.source.length ; v.i++ ){
+        dev.step(1.1);  // ファイル単位にjsdocを実行
+        v.r = await runJsdoc(sourceFile.source[v.i].full);
+        if( typeof v.r === 'string' ) throw new Error(v.r);
+
+        dev.step(1.2);  // DocLet単位にばらして保存
+        v.r.forEach(o => doclet.push({
+          unique:sourceFile.source[v.i].unique, // 固有パスはファイル単位なのでここで追加
+          origin:o,
+        }));
+      }
+
+      dev.step(2);  // DocLet単位の処理
+      for( v.i=0 ; v.i<doclet.length ; v.i++ ){
+        v.s = doclet[v.i].origin; // source
+        v.d = doclet[v.i];  // destination
+
+        dev.step(2.1);  // docletの型を判定、不明ならスキップ
+        v.r = identifyDocletType(v.s);
+        if( v.r instanceof Error ) throw v.r;
+        if( v.r === 'unknown' ) continue; else v.d.type = v.r;
+
+        dev.step(2.2);  // idを設定(固有パス＋ファイル名＋行番号)
+        v.d.id = v.d.unique + `:${v.s.meta.lineno}`;
+
+        dev.step(2.3);  // labelを抽出
+        v.m = v.s.comment.split('\n')[0].match(/^\/\*\*\s*(.+)\n/);
+        v.desc = (v.s.description ?? v.s.classdesc ?? v.s.longname ?? '')
+          .split('\n')[0] ?? '(ラベル未設定)';
+        v.d.label = v.m !== null ? v.m[1]
+        : (v.d.type === 'name' || v.d.type === 'class' ? (v.s.longname ?? v.desc) : v.desc);
+
+        // description, classdesc
+        // params
+        // properties, returns
+        // name
+
+        //dev.step(2.3,{id:v.d.id,type:v.d.type,desc:v.s.description});  // descriptionの行頭空白を削除
+        //if( typeof v.s.description !== 'undefined' ){
+          //v.d.description = trimCommonIndent(v.s.description);
+          //if( v.d.description instanceof Error ) throw v.d.description;
+        //}
+        dev.step(99.284,{m:v.m,unique:v.d.unique,id:v.d.id,type:v.d.type,label:v.d.label,origin:v.s});
+        //dev.step(99.275,{unique:v.d.unique,id:v.d.id,type:v.d.type,label:v.d.label,comment:v.s.comment,desc:v.s.description});
+        if( v.d.type === 'description' ){
+          dev.step(99.278,v.s);
+        }
+
+      }
+      //dev.step(99.258,doclet[0]);
+
+      dev.end(); // 終了処理
+      return v.rv;
+
+    } catch (e) { return dev.error(e); }
+  }
+
   /** identifyDoclet: jsdoc -Xで出力されたオブジェクト(DocLet)の型を判定
    * @param {Object} doclet
    * @returns {string|Error}
@@ -213,6 +277,138 @@ async function createSpec() {
       }
 
       dev.end();
+      return v.rv;
+
+    } catch (e) { return dev.error(e); }
+  }
+
+  /** investigate: 【開発用】jsdoc -Xで出力されたオブジェクトの内容を調査
+   * @param {void}
+   * @returns {Object|Error} データ型はstep 1.1参照
+   * @example
+   * - コンソールの内容をresult.txt等に保存
+   * - "createSpec.investigate normal end"を検索
+   * - result欄の内容確認(以下はサンプル)
+   *   result:  [
+   *     "10: typedef", // string   ※行頭数字はシンボル(関数・クラス・プロパティ)の行番号
+   *     "22: interface", // string
+   *     "34: global function", // string
+   *     "39: inner function(func01~arrow01)", ※括弧内は親関数・クラス
+   *     "40: typedef", // string
+   *     "46: unknown", // string
+   *     "49: typedef", // string
+   *     "55: unknown", // string
+   *     "65: class", // string
+   *     "66: typedef", // string
+   *     "77: constructor(class01)", // string
+   *     "85: unknown", // string
+   *     "92: method(class01#method01)", // string
+   *   ], // Array
+   */
+  function investigate() {
+    const v = {whois:`${pv.whois}.investigate`, arg:{}, rv:[]};
+    const dev = new devTools(v);
+    try {
+
+      // -------------------------------------------------------------
+      dev.step(1);  // 事前処理
+      // -------------------------------------------------------------
+      dev.step(1.1);  // 戻り値の形式定義
+      v.rv = {  
+        idListLength: pv.idList.length, // 対象JSDoc要素数
+        idList: pv.idList,              // 対象JSDoc要素のIDリスト
+        result: [],                     // 判定結果
+        kindList: [],                   // kind属性に設定されている値のリスト
+        scopeList: [],                  // scope 〃
+        sampleNum: 0,                   // サンプルの件数
+        sample:[],                      // サンプルのダンプ
+      };
+
+      dev.step(1.2);  // 抽出条件の定義
+      v.ex = {
+        all: o => true,
+        // ①scope:"global"
+        global: o => {return typeof o.scope !== 'undefined' && o.scope === 'global'},
+        // ②kind:"typedef"
+        typedef: o => {return typeof o.kind !== 'undefined' && o.kind === 'typedef'},
+        // ③kind:"function"
+        func: o => {return typeof o.kind !== 'undefined' && o.kind === 'function'},
+        // ④kind: "class"
+        class: o => {return typeof o.kind !== 'undefined' && o.kind === 'class'},
+        // ⑤meta.code.type:"MethodDefinition"
+        method: o => o.meta?.code?.type === "MethodDefinition",
+        // ⑥判定テストで「unknown」
+        unknown: o => / unknown$/.test(o.result),
+      }
+
+      dev.step(1.3);  // サンプルの表示方法指定
+      v.st = {
+        all: o => o,  // 全属性表示
+        major: o => {const rv = {}; // 主要属性のみ抽出
+          ['name','description','kind','memberof','scope']
+          .forEach(x => rv[x] = o[x] ?? '');
+          return rv;
+        },
+      }
+
+      dev.step(1.4);  // 適用条件の指定
+      v.cond = v.ex.all; // 適用する抽出条件
+      v.disp = v.st.all;  // サンプルの表示方法
+
+      // -------------------------------------------------------------
+      dev.step(2);  // JSDoc要素毎に検査実施
+      // なおテストに伴いJSDoc要素に"id","result"メンバが追加されるので注意
+      // -------------------------------------------------------------
+      pv.idList.forEach(x => {
+
+        dev.step(2.1);  // idを追加
+        v.mObj = Object.assign({id:x},pv.map[x]);
+
+        dev.step(2.2);  // 指定属性の値一覧を作成
+        ['kind','scope'].forEach(p => {
+          if( v.mObj.hasOwnProperty(p) && !v.rv[`${p}List`].includes(v.mObj[p]) ){
+            v.rv[`${p}List`].push(v.mObj[p]);
+          }
+        });
+
+        dev.step(2.3);  // 判定テスト
+        v.mObj.result = v.mObj.meta.lineno + ': ';
+        switch( v.mObj.kind ){
+          case 'typedef':
+            v.mObj.result +=  'typedef';
+            break;
+          case 'interface':
+            v.mObj.result +=  'interface';
+            break;
+          case 'function':
+            switch( v.mObj.scope ){
+              case 'global': v.mObj.result +=  'global function'; break;
+              case 'inner': v.mObj.result +=  `inner function(${v.mObj.longname})`; break;
+              case 'instance': v.mObj.result +=  `method(${v.mObj.longname})`;
+                break;
+              default: v.mObj.result +=  `unknown`;
+            }
+            break;
+          case 'class':
+            switch( v.mObj.meta.code.type ){
+              case 'ClassDeclaration': v.mObj.result +=  'class'; break;
+              case 'MethodDefinition': v.mObj.result +=  `constructor(${v.mObj.longname})`; break;
+              default: v.mObj.result +=  'unknown';
+            }
+            break;
+          default: v.mObj.result +=  `unknown`;
+        }
+        v.rv.result.push(v.mObj.result);
+
+        dev.step(2.4);  // 抽出条件に従ってサンプルを追加
+        if( v.cond(v.mObj) ) v.rv.sample.push(v.disp(v.mObj));
+
+      });
+
+      dev.step(3);  // 終了処理
+      // サンプル数を戻り値にセット
+      v.rv.sampleNum = v.rv.sample.length;
+      dev.end(v.rv);
       return v.rv;
 
     } catch (e) { return dev.error(e); }
@@ -319,65 +515,75 @@ async function createSpec() {
     } catch (e) { return dev.error(e); }
   }
   
-  /** execJSDoc: 対象ファイルに順次jsdocを実行、結果をdocletに保存 */
-  async function execJsdoc(){
-    const v = {whois:`${pv.whois}.execJsdoc`, arg:{}, rv:null};
+  /** makeMarkdown: Markdown形式の仕様書を作成
+   * @param 
+   */
+  function makeMarkdown(arg) {
+    const v = {whois:`${pv.whois}.identifyDocletType`, arg:{arg}, rv:[]};
     const dev = new devTools(v);
+    /**
+     * @name クラス・グローバル関数文書の構成
+     * @desc
+     * 
+     * 1. ヘッダ部("クラス名_top")
+     *    1. タイトル(○○クラス仕様書、等)
+     *    2. ラベル(一行にまとめた説明)
+     *    3. 概要説明(数行程度)
+     * 2. 詳細説明("クラス名_desc")
+     *    - 
+     * 3. 一覧
+     *    1. メンバ一覧("クラス名_prop")
+     *    2. メソッド一覧("クラス名_func")
+     * 4. 個別メソッド("クラス名-メソッド名") ※注意：'_'ではなく'-'
+     *    1. タイトル(クラス名.メソッド名)("クラス名-メソッド名_top")
+     *    2. ラベル(一行にまとめた説明)
+     *    3. 機能概要(数行程度)
+     *    4. 詳細説明
+     *    5. 引数
+     *    6. 戻り値
+     */
+    /**
+     * @name 一覧文書(フォルダ毎)の構成
+     * @desc
+     * 
+     * - 並び順はフォルダ内のデータ型名順(アルファベット順)
+     * 
+     * 1. ヘッダ部("フォルダ名_top")
+     * 2. グローバル関数・クラス一覧("フォルダ名_list")
+     * 3. データ型一覧("フォルダ名_type")
+     * 4. 個別データ型("フォルダ名-データ型名") ※注意：'_'ではなく'-'
+     */
+    /**
+     * @name 中間データオブジェクトの形式
+     * {
+     *   パス名: {  ※固有部分のパスについて'/'を'_'に変換したもの
+     *     グローバル関数・クラス名: {
+     *     },
+     *     "readme": {   ※一覧文書(フォルダ毎)
+     *     }
+     *   }
+     * }
+     */
+    /** articleObj: 単一記事(タイトル＋本文)のデータ型
+     * - `<!--::記事のID::-->`で他記事も埋め込み可とする
+     * - アンカーのidは識別子を小文字変換したものとする
+     * 
+     * @interface articleObj
+     * @prop {string} id - 記事の識別子
+     * @prop {string} title - 記事のタイトル
+     * @prop {string} [icon] - アイコンを付ける場合に設定
+     * @prop {boolean} [anchor=false] - アンカーを設定する場合に設定(`<span id="〜">`)
+     * @prop {string} [link] - タイトルにリンクを張る場合の参照先URL
+     * @prop {string} [top] - タイトルの前に挿入する文字列(固定メニュー等)
+     * @prop {string} [middle] - タイトルの後・記事の前に〃
+     * @prop {string} [bottom] - 記事の後に〃
+     * @prop {string} content - 記事本文
+     */
     try {
 
-      dev.step(1);  // ファイル単位の処理：jsdocを実行、結果をdocletに格納
-      for( v.i=0 ; v.i<sourceFile.source.length ; v.i++ ){
-        dev.step(1.1);  // ファイル単位にjsdocを実行
-        v.r = await runJsdoc(sourceFile.source[v.i].full);
-        if( typeof v.r === 'string' ) throw new Error(v.r);
+      // -------------------------------------------------------------
 
-        dev.step(1.2);  // DocLet単位にばらして保存
-        v.r.forEach(o => doclet.push({
-          unique:sourceFile.source[v.i].unique, // 固有パスはファイル単位なのでここで追加
-          origin:o,
-        }));
-      }
-
-      dev.step(2);  // DocLet単位の処理
-      for( v.i=0 ; v.i<doclet.length ; v.i++ ){
-        v.s = doclet[v.i].origin; // source
-        v.d = doclet[v.i];  // destination
-
-        dev.step(2.1);  // docletの型を判定、不明ならスキップ
-        v.r = identifyDocletType(v.s);
-        if( v.r instanceof Error ) throw v.r;
-        if( v.r === 'unknown' ) continue; else v.d.type = v.r;
-
-        dev.step(2.2);  // idを設定(固有パス＋ファイル名＋行番号)
-        v.d.id = v.d.unique + `:${v.s.meta.lineno}`;
-
-        dev.step(2.3);  // labelを抽出
-        v.m = v.s.comment.split('\n')[0].match(/^\/\*\*\s*(.+)\n/);
-        v.desc = (v.s.description ?? v.s.classdesc ?? v.s.longname ?? '')
-          .split('\n')[0] ?? '(ラベル未設定)';
-        v.d.label = v.m !== null ? v.m[1]
-        : (v.d.type === 'name' || v.d.type === 'class' ? (v.s.longname ?? v.desc) : v.desc);
-
-        // description, classdesc
-        // params
-        // properties, returns
-        // name
-
-        //dev.step(2.3,{id:v.d.id,type:v.d.type,desc:v.s.description});  // descriptionの行頭空白を削除
-        if( typeof v.s.description !== 'undefined' ){
-          v.d.description = trimCommonIndent(v.s.description);
-          if( v.d.description instanceof Error ) throw v.d.description;
-        }
-        dev.step(99.284,{m:v.m,unique:v.d.unique,id:v.d.id,type:v.d.type,label:v.d.label,origin:v.s});
-        //dev.step(99.275,{unique:v.d.unique,id:v.d.id,type:v.d.type,label:v.d.label,comment:v.s.comment,desc:v.s.description});
-        if( v.d.type === 'description' ){
-          dev.step(99.278,v.s);
-        }
-
-      }
-      //dev.step(99.258,doclet[0]);
-
-      dev.end(); // 終了処理
+      dev.end(v.rv);
       return v.rv;
 
     } catch (e) { return dev.error(e); }
@@ -432,6 +638,82 @@ async function createSpec() {
     });
   }
 
+  // -------------------------------------------------------------
+  // メイン処理
+  // -------------------------------------------------------------
+  const dev = new devTools(pv);
+  try {
+
+    dev.step(1);  // sourceFileに対象ファイルリスト作成
+    pv.r = listSource();
+    if( pv.r instanceof Error) throw pv.r;
+
+    dev.step(2);  // 対象ファイルについて順次jsdocを実行
+    pv.r = execJsdoc();
+    if( pv.r instanceof Error) throw pv.r;
+
+  } catch (e) { dev.error(e); return e; } finally {
+    // jsdoc動作定義ファイルを削除
+    //if( existsSync(cf.jsdocJson) )
+      //unlinkSync(cf.jsdocJson);
+    // ダミーディレクトリを削除
+    //if( existsSync(cf.dummyDir) )
+      //rmSync(cf.dummyDir, { recursive: true, force: true });
+  }
+}
+
+/** trash: 過去作成したソースで必要なくなったもの。備忘用 */
+function trash(){
+  /** makeMap: 「ファイル名＋行番号」を識別子とするマップを作成 */
+  async function makeMap(list){
+    const v = {whois:`${pv.whois}.makeMap`, arg:{list}, rv:{}};
+    const dev = new devTools(v);
+    try {
+
+      for( v.i=0 ; v.i<list.length ; v.i++ ){
+
+        dev.step(1.1);  // JSDocを取得
+        v.arr = await runJSDoc(list[v.i]);
+        if( v.arr instanceof Error ) throw v.arr;
+
+        dev.step(1.2);  // 取得結果のチェック。配列で無い場合はメッセージを出してスキップ
+        if( !Array.isArray(v.arr) ){
+          dev.step(1.99,`not Array: ${JSON.stringify(v.arr)}`);
+          continue;
+        }
+
+        dev.step(2);  // v.mapの作成
+        v.arr.forEach(o => {
+          // 【備忘】
+          // ①"meta.code.id"は存在しない場合があるので使用を断念。
+          // ②'kind:"package"'
+          //   -> プロジェクトのメタ情報（name, version, description など）
+          //   "meta.lineno"を持たないが、仕様書作成に使用しないのでmap登録対象外とする
+          if( typeof o.meta !== 'undefined' && typeof o.meta.lineno === 'number'){
+            dev.step(2.1);  // v.mapのキー文字列(ID)の作成
+            //v.id = `${list[v.i]}-${o.meta.lineno}`;
+            v.id = `${o.meta.path ?? ''}/${o.meta.filename ?? ''}-${o.meta.lineno}`;
+
+            if( pv.idList.includes(v.id) ){
+              dev.step(2.2);  // 登録済なら結合
+              o = mergeDeeply(pv.map[v.id],o);
+            } else {
+              dev.step(2.3);  // 未登録なら登録済IDリストに追加
+              pv.idList.push(v.id);
+            }
+
+            dev.step(2.4);  // pv.mapへの登録
+            pv.map[v.id] = o;
+          }
+        });
+      }
+
+      dev.end(); // 終了処理
+      return v.rv;
+
+    } catch (e) { return dev.error(e); }
+  }
+
   /** trimCommonIndent: 文字列を行単位に分割、全行に共通する先頭の空白を削除
    * 但し先頭行頭に空白が無かった場合、調査対象から除外する
    * @param {string[]} txt
@@ -464,28 +746,5 @@ async function createSpec() {
       return v.rv;
 
     } catch (e) { return dev.error(e); }
-  }
-
-  // -------------------------------------------------------------
-  // メイン処理
-  // -------------------------------------------------------------
-  const dev = new devTools(pv);
-  try {
-
-    dev.step(1);  // sourceFileに対象ファイルリスト作成
-    pv.r = listSource();
-    if( pv.r instanceof Error) throw pv.r;
-
-    dev.step(2);  // 対象ファイルについて順次jsdocを実行
-    pv.r = execJsdoc();
-    if( pv.r instanceof Error) throw pv.r;
-
-  } catch (e) { dev.error(e); return e; } finally {
-    // jsdoc動作定義ファイルを削除
-    //if( existsSync(cf.jsdocJson) )
-      //unlinkSync(cf.jsdocJson);
-    // ダミーディレクトリを削除
-    //if( existsSync(cf.dummyDir) )
-      //rmSync(cf.dummyDir, { recursive: true, force: true });
   }
 }
