3588e2f51275c4f31139886c702e131d6e572695
nakaone <ena.kaon@gmail.com>
2026-02-12 11:57:35 +0900

    Update test.sh

--- underDev/specify/test.sh
+++ underDev/specify/test.sh
@@ -14,16 +14,18 @@ jsdoc -X sample/test.js > $tmp/test.json
 node createSpec.mjs sample/test.js -o $doc/test > $tmp/test.result.txt 2> $tmp/test.error.txt
 
 # 2. createSpec本体
-jsdoc -X createSpec.mjs > $tmp/createSpec.json
+cp createSpec.mjs $tmp/createSpec.js
+jsdoc -X $tmp/createSpec.js > $tmp/createSpec.json
+rm $tmp/createSpec.js
 node createSpec.mjs createSpec.mjs -o $doc/createSpec > $tmp/createSpec.result.txt 2> $tmp/createSpec.error.txt
 
 # 3. 引数無しでの起動時
-msg="\n=== no argument"; echo $msg > $tmp/noarg.result.txt; echo $msg > $tmp/noarg.error.txt
-node createSpec.mjs >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
-msg="\n=== arg = --help"; echo $msg >> $tmp/noarg.result.txt; echo $msg >> $tmp/noarg.error.txt
-node createSpec.mjs --help >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
-msg="\n=== arg = -H"; echo $msg >> $tmp/noarg.result.txt; echo $msg >> $tmp/noarg.error.txt
-node createSpec.mjs -H >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
+#msg="\n=== no argument"; echo $msg > $tmp/noarg.result.txt; echo $msg > $tmp/noarg.error.txt
+#node createSpec.mjs >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
+#msg="\n=== arg = --help"; echo $msg >> $tmp/noarg.result.txt; echo $msg >> $tmp/noarg.error.txt
+#node createSpec.mjs --help >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
+#msg="\n=== arg = -H"; echo $msg >> $tmp/noarg.result.txt; echo $msg >> $tmp/noarg.error.txt
+#node createSpec.mjs -H >> $tmp/noarg.result.txt 2>> $tmp/noarg.error.txt
 
 # --- 開発用サンプル
 
