035a21ba00f35625fd8ba10147ec220df7a6d5a4
nakaone <ena.kaon@gmail.com>
2026-02-20 11:05:07 +0900

    Update archives.sh

--- underDev/Auth/tools/archives.sh
+++ underDev/Auth/tools/archives.sh
@@ -1,16 +1,11 @@
-#!/bin/sh
-# -x  つけるとverbose
-set -e # エラー時点で停止
-source ~/Desktop/GitHub/tools/common.sh
+#!/usr/bin/env zsh
 
+Desktop="/Users/ena.kaon/Desktop"
+GitHub="$Desktop/GitHub"
+lib="$GitHub/library"
 prj="$lib/underDev/Auth"
 arc="$prj/archives"
-src="$prj/src"
-doc="$prj/doc"
 
-# ----------------------------------------------
-# バックアップファイルの作成
-# ----------------------------------------------
 cd $prj
 zip -r $arc/`date +"%Y%m%d"`.zip ./ -x "archives/*" "node_modules/*" "tmp/*"
 # 除外指定(-x)ではフォルダ名を直接指定のこと(変数だと除外されない)
