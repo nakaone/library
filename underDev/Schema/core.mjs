b1d0e23bc08df85af4452ec104ef6ac482c0b036
nakaone <ena.kaon@gmail.com>
2026-03-07 15:50:12 +0900

    Update core.mjs

--- underDev/Schema/core.mjs
+++ underDev/Schema/core.mjs
@@ -1,3 +1,5 @@
+import { utils } from "./utils.mjs";
+
 /** Schema: DB・データ型構造定義オブジェクト
  * - 各種アプリで使用するテーブル・データ型を宣言
  * - 各種アプリでは本クラスを拡張し、configとすることを想定
@@ -8,10 +10,8 @@
  * @property {string} [dbName] - 物理DB名。省略時はnameを流用
  * @property {string} [desc=''] - Schema全体に関する概要説明
  * @property {string} [note=''] - Schema全体に関する備考
- * @property {Object.<string, TypeDef>} types - 論理テーブル名をキーとするテーブル定義
+ * @property {Object.<string, TableDef>} types - 論理テーブル名をキーとするテーブル定義
  * @property {string} original - Schemaインスタンス生成時の引数(JSON)。自動生成、設定不可
- * @property {string[]} allowedColumnTypes - 許容するColumnのデータ型のリスト。自動生成、設定不可<br>
- *   - 'string' | 'number' | 'boolean' | 'object' | 'array' | 'datetime' | 'function'
  * 
  * @example
  * ```
@@ -30,8 +30,11 @@
  * ```
  * 
  * @history
- * - 2.1.1 2026/03/07
- *   - TypeDef, ColumnDefをクラス化
+ * - 2.2.0 2026/03/07
+ *   - TableDef, ColumnDefをクラス化
+ *   - isObjectをutilsに移管
+ *   - sanitizeArgが使われていなかったので削除
+ *   - factoryをSchemaからTableDefに移動
  * - 2.1.0 2026-01-24
  *   - defaultのデータ型として関数利用を可能に変更
  * - 2.0.0 2026-01-13
@@ -62,7 +65,7 @@ export class Schema {
       for( v.i=0 ; v.i<arg.length ; v.i++ ){
 
         dev.step(2);  // 引数argの型チェック
-        if( !this.isObject(arg[v.i]) )
+        if( !utils.isObject(arg[v.i]) )
           throw new Error(`argument must be object.`);
 
         dev.step(3);  // 既定値・初期値の設定
@@ -75,14 +78,12 @@ export class Schema {
         this.types = this.types ?? {};
         // 引数argのバックアップも上書きしない
         this.original = this.original ?? JSON.stringify(arg);
-        this.allowedColumnTypes 
-          = ['string','number','boolean','object','array','datetime','function'];
 
-        dev.step(4);  // 子要素(TypeDef)の設定
-        if( this.isObject(arg[v.i].types) ){
+        dev.step(4);  // 子要素(TableDef)の設定
+        if( utils.isObject(arg[v.i].types) ){
           // arg[v.i].types定義分を追加。同名なら上書き
           Object.keys(arg[v.i].types).forEach(x => {
-            this.types[x] = this.typedef(x,arg[v.i].types[x]);
+            this.types[x] = new TableDef(x,arg[v.i].types[x]);
           })
         }
       }
@@ -97,165 +98,91 @@ export class Schema {
       return dev.error(e);
     }
   }
+}
 
-  /** isObject: 引数がオブジェクトであるか判定
-   * @memberof Schema
-   * @param {any} arg - 判定対象
-   * @returns {boolean} オブジェクトならtrue
-   */
-  isObject(arg){
-    return typeof arg !== 'undefined'
-      && arg !== null
-      && typeof arg === 'object'
-      && !Array.isArray(arg)
-      && arg.constructor === Object;
-  }
+/** TableDef: 論理テーブル構造定義
+ * - name~colsはインスタンス生成時に指定、header以下は自動生成
+ * 
+ * @class
+ * @property {string} name - 論理的な識別名（TableDef のキー）
+ *   - クラス・API・ログで使用。例: 'Member', 'AuthAuditLog'
+ *   - constructorに渡す定義オブジェクトでは省略(メンバ名を引用)
+ * @property {string} [desc=''] - テーブルに関する概要説明
+ * @property {string} [note=''] - テーブルに関する備考
+ * @property {string[]} [primaryKey=[]] - 主キー項目名
+ * @property {string[]} [unique=[]] - 主キー以外の一意制約
+ * @property {ColumnDef[]} cols - 項目定義（順序を考慮するため配列）
+ * 
+ * @property {string[]} header - 項目名の一覧(引数不可、自動生成)
+ * @property {Object.<string, ColumnDef>} map - 項目名をキーとする項目定義集(引数不可、自動生成)
+ * 
+ * @history
+ * - rev.1.0.0: 2026/03/07 初版
+ */
+export class TableDef {
 
-  /** TypeDef: 論理テーブル構造定義
-   * @typedef {Object} TypeDef - 論理テーブル構造定義
-   * @memberof Schema
-   * @property {string} name - 論理的な識別名（TypeDef のキー）
-   *   - クラス・API・ログで使用。例: 'Member', 'AuthAuditLog'
-   *   - constructorに渡す定義オブジェクトでは省略(メンバ名を引用)
-   * @property {string} [typeName='TypeDef'] - データ型名
-   *   - 同一データ型のテーブル・インスタンスを複数使用する場合、instanceofの代わりに使用
-   * @property {string} [desc=''] - テーブルに関する概要説明
-   * @property {string} [note=''] - テーブルに関する備考
-   * @property {string[]} [primaryKey=[]] - 主キー項目名
-   * @property {string[]} [unique=[]] - 主キー以外の一意制約
-   * @property {ColumnDef[]} cols - 項目定義（順序を考慮するため配列）
-   * @property {string[]} header - 項目名の一覧(引数不可、自動生成)
-   * @property {Object.<string, ColumnDef>} map - 項目名をキーとする項目定義集(引数不可、自動生成)
+  /**
+   * @constructor
+   * @memberof TableDef
+   * @param {string} name - オブジェクト名(TableDef.name設定値)
+   * @param {Object} obj - 初期設定値のオブジェクト(TableDef型の値指定) 
+   * @returns {TableDef|Error}
    */
-  /** typedef: TypeDef型オブジェクトを生成
-   * @memberof Schema
-   * @param {string} name - オブジェクト名(TypeDef.name設定値)
-   * @param {Object} obj - 生成するオブジェクトに設定される値(値指定) 
-   * @returns {TypeDef|Error}
-   */
-  typedef(name,obj){
-    const v = {whois:this.constructor.name+'.typedef',arg:{name,obj},rv:{}};
-    const dev = new devTools(v);
+  constructor(name,obj) {
+    const v = {whois:`TableDef.constructor`, arg:{name,obj}, rv:null};
+    const dev = new devTools(v,{mode:'pipe'});
     try {
 
       dev.step(1.1);  // 引数の型チェック
       if( typeof name !== 'string' )
         throw new Error(`argument "name" must be string.`);
-      if( !this.isObject(obj) )
+      if( !utils.isObject(obj) )
         throw new Error(`argument "obj" must be object.`);
 
       dev.step(1.2);  // 必須指定項目の存在チェック
       if( typeof obj.cols === 'undefined' || !Array.isArray(obj.cols) )
-        throw new Error(`cols must be array of length 1 or greater.`);
+        throw new Error(`argument "cols" must be array of length 1 or greater.`);
 
       dev.step(2);  // 既定値・初期値の設定
-      v.rv.name = name;
-      v.rv.typeName = 'TypeDef';  // instanceofの代替
-      v.rv.desc = obj.desc ?? '';
-      v.rv.note = obj.note ?? '';
-      v.rv.primaryKey = obj.primaryKey ?? [];
-      v.rv.unique = obj.unique ?? [];
-      v.rv.cols = [];
-      v.rv.header = []; // 自動生成項目
-      v.rv.map = {};    // 自動生成項目
+      this.name = name;
+      this.desc = obj.desc ?? '';
+      this.note = obj.note ?? '';
+      this.primaryKey = obj.primaryKey ?? [];
+      this.unique = obj.unique ?? [];
+      this.cols = [];
+      this.header = []; // 自動生成項目
+      this.map = {};    // 自動生成項目
 
       dev.step(3);  // 子要素(ColumnDef)の設定
       obj.cols.forEach(x => {
-        v.col = this.columndef(x);
+        v.col = new ColumnDef(x);
         if( v.col instanceof Error ) throw v.col;
 
-        v.rv.cols.push(v.col);
-        v.rv.header.push(v.col.name);
-        v.rv.map[v.col.name] = v.col;
+        this.cols.push(v.col);
+        this.header.push(v.col.name);
+        this.map[v.col.name] = v.col;
       })
 
-      dev.end();
-      return v.rv;
-    } catch(e) {
-      return dev.error(e);
-    }
+      dev.end(); // 終了処理
+
+    } catch (e) { return dev.error(e); }
   }
 
-  /** ColumnDef: 項目定義
-   * @typedef {Object} ColumnDef - 項目定義
-   * @property {string} name - 項目名（英数字・システム用）
-   * @property {string} [label] - 表示用ラベル（省略時は name)
-   * @property {string} [desc=''] - 項目に関する概要説明
-   * @property {string} [note=''] - 項目に関する備考・意味説明
-   * @property {string} [type='string'] - 論理データ型
-   *   - 'string' | 'number' | 'boolean' | 'object' | 'array' | 'datetime' | 'function'
-   * @property {boolean} [nullable=true] - null を許可するか
-   * @property {any} [default=null] - 既定値
-   *   - データ型が関数の場合、引数はfactoryメソッドに渡されるargと看做す
-   * 
-   * 【datetimeが固定値ではない場合の記述方法】
-   * ex. factoryメソッドで生成するオブジェクトに生成日時を設定したい
-   *   ColumnDef.default = "Date.now()"
-   * ex. 有効期間として1日(24時間)後を設定したい
-   *   ColumnDef.default = "new Date(Date.now()+24*3600*1000)"
-   * - 【注意】引数は使用不可
-   * - factoryメソッドではこれを new Function('x',`return ${default}`) として関数化し、実行結果を返す
-   */
-  /** columndef: ColumnDef型オブジェクトを生成
-   * @memberof Schema
-   * @param {Object} obj - 生成するオブジェクトに設定される値(値指定) 
-   * @returns {ColumnDef|Error}
-   */
-  columndef(obj){
-    const v = {whois:this.constructor.name+'.columndef',arg:{obj},rv:{}};
-    const dev = new devTools(v);
-    try {
-
-      dev.step(1.1);  // 引数の型チェック
-      if( !this.isObject(obj) )
-        throw new Error(`argument "obj" must be object.`);
-
-      dev.step(1.2);  // 必須指定項目の存在チェック
-      ['name'].forEach(x => {
-        if( !obj.hasOwnProperty(x) )
-          throw new Error(`"${x}" is not specified.`);
-      });
-
-      dev.step(2);  // 既定値・初期値の設定
-      v.rv.name = obj.name;
-      v.rv.label = obj.label ?? obj.name;
-      v.rv.desc = obj.desc ?? '';
-      v.rv.note = obj.note ?? '';
-      v.rv.type = obj.type ?? 'string';
-      v.rv.nullable = obj.nullable ?? true;
-      v.rv.default = obj.default ?? null;
-
-      dev.step(3);  // データ型のチェック
-      if( !this.allowedColumnTypes.includes(v.rv.type) )
-        throw new Error(`"${v.rv.name}" is invalid type.`);
-
-      dev.end();
-      return v.rv;
-    } catch(e) {
-      return dev.error(e);
-    }
-  }
-
-  /** factory: 指定TypeDef型のオブジェクトを生成
-   * @memberof Schema
-   * @param {string} name - データ型名。TypeDef.name
+  /** factory: 論理テーブル構造定義を基に実オブジェクトを生成
+   * @memberof TableDef
    * @param {Object.<string, any>} [arg={}] - 各メンバの設定値
-   * @returns {Object} TypeDefで指定したデータ型のオブジェクト
+   * @returns {Object} TableDefで指定したデータ型のオブジェクト
    */
-  factory(name,arg={}){
-    const v = {whois:this.constructor.name+'.factory',arg:{name,arg},rv:{}};
-    const dev = new devTools(v);
+  factory(arg) {
+    const v = {whois:`${this.constructor.name}.factory`, arg:{arg}, rv:null};
+    const dev = new devTools(v,{mode:'pipe'});
     try {
 
       dev.step(1);  // 引数のチェック
-      if( typeof name !== 'string' )
-        throw new Error(`argument "name" must be string.`);
-      if( !Object.keys(this.types).includes(name) ) // 未定義のtypeならエラー
-        throw new Error(`"${name}" is not defined.`);
-      if( !this.isObject(arg) )
+      if( !utils.isObject(arg) )
         throw new Error(`argument "arg" must be object.`);
 
-      dev.step(2,arg);  // 判定・変換ロジックの定義
+      dev.step(2);  // 判定・変換ロジックの定義
       v.trueValues = [1, '1', -1, '-1', 'true', 'TRUE', true];
       v.falseValues = [0, '0', 'false', 'FALSE', false];
       v.eval = x => { return typeof x === 'function' ? (x(arg) ?? null) : null };
@@ -267,7 +194,7 @@ export class Schema {
         },
         boolean: x => v.trueValues.includes(x) ? true
           : (v.falseValues.includes(x) ? false : v.eval(x)),
-        object: x => this.isObject(x) ? x : v.eval(x),
+        object: x => utils.isObject(x) ? x : v.eval(x),
         array: x => Array.isArray(x) ? x : v.eval(x),
         datetime: x => {  // 日付型
           if( x === null ) return null; // null指定は無効
@@ -296,8 +223,8 @@ export class Schema {
       };
 
       // オブジェクトの項目毎に値を設定
-      this.types[name].cols.forEach(col => {
-        if( arg.hasOwnProperty(col.name) ){
+      this.cols.forEach(col => {
+        if( Object.hasOwn(arg,col.name) ){
           dev.step(3.1); // 引数arg(設定値の指定)に有った場合
           v.rv[col.name] = v.checkers[col.type](arg[col.name]);
           dev.step(3.2); // 指定が有るのに指定データ型に変換できなかった場合はエラー
@@ -324,68 +251,72 @@ export class Schema {
         }
       });
 
-      dev.end();
+      dev.end(); // 終了処理
       return v.rv;
-    } catch(e) {
-      return dev.error(e);
-    }
-  }
 
-  /** sanitizeArg: プリミティブ型のみで構成されるよう無毒化
-   * @memberof Schema
-   * @param {*} value - チェック対象の変数
-   * @param {string} path='$' - エラーメッセージ用にオブジェクト内の階層を保持
-   * @returns {Object|Error}
+    } catch (e) { return dev.error(e); }
+  }
+}
+
+/** ColumnDef: 論理テーブルに於ける項目定義
+ * - name~defaultはインスタンス生成時に指定、allowedColumnTypes以下は自動生成
+ * 
+ * @class
+ * @property {string} name - 項目名（英数字・システム用）
+ * @property {string} [label] - 表示用ラベル（省略時は name)
+ * @property {string} [desc=''] - 項目に関する概要説明
+ * @property {string} [note=''] - 項目に関する備考・意味説明
+ * @property {string} [type='string'] - 論理データ型。allowedColumnTypesの何れか
+ * @property {boolean} [nullable=true] - null を許可するか
+ * @property {any} [default=null] - 既定値
+ *   - データ型が関数の場合、引数はfactoryメソッドに渡されるargと看做す
+ * 
+ * @property {string[]} allowedColumnTypes - 許容するColumnのデータ型のリスト。自動生成、設定不可<br>
+ *   - 'string' | 'number' | 'boolean' | 'object' | 'array' | 'datetime' | 'function'
+ * 
+ * @history
+ * - rev.1.0.0: 2026/03/07 初版
+ */
+export class ColumnDef {
+
+  /**
+   * @constructor
+   * @memberof ColumnDef
+   * @param {Object} obj - 生成するオブジェクトに設定される値(値指定) 
+   * @returns {ColumnDef|Error}
    */
-  sanitizeArg(value, path = '$') {
-    const v = {whois:this.constructor.name+'.sanitizeArg',arg:{value,path},rv:null};
-    const dev = new devTools(v);
+  constructor(obj) {
+    const v = {whois:`ColumnDef.constructor`, arg:{obj}, rv:null};
+    const dev = new devTools(v,{mode:'pipe'});
     try {
 
-      main: {
-        dev.step(1);
-        if( value === null ||
-          typeof value === 'string' ||
-          (typeof value === 'number' && Number.isFinite(value)) ||
-          typeof value === 'boolean'
-        ){
-          v.rv = value;
-          break main;
-        };
+      dev.step(1.1);  // 引数の型チェック
+      if( !utils.isObject(obj) )
+        throw new Error(`argument "obj" must be object.`);
 
-        if (Array.isArray(value)) {
-          v.rv = value.map((v, i) => this.sanitizeArg(v, `${path}[${i}]`));
-          break main;
-        }
+      dev.step(1.2);  // 必須指定項目の存在チェック
+      ['name'].forEach(x => {
+        if( !Object.hasOwn(obj,x) )
+          throw new Error(`"${x}" is not specified.`);
+      });
 
-        if (typeof value === 'object') {
-          // 明示的に禁止したい型
-          if (
-            value instanceof CryptoKey ||
-            value instanceof Date ||
-            value instanceof Error ||
-            value instanceof Map ||
-            value instanceof Set
-          ) {
-            throw new Error(`Unsupported argument type at ${path}`);
-          }
+      dev.step(2);  // 既定値・初期値の設定
+      this.name = obj.name;
+      this.label = obj.label ?? obj.name;
+      this.desc = obj.desc ?? '';
+      this.note = obj.note ?? '';
+      this.type = obj.type ?? 'string';
+      this.nullable = obj.nullable ?? true;
+      this.default = obj.default ?? null;
+      this.allowedColumnTypes
+        = ['string','number','boolean','object','array','datetime','function'];
 
-          const obj = {};
-          for (const [k, v] of Object.entries(value)) {
-            if (typeof v === 'function') {
-              throw new Error(`Function not allowed in arg at ${path}.${k}`);
-            }
-            obj[k] = this.sanitizeArg(v, `${path}.${k}`);
-          }
-          v.rv = obj;
-        }
-      }
-      dev.end();
-      return v.rv;
-    } catch(e) {
-      return dev.error(e);
-    }
+      dev.step(3);  // データ型のチェック
+      if( !this.allowedColumnTypes.includes(this.type) )
+        throw new Error(`"${this.name}" is invalid type.`);
 
-    throw new Error(`Unsupported value type at ${path}`);
+      dev.end(); // 終了処理
+
+    } catch (e) { return dev.error(e); }
   }
 }
