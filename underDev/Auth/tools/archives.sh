920215094752095e8a67fe360361123b39b2f518
nakaone <ena.kaon@gmail.com>
2026-03-07 13:05:09 +0900

    Update archives.sh

--- underDev/Auth/tools/archives.sh
+++ underDev/Auth/tools/archives.sh
@@ -6,6 +6,8 @@ lib="$GitHub/library"
 prj="$lib/underDev/Auth"
 arc="$prj/archives"
 
+find $GitHub -name '.DS_Store' -type f -ls -delete
+
 cd $prj
 zip -r $arc/`date +"%Y%m%d"`.zip ./ -x "archives/*" "node_modules/*" "tmp/*"
 # 除外指定(-x)ではフォルダ名を直接指定のこと(変数だと除外されない)
